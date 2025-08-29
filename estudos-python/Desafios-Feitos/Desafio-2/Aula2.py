4ab896041dbed6e24a90be84545d067840dbcdac
pietromarinho22 <pietrovmarinho@gmail.com>
2025-08-29 17:57:55 -0300

    Desafios Python Iniciais

---
+++ estudos-python/Desafios-Feitos/Desafio-2/Aula2.py
@@ -0,0 +1,4 @@
+dia = input ('Em dia você nasceu?')
+mes = input ('Qual o mês?')
+ano = input ('E o ano?')
+print ('Você nasceu no dia', dia , '/' , mes, '/' , ano , '!' , 'Correto?')
---
+++ estudos-python/Desafios-Feitos/Desafio-3/Aula3.py
@@ -0,0 +1,3 @@
+numero_1= int (input("Escolha um número para ser o primeiro termo da soma" ))
+numero_2= int (input("Agora escolha o segundo número "))
+print (numero_1 + numero_2)
