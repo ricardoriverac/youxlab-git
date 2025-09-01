8b496e9d96f56bfd34e2ac092084cfa38e2ff3de
Youx <deivs.abreu-youxlab@institutoyoux.org.br>
2025-09-01 17:17:55 -0300

    exercicios

---
+++ exercicios.py/exercicios2.py
@@ -0,0 +1,4 @@
+dia = input("Que dia do mês em que nasceu?")
+mes = input("Em que mês você nasceu?")
+ano = input("Por fim, Em que ano você nasceu?")
+print ("Você nasceu no dia", dia,"de", mes, "de", ano)
---
+++ exercicios.py/exercicios3.py
@@ -0,0 +1,6 @@
+x = input("digite um numero")
+y = input("agora o segundo")
+x = int(x)
+y = int(y)
+soma = x + y
+print("seu numero final é", soma)
