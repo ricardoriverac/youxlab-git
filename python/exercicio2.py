d105aa7023a01117941413f7db7f65acad2458ce
Youx <juan.ferreira-youxlab@institutoyoux.org.br>
2025-09-01 17:45:51 -0300

    exercicio 2 do curso de python

---
+++ python/exercicio2.py
@@ -0,0 +1,4 @@
+dia = input ("qual dia voce nasceu? ")
+mes = input ("que mes voce nasceu? ")
+ano = input ("qual ano voce nasceu? ")
+print ("voce nasceu no dia " , dia , 'do mes de ' , mes  ,'do ano de ' , ano ,"! Correto?")
