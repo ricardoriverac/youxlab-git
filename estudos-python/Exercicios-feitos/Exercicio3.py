31ba29519aec8140f948719e892ca2584af2f867
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-01 15:12:41 -0300

    Exercício corrigido de outra maneira

--- estudos-python/Exercicios-feitos/Exercicio3.py
+++ estudos-python/Exercicios-feitos/Exercicio3.py
@@ -1,4 +1,4 @@
 numero1 = int (input ('Insira um número inteiro '))
 numero2 = int (input ('Agora outro '))
 numero3 = int (numero1 + numero2)
-print ('A soma de {0} e {1} é {2}'.format(numero1, numero2, numero3))
+print ('A soma de {} e {} é {}'.format(numero1, numero2, numero3))
