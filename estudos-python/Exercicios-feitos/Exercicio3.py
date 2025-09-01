67f1d7d447f8f09c34b509c3ff4a499daee0cabb
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-01 14:46:58 -0300

    Exercício novo feito

---
+++ estudos-python/Exercicios-feitos/Exercicio3.py
@@ -0,0 +1,4 @@
+numero1 = int (input ('Insira um número inteiro'))
+numero2 = int (input ('Agora outro'))
+numero3 = int (numero1 + numero2)
+print ('A soma de ' , numero1 , 'e' , numero2 , 'é' , numero3)
