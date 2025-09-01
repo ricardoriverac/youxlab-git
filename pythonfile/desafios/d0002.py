886ee2e14391677650c4bebc6ff362949ad5eb74
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-09-01 17:23:38 -0300

    eu fiz o desafio 2

---
+++ pythonfile/desafios/d0002.py
@@ -0,0 +1,4 @@
+dia=int(input("Qual o dia do seu nascimento"))
+mes=str(input("Qual o mes que voce nasceu? "))
+ano=int(input("Qual o ano que voce nasceu? "))
+print(f"Voce nasceu no dia {dia}, no mes {mes} e no ano {ano}!")
