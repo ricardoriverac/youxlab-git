70f65fc1df86bf75dd336a922b6205a532f38d99
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-09-02 16:42:41 -0300

    eu terminei o desafio 4

--- pythonfile/desafios/d0003.py
+++ pythonfile/desafios/d0003.py
@@ -1,4 +1,4 @@
 numero=int(input('Digite um numero'))
 numero2=int(input('Digite outro numero'))
 soma = numero + numero2
-print(soma)
+print(f"A soma dos dois números resulta em uma resposta equivalente a: {soma}")
