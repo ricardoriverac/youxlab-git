ef66bb861430c3fbc48be35b12aedd5cfa094f31
otto_fillipi <ottofillipi185@gmail.com>
2025-09-02 16:11:45 -0300

    python/aula_1

---
+++ python/aula_1/anotacoes.py
@@ -0,0 +1,2 @@
+print('bilolinha redonda 88 gramas')
+print(4+78)
---
+++ python/aula_1/atividade_1.py
@@ -0,0 +1,23 @@
+#AULA 1/ATIVIDADE 1/
+
+#Atividade sobre print 
+print('Olá, mundo')
+
+#Caulculos de números 
+print(7+3)
+
+#Mostrar números
+print('7'+'3')
+
+#Juntar números e letras utilizando  á " , " (\n pode ser utilizado como uma quebra de linha  )
+print('Quantos anos você tem?\nEu tenho' , 15 , 'anos')
+
+#Como criar variaveis 
+
+nome = 'otto'
+
+idade = 25 
+
+peso = 75.8 
+
+print(nome , idade , peso)
