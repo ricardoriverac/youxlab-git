676311fb4b67d508c105b22bd5e31d3c84e4f2e0
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-02 14:36:41 -0300

    Coisinha de falar antecessor e sucessor

--- estudos-python/Exercicios-feitos/Exercicio5.py
+++ estudos-python/Exercicios-feitos/Exercicio5.py
@@ -1,2 +1,4 @@
-nome = input ('Insira seu nome')
-print ('Prazer em te conhecer {:^20} !'.format(nome))
+numero = int (input ('Insira um número'))
+numeroSucessor = (numero + 1)
+numeroAntecessor = (numero - 1)
+print ('Esse é seu número {} ! Seu sucessor e antecessor são {} e {}'.format(numero,numeroSucessor,numeroAntecessor))
