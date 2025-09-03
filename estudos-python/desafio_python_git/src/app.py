fb69fd303cc8ef33b6a93180cc7f484501ca2381
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-03 17:51:51 -0300

    Criando e colocando o arquivo app.py dentro da nova pasta src

--- estudos-python/desafio_python_git/src/app.py
+++ estudos-python/desafio_python_git/src/app.py
@@ -1,4 +1,4 @@
-nome = input('Digite seu nome')
-idade = input('Sua idade')
-linguagem = input('E linguagem de programação favorita')
+nome = input('Digite seu nome ')
+idade = input('Sua idade ')
+linguagem = input('E linguagem de programação favorita ')
 print (f'Seu {nome}, {idade} e {linguagem}')
