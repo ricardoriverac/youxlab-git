96ce3900bc682793c1c99b5528d79f2e2658c125
Youx <deivs.abreu-youxlab@institutoyoux.org.br>
2025-09-03 13:51:45 -0300

    2x3xraiz7

---
+++ exercicios.py/2x 3x e raiz.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um número: '))
+d = n * 2
+t = n * 3 
+r = n ** (1/2)
+print(f'O dobro de {n} vale {n*2}.\nO triplo de {n} vale {n*3}.\nA raiz quadrada de {n} é igual a {n**(1/2)}.')
