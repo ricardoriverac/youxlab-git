d9b27d8c8904ce2274272eb998619fe8f347c983
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-09-03 15:22:00 -0300

    eu terminei os desafios 6,7,8,9 e 10

---
+++ pythonfile/d0007.py
@@ -0,0 +1,4 @@
+nota= int(input('Digite sua nota'))
+nota2= int(input('Digite a segunda nota'))
+media= (nota + nota2) /2 
+print('A sua media é ', media)
---
+++ pythonfile/d0010.py
@@ -0,0 +1,4 @@
+carteira= float(input('Quanto voce tem na carteira?'))
+dólar= 3.27
+conversao= carteira/dólar
+print(f'Voce tem o valor de:{conversao:.2f} dólares')
