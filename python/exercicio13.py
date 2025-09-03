3009bd4dceac1a99bf7a64b5477acf775a077e6f
Youx <juan.ferreira-youxlab@institutoyoux.org.br>
2025-09-03 16:19:16 -0300

    aumento de 15% de um determinado número

---
+++ python/exercicio13.py
@@ -0,0 +1,4 @@
+salario = float(input('Qual o valor? '))
+aument = salario*15/100
+resutado = salario + aument
+print ('o sálairo atuale é {}, e o sálario com o aumento aplicado é {}' .format (salario, resutado))
