3bbfc36ad06ce5b4947d609130b567a81d3d75a3
Youx <juan.ferreira-youxlab@institutoyoux.org.br>
2025-09-03 15:15:08 -0300

    calculando a área de um determinado número

---
+++ python/exercicio11.py
@@ -0,0 +1,4 @@
+l = float(input('quantos metros de altura a parede tem? '))
+t = float(input('quantos metros de largura a parede tem? '))
+mq = l*t
+print ('a parede tem a dimensão de {}x{}, e a área de {} metros quadrados' .format(l, t, mq))
