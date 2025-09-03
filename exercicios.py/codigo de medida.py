4dd8c239883d29801f174f05b70e8a1e38bff47d
deivs683 <deivs.abreu-youxlab@institutoyoux.org.br>
2025-09-03 13:57:42 -0300

    codigo de medida.py

--- exercicios.py/codigo de medida.py
+++ exercicios.py/codigo de medida.py
@@ -1,1 +1,4 @@
-print=(caba_boca)
+medida = float(input('Uma distância em metros: '))
+cm = medida * 100
+mm = medida * 1000
+print('A média de {}m corresponde a {}cm e {}mm'.format(medida, cm, mm))
