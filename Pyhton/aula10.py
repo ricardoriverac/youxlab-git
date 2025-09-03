679d6af86e3043c0d1c5905ed120ae2b8760fd0f
Emily <emily.ferreira-youxlab@institutoyoux.org.br>
2025-09-02 22:14:54 -0300

    desafio 6

---
+++ Pyhton/aula10.py
@@ -0,0 +1,7 @@
+numero = float(input("Digite um número: "))
+dobro = numero * 2
+triplo = numero * 3
+raiz_quadrada = numero ** 2
+print(f"O dobro de {numero} é: {dobro}")
+print(f"O triplo de {numero} é: {triplo}")
+print(f"A raiz quadrada de {numero} é: {raiz_quadrada:.2f}")
