5a15fc1108030f631f003cd02257f7107e8825a2
desafio_python_git <ottofillipi185@gmail.com>
2025-09-05 17:19:14 -0300

    aulas_python

---
+++ python/aula_2/aulas/aula_1.py
@@ -0,0 +1,15 @@
+#Tipos primitivos 
+
+#Para comverter um tipo para outro e necessario passar os dados dentro do parenteses(O valor que a variável receberá depende do seu tipo)
+
+
+'''Exemplo:
+n = int('11')'''
+
+''' n1 = int(input('Digite um número))'''
+
+#Exemplo
+
+n1 = int(input('Digite um número : ' ))
+print(f'Esse e o número escolhido {n1}')
+
