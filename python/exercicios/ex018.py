cc33c46e79fbab750bb996f71af90d4abe213216
agathae <agatha.silva-youxlab@institutoyoux.org.br>
2025-09-07 17:01:36 -0300

    Organizei as pastas e atualizei as atividades

--- python/exercicios/ex018.py
+++ python/exercicios/ex018.py
@@ -4,4 +4,3 @@ seno = math.sin(math.radians(an))
 cos = math.cos(math.radians(an))
 tan = math.tan(math.radians(an))
 print('com base no valor do seu angulo,aqui esta seu seno {:.2f},cosseno {:.2f} e tangente : {:.2f}' .format(seno,cos,tan))
-
