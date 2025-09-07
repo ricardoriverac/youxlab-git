d4b6f3d0a7e38fee7a1d48a631fb12de15de3664
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-09-07 20:13:49 -0300

    eu terminei o desafio 18

---
+++ pythonfile/desafios/d0018.py
@@ -0,0 +1,8 @@
+import math
+angulo = float(input('Digite um angulo: '))
+seno = math.sin(angulo)
+cos = math.cos(angulo)
+tan = math.tan(angulo)
+print('O valor do seno é {:.5f}'.format(seno))
+print('O valor do cosseno é {:.5f}'.format(cos))
+print('O valor da tangente é {:.5f}'.format(tan))
