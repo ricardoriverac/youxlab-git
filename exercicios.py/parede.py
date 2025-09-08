c30d68ebdfee5cba6e787f83468b6920852cac23
Youx <deivs.abreu-youxlab@institutoyoux.org.br>
2025-09-08 13:11:06 -0300

    desafio

---
+++ exercicios.py/parede.py
@@ -0,0 +1,6 @@
+larg = float (input('largura da parede: '))
+alt = float(input('altura da parede: '))
+area = larg * alt 
+print('sua parede tem a dimensao de{}x{}e sua area é de {} m²')
+tinta=area
+print('para pintar essa parede voce precisa de {} tinta.'.format(tinta))
---
+++ projeto/app.py
@@ -0,0 +1,6 @@
+nome = "Deivs"
+idade = 16
+altura = 1.76
+linguagem_de_programacao = 'python'
+mensagem = f"Olá, meu nome é {nome}, tenho {idade} anos e minha altura é {altura:.2f} metros e minha linguagem de programação favorita é {linguagem_de_programacao}."
+print(mensagem)
