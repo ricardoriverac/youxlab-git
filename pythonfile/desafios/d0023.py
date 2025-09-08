1ad69a2e8a3af953127e16e2b4035b2821eb8f82
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-09-08 15:09:43 -0300

    eu terminei o desafio 23

---
+++ pythonfile/desafios/d0023.py
@@ -0,0 +1,7 @@
+numero = int(input('Digite um numero'))
+n = str(numero)
+print('Analisando o valor digitado {}'.format(numero))
+print('Unidade {}'.format(n [3]))
+print('Dezena {}'.format(n [2]))
+print('Centena {}'.format(n [1]))
+print('Milhar {}'.format(n [0]))
