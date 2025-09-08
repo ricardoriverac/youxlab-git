eed7c9db052b1db77ce9bc4d833426eda1facaaa
João Othávio <joaoothaviofabri14@gmail.com>
2025-09-08 16:19:55 -0300

    Exercício 16 feito

---
+++ curso-de-python/Módulo-1/exercicios/ex016.py
@@ -0,0 +1,17 @@
+from math import trunc
+numero = float(input('Digite um número com números decimais: '))
+inteiro = trunc(numero)
+print('A parte inteira de {} é {:.0f}'.format(numero, inteiro))
+
+'''Sgunda Forma
+
+from math import trunc
+numero = float(input('Digite um número com números decimais: '))
+print('A parte inteira de {} é {:.0f}'.format(numero, trunc(numero))'''
+
+'''Terceira Forma
+numer = float(input('Digite um valor: '))
+print(A parte inteira de {} é {:.0f}'.format(numero, int(numero)))
+'''
+
+#Vídeo do exercício: https://youtu.be/-iSbDpl5Jhw?si=yhtqTV1MAIUzZcu4
