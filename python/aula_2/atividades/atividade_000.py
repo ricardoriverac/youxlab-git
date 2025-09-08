575bface6ab64fe58690d5c9cac716a5d3884a23
desafio_python_git <ottofillipi185@gmail.com>
2025-09-08 16:26:59 -0300

    atividade_python_git

--- python/aula_2/atividades/atividade_000.py
+++ python/aula_2/atividades/atividade_000.py
@@ -2,4 +2,4 @@ n1 = float(input('Digite um número a seguir : '))
 n2 = float(input('Digite um número a seguir : '))
 r = n1 + n2
 
-print(f'A soma entre {n1} é {n2} e equivalente á {r}')
+print('A soma entre {} é {} e equivalente á {}'.format(n1, n2, r))
