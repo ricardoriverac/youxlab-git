b5444134b8e86a13d690ad07d41173de44599cbd
Youx <isabelamilene74@gmail.com>
2025-09-08 17:49:58 -0300

    ex006

---
+++ python/aula006.py
@@ -0,0 +1,6 @@
+n = int(input('digite um número: '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print('0 dobro de {} vale {},'. format(n, d))
+print('O triplo de {} vale {}. \nA raiz quadrada de {} é igual a {:.2f}'.format(n, t, n, r))
