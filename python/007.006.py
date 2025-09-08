29e6f0d9bb13cd0b66b126d0883480b74f8ed5db
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-09-08 17:49:35 -0500

    006

---
+++ python/007.006.py
@@ -0,0 +1,6 @@
+numero = int(input('digite um numero: '))
+dobro = numero * 2
+triplo = numero * 3
+raiz = numero **(1/2)
+print(f'o dobro de {numero} vale {dobro}')
+print(f'o triplo de {numero} vale {triplo}.\nA raiz quadrada de {numero} e igual a {raiz:.2f}.')
