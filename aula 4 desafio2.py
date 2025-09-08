1f70907ef98cdeed5a8d6ed615bfbac677ac15ca
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2025-09-08 16:46:04 -0300

    aula 7

---
+++ aula 4 desafio2.py
@@ -0,0 +1,6 @@
+dia = int(input( 'que dia vc nasceu?' ))
+mes = input( 'que mês vc nasceu?' )
+ano = int(input( 'que ano vc nasceu?' ))
+print( 'voce nasceu' ,  dia, mes , ano)
+
+
---
+++ teste.py
@@ -0,0 +1,4 @@
+print('Olá mundo')
+nome = input( 'qual é o seu nome?' )
+idade = input( 'qual é a sua idade?' )
+peso = input( 'qual é o seu peso?' )
