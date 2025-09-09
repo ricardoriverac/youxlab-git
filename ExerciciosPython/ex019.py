ea68b7e00d7e4f4ae63f8318ae9695c06e08e473
juliaassis00 <juliaass92@gmail.com>
2025-09-09 15:06:35 -0300

    salvando exercicio 019

---
+++ ExerciciosPython/ex019.py
@@ -0,0 +1,8 @@
+import random
+aluno1= input('Digite o nome do primeiro aluno:')
+aluno2= input('Digite o nome do segundo aluno:')
+aluno3= input('Digite o nome do terceiro aluno:')
+aluno4= input('Digite o nome do quarto aluno:')
+print(f' 1.{aluno1}\n 2.{aluno2}\n 3.{aluno3}\n 4.{aluno4}')
+alunos = random.randint(1, 4)
+print(alunos)
