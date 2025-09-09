badf512dbdcf389b7986f7a9663adce2891f395e
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-09 15:21:15 -0300

    Exercício 22

---
+++ estudos-python/Exercicios-feitos/Exercicio23.py
@@ -0,0 +1,9 @@
+chosenNumber = (input('Choose one number from 0 to 9999 = '))
+unidade=chosenNumber[3]
+dezena=chosenNumber[2]
+centena=chosenNumber[1]
+milhar=chosenNumber[0]
+print('unidade:{}'.format(unidade))
+print('dezena:{}'.format(dezena))
+print('centena:{}'.format(centena))
+print('milhar:{}'.format(milhar))
