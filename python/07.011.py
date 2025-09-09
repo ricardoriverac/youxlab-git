7feb746e16b1da00c230de08e1779825bd5fc749
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-09-09 16:28:42 -0500

    011

---
+++ python/07.011.py
@@ -0,0 +1,6 @@
+largura =float(input('largura da parede: '))
+altura = float(input('altura da parede: '))
+area = largura * altura
+print(f'sua parede tem a dimensao de {largura} x {altura} e sua area e de  {area}m² ')
+tinta = area / 2
+print(f'para pintar uma parede voce precisara de {tinta}l')
