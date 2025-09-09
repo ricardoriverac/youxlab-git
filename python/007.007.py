907f96f1de52808d2654e4e39b81145c918ee1e2
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-09-09 13:51:51 -0500

    exercicios

---
+++ python/007.007.py
@@ -0,0 +1,4 @@
+nota1 = float(input('primeira nota do aluno: '))
+nota2 = float(input('segunda nota do aluno: '))
+media = (nota1 + nota2) /2
+print(f'a media entre {nota1} e {nota2} e igual a {media}')
