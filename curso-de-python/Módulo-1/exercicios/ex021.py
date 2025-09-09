c03ad40beafe462b74e3062ecbfb3b2d4d076e1c
João Othávio <joaoothaviofabri14@gmail.com>
2025-09-09 17:36:49 -0300

    Prática da vídeo aula 09 feita

--- curso-de-python/Módulo-1/exercicios/ex021.py
+++ curso-de-python/Módulo-1/exercicios/ex021.py
@@ -1,6 +1,8 @@
-import pygame
+import pygame # type: ignore
 pygame.init()
 pygame.mixer.music.load('ex021a.mp3')
 pygame.mixer.music.play()
 input()
 pygame.event.wait()
+
+'''Vídeo de exercício: https://youtu.be/9FiEji_fzvk?list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6'''  
---
+++ curso-de-python/Módulo-1/videos-cursos/prática/curso-python-009/app.py
@@ -0,0 +1,34 @@
+frase = 'Curso em Vídeo Python'
+dividido = frase.split()
+print(dividido[2][3])
+
+'''frase = 'Curso em Vídeo Python'
+print(frase.split())'''
+
+
+'''frase = 'Curso em Vídeo Python'
+print(frase.lower().find('vídeo'))'''
+
+'''frase = 'Curso em Vídeo Python'
+print(frase.find('Vídeo'))'''
+
+'''frase = 'Curso em Vídeo Python'
+print('Curso' in frase)'''
+
+'''frase = 'Curso em Vídeo Python'
+print(frase[0])'''
+
+'''frase = 'Curso em Vídeo Python'
+print(frase.replace('Python', 'Android'))'''
+
+'''frase = '   Curso em Vídeo Python   '
+print(len(frase.strip()))'''
+
+'''frase = 'Curso em Vídeo Python'
+print(len(frase))'''
+
+'''frase = 'Curso em Vídeo Python'
+print(frase.upper().count('O'))'''
+
+'''frase = 'Curso em Vídeo Python'
+print(frase[::2])'''
