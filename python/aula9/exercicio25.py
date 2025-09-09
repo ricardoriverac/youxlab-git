2e4faa25bf04136d8e154e62c7d5de66d6906dfd
Lucas RibYouX <lucas.pereira-youxlab@institutoyoux.org.br>
2025-09-09 15:17:00 -0300

    novas atividades

---
+++ python/aula9/exercicio25.py
@@ -0,0 +1,6 @@
+#prog. que lê uma frase e diz quantaz vezes aparece a letra 'A', em que posição ela aparece pela primeira vez e em que posição ela aparece a última vez
+frase = str(input('Digite a frase: ')).upper().strip()
+print('{}'.format(frase.count('A')))
+print('{}'.format(frase.find('A'))) #colocar o +1 após o find alterará a posição do elemento, somando 1
+print('{}'.format(frase.rfind('A')))
+print(f'{frase}')
---
+++ python/aula9/exercicio26.py
@@ -0,0 +1,3 @@
+nome = str(input('Digite o nome completo: ')).split()
+print('{}'.format(nome[0]))
+print('O sobrenome é {}'.format(nome[len(nome) - 1]))
