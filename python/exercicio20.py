6054fe6da6892dc54a374aa83356804e625940f9
Youx <juan.ferreira-youxlab@institutoyoux.org.br>
2025-09-09 13:56:40 -0300

    modificando um nome para ficar em maiusculo, minusculo, contar todas as letra dele e contar as letras do primeiro nome

--- python/exercicio20.py
+++ python/exercicio20.py
@@ -1,9 +1,9 @@
-from random import shuffle
-a1 = str(input('aluno um: '))
-a2 = str(input('aluno dois: '))
-a3 = str(input('aluno tres: '))
-a4 = str(input('aluno quatro: '))
+import random
+a1 = input('aluno um: ')
+a2 = input('aluno dois: ')
+a3 = input('aluno tres: ')
+a4 = input('aluno quatro: ')
 lista  = [a1, a2, a3, a4 ]
-shuffle (lista)
+random.shuffle (lista)
 print ('a ordem de alunos é ')
 print (lista)
---
+++ python/exercicio22.py
@@ -0,0 +1,4 @@
+nome = str(input('Qula o seu nome completo: ')).strip()
+print ('Seu nome em maisculas é {}' .format(nome.upper()))
+print ('Seu nome em minusculas é {}' .format(nome.lower()))
+print ('Seu nome ao tod tem {} '.format(len(nome) - nome.count(' ')))
