4fdb8b70fdada5236453fdc617cbd162b5fb4cd8
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2025-09-09 13:40:03 -0300

    ex007

---
+++ ex007.py
@@ -0,0 +1,5 @@
+nota_1 = float(input('Primeira nota do aluno: '))
+nota_2 = float(input('Segunda nota do aluno: '))
+média = (nota_1 + nota_2) /2 
+print(f'A média entre {nota_1} e {nota_2} é igual a {média}')
+
