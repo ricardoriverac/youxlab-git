11525478c4718d9c86349d9e1982e0502765ef41
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2025-09-09 16:34:21 -0300

    ex011

---
+++ ex011.py
@@ -0,0 +1,6 @@
+largura = float(input('largura da parede:'))
+altura = float(input('altura da parede:'))
+área = largura*altura 
+print(f'Sua parede tem a dimensão de {largura} x {altura} e sua área é de {área}m²')
+tinta = área /2
+print(f'Para pintar essa parede,você precisará de {tinta}l de tinta')
