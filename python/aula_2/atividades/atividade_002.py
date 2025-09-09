0b530f2cd4808913ffe90aa2cb112778d8aadf3d
desafio_python_git <ottofillipi185@gmail.com>
2025-09-09 15:20:06 -0300

    atividades_python

--- python/aula_2/atividades/atividade_002.py
+++ python/aula_2/atividades/atividade_002.py
@@ -6,7 +6,8 @@ as informações possiveis sobre ele
 
 #Responda
 
-informação_quaisquer = input('')
+informação_quaisquer = input('Digite quais quer coisa : ')
 
 tipo_da_variavel = type(informação_quaisquer)
 print(tipo_da_variavel)
+print(informação_quaisquer.isnumeric())
