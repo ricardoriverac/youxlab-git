9ed031b291093cf2a026b1d99e64fd1d02a51210
Emily <emily.ferreira-youxlab@institutoyoux.org.br>
2025-09-10 17:04:55 -0300

    desafio 28

--- desafio28.py
+++ desafio28.py
@@ -1,7 +1,7 @@
 import random
 numeros = [0,1,2,3,4,5]
 resultado = random.choice(numeros)
-print('Pensarei em um número de 0 a 5... Tente adivinhar!')
+print('Pensarei em um número de 0 a 5.... Tente adivinhar!')
 adivinhe = int(input('Escolhe um número: '))
 if adivinhe == (resultado):
     print('Você acertou, parabéns!')
