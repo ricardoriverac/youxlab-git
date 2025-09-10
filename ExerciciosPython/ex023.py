a142ee2bff66948168ad5700c52d3b53ebeca7d9
juliaassis00 <juliaass92@gmail.com>
2025-09-10 16:55:37 -0300

    salvando exercício 23

---
+++ ExerciciosPython/ex023.py
@@ -0,0 +1,7 @@
+num = int(input('Digite um número:'))
+numero = str(num)
+print('Analisando o número {}'.format(num))
+print('Unidades:{}'.format(numero[3])) 
+print('Dezena:{}'.format(numero[2]))
+print('Centena:{}'.format(numero[1]))
+print('Milhar:{}'.format(numero[0]))
