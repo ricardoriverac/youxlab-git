813dc8afe42bfba69c5ebb6d9a7e2790466e3226
Youx <deivs.abreu-youxlab@institutoyoux.org.br>
2025-09-10 17:04:07 -0300

    desafio31

---
+++ exercicios/desafio31.py
@@ -0,0 +1,9 @@
+distancia = float(input('qual a distancia dessa viagem? '))
+print('Aperte os cintos a sua viagem,{}ira começar')
+if  distancia <= 200:
+    preço = distancia *0.50
+else:
+    distancia >= 200
+    preço = distancia *0.45
+print('e o preco da sua viagem sera de R${:.2f}'.format(preço)) 
+        
