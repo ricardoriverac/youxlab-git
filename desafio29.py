07bc68086661723d689de66b83cdd478d2f4c7e5
Emily <emily.ferreira-youxlab@institutoyoux.org.br>
2025-09-10 16:59:37 -0300

    desafio 29

---
+++ desafio29.py
@@ -0,0 +1,8 @@
+velocidade = int(input('Quantos km seu carro estava? '))
+multa = (velocidade - 80) * 7
+if velocidade>80:
+    print('Seu carro foi multado!')
+    print(f'Você precisa pagar a multa de R${multa}')
+else:
+    print('O limite de velocidade é 80km e sua velocidade é {}'.format(velocidade))
+    print('Você está dentro do limite de velocidade!')
--- teoria1.py
+++
@@ -1,4 +0,0 @@
-nome = str(input('Qual é seu nome? '))
-if nome == 'Emily':
-    print('Que nome lindo voê tem')
-print('Bom dia {}'.format(nome))
