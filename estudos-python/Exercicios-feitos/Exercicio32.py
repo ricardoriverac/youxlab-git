53ccd674d0dee24eea6a5ed3969df9cd45aecfd1
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-10 16:29:44 -0300

    Exercício 32

---
+++ estudos-python/Exercicios-feitos/Exercicio32.py
@@ -0,0 +1,5 @@
+year = int(input('Enter a year to verify'))
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    print (f'O ano {year} é bissexto')
+else:
+    print (f'O ano {year} não é bissexto')
