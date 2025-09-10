d187b6a51d0f3162192523f52437754cd40dac15
Emily <emily.ferreira-youxlab@institutoyoux.org.br>
2025-09-10 17:05:45 -0300

    desafio 29

--- desafio29.py
+++ desafio29.py
@@ -5,4 +5,4 @@ if velocidade>80:
     print(f'Você precisa pagar a multa de R${multa}')
 else:
     print('O limite de velocidade é 80km e sua velocidade é {}'.format(velocidade))
-    print('Você está dentro do limite de velocidade!')
+    print('Você está dentro do limite de velocidade! ')
