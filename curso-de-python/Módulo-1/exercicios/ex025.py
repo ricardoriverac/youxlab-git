3757ad1591cad7d9e4350ac690156e37cc321f1f
João Othávio <joaoothaviofabri14@gmail.com>
2025-09-10 15:28:24 -0300

    Exercício 025 feito

---
+++ curso-de-python/Módulo-1/exercicios/ex025.py
@@ -0,0 +1,4 @@
+nome = str(input('Qual é o seu nome? '))
+print('Seu nome tem Silva? {}'.format('SILVA' in nome.upper()))
+
+'''Vídeo de Exercício: https://youtu.be/WHWGz2Dy1ZU?list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6'''
