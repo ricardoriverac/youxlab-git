f093072fe9630418967101c9aa870eb7340dd93b
Youx <isabelamilene74@gmail.com>
2025-09-10 13:31:59 -0300

    ex013.py

---
+++ python/aula012.py
@@ -0,0 +1,4 @@
+preco = float(input('Qual é o preço do produto? R$'))
+novo_preco = preco - (preco *5 / 100) 
+print(f'O produto que custava R${preco}, na promoção com desconto de 5% vai custar R${novo_preco}')
+
---
+++ python/aula013.py
@@ -0,0 +1,3 @@
+salário = float(input('Qual é o salário do Funcionário? R$'))
+novo =  salário + (salário * 15 / 100)
+print(f'O novo valor de {salário} com os 15% de aumento sera de {novo}' )
