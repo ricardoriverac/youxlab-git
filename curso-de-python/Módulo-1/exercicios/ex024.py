1d70a32a2862feeaf00d01bc0782ad0c5cb89c98
João Othávio <joaoothaviofabri14@gmail.com>
2025-09-10 15:16:03 -0300

    Exercício 024 feito

---
+++ curso-de-python/Módulo-1/exercicios/ex024.py
@@ -0,0 +1,4 @@
+cidade = str(input('Em qual cidade você nasceu? ')).strip()
+print(cidade[:5].upper() == 'SANTO')
+
+'''Viídeo de Exercício: https://youtu.be/QroT8cZMRnc?list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6'''
