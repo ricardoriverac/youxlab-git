24a86d079ffac0d6949cd824901e20bbbf5ad682
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-10 15:52:54 -0300

    Exercício 30

---
+++ estudos-python/Exercicios-feitos/Exercicio30.py
@@ -0,0 +1,6 @@
+selectedNumber = int (input ('Enter a number - '))
+selectedNumber = (selectedNumber%2)
+if selectedNumber == 0:
+    print ('Your number is even')
+else:
+    print ('Your number is odd')
