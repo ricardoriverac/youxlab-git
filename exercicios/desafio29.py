1871dd1ada9c2c4d2db38760a9d17651e17fc41b
Youx <deivs.abreu-youxlab@institutoyoux.org.br>
2025-09-10 16:37:13 -0300

    desafio29

--- exercicios/desafio29.py
+++ exercicios/desafio29.py
@@ -1,7 +1,7 @@
 velocidade = float(input('qual a velocidade do seu corsa?'))
 if velocidade>80:
     print('carai esse corsa ta turbinado!')
-    multa = velocidade *7
+    multa = (velocidade-80) *7
     print(' EU AVISEI MANO,TOMOU A MULTA DE DE R${:.2f}!'.format(multa))
 else:
     print('ai sim,ta andando com segurança e sem risco de tomar uma multa')
