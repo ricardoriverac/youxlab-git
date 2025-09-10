0888dc9182d356d50f09c89d35cc9473a0706299
oigorromao <igor.faustino-youxlab@institutoyoux.org.br>
2025-09-10 16:47:13 -0300

    menage

---
+++ desafios/exercicio23.py
@@ -0,0 +1,7 @@
+num = (input('Digite um numero: '))
+n = str (num)
+print ('Analisando o número {}'.format(num))
+print ('Unidade: {}'.format(num[3]))
+print ('Dezena: {}'.format(num[2]))
+print ('Centena: {}'.format(num[1]))
+print ('Milhar:{}'.format(num[0]))
---
+++ desafios/exercicio24.py
@@ -0,0 +1,3 @@
+cidade = input ('Digite o nome da cidade: ').upper()
+print (cidade)
+print (cidade[0:5] == 'SANTO')
