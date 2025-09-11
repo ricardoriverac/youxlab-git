c6f61ecfdcfe3a0b175583ac4b150094b4bc668d
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-09-11 13:41:51 -0300

    Eu terminei o desafio 34

---
+++ pythonfile/desafios/d0034.py
@@ -0,0 +1,7 @@
+salario = float(input('Qual é o seu salario? : '))
+aumento = salario  +125
+aumento_15 = salario +187.7
+if salario <1250:
+    print(f'O seu aumento é de {aumento}')
+else:
+    print(f'O seu aumento é de {aumento_15}')
