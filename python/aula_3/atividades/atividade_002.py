31ddd826580c4f98206de4af36a6bc91c8b04919
desafio_python_git <ottofillipi185@gmail.com>
2025-09-13 19:12:11 -0300

    Estudando_python

--- python/aula_3/atividades/atividade_002.py
+++
@@ -1,9 +0,0 @@
-#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua media 
-
-#Responda 
-
-nota1 = int(input('Digite a primeira nota : '))
-nota2 = int(input('Digite a segunda nota : '))
-media = (nota1 + nota2) * 1
-
-print(f'A media do aluno é : {media} ')
