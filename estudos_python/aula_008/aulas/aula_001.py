1157da97f6e98a64ac5dc8c592a416f4ed2f2079
desafio_python_git <ottofillipi185@gmail.com>
2025-09-13 19:07:54 -0300

    Como baixar e utilizar bibliotecas

---
+++ estudos_python/aula_008/aulas/aula_001.py
@@ -0,0 +1,18 @@
+#Bibliotecas e suas funções
+
+'''
+math
+     ceil = arredonda um número para cima 
+     floor = arredonda um número para baixo
+     trunc = ele elimina todos os números depois da virgula sem fazer arredondamentos
+     pow = tem a mesma funcionalidade dos "**" 
+     sqrt = serve para caucular a rais quadrada 
+     factorial = faz um cauculo fatorial 
+'''
+
+'''
+random
+       random = mostra números quebrados aleatorios entre 0 e 1 
+       randit = mostra númeors aleatorios que o usuario determinou
+
+'''
