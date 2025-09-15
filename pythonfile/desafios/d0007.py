aa2e6725dc295d3961c16d09203cc7b90cd6a045
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-09-15 14:00:13 -0300

    Eu adicionei cores no meu codigo do desafio 07

--- pythonfile/desafios/d0007.py
+++ pythonfile/desafios/d0007.py
@@ -1,4 +1,4 @@
-nota= int(input('Digite sua nota'))
-nota2= int(input('Digite a segunda nota'))
+nota= int(input('\033[4;31;41m Digite sua nota \033[4;31;41m'))
+nota2= int(input('\033[4;31;41m Digite a segunda nota \033[4;31;41m'))
 media= (nota + nota2) /2 
-print('A sua media é ', media)
+print('\033[4;31;41mA sua media é \033[4;31;41m', media)
