afc5119123bb3be0e392e81546bb3ae47905f257
agathae <agatha.silva-youxlab@institutoyoux.org.br>
2025-09-15 16:16:03 -0300

    exercicio 45 Pedra,Papel e Tesoura

---
+++ python2/ex045.py
@@ -0,0 +1,19 @@
+import random
+print('VAMOS JOGAR PEDRA PAPEL E TESOURA!')
+jogador = str(input('Pedra, Papel ou Tesoura?')).lower()
+computador = random.choice(['Pedra','Papel','Tesoura']).lower()
+print(f'Voce escolheu {jogador}')
+print(f' O computador escolheu {computador}')
+if jogador == computador:
+    print("Empate!")
+elif (jogador == "pedra" and computador == "tesoura") :
+    print('Voce ganhou!')
+elif (jogador == "papel" and computador == "pedra"):
+    print('Voce ganhou!')
+elif(jogador == "tesoura" and computador == "papel"):
+    print("Você ganhou!")
+else:
+    print("Computador ganhou!")
+
+
+
