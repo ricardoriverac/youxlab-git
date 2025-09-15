efe177e19c22cba7d8676bb6c80e533488bd72fa
Youx <heronherin10@gmail.com>
2025-09-15 13:51:44 -0300

    refiz o exercício 6,que é um código que calcula o dobro triplo e a raiz quadrada de qualquer número

---
+++ pythonteste001.py/ex006.py
@@ -0,0 +1,5 @@
+numero=input('Digite um número: ')
+d=int(numero)*2
+t=int(numero)*3
+r=int(numero)**(1/2)
+print('O dobro de {} é {},\n o triplo é {} \n e a raiz quadrada é {:.2f}'.format(numero, d, t, r))
