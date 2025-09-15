ede5c190a30a04e93ed63cfbb4da4c5941c1700d
Youx <heronherin10@gmail.com>
2025-09-15 15:50:10 -0300

    criei um código que mostra qual numero ocupa a unidade,dezena,centena e milhar,de qualquer numero entre 0 e 9999

---
+++ ex024.py
@@ -0,0 +1,10 @@
+numero=int(input("informe um número: "))
+u=numero // 1 % 10
+d=numero // 10 % 10
+c=numero // 100 % 10
+m=numero // 1000 % 10
+print('analisando o numero {}'.format(numero))
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
