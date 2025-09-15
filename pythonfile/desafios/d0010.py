50c5d229ec0428bcfc8b2e1e0de62f38ed346dee
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-09-15 14:09:42 -0300

    Eu adicionei cores no meu codigo do desafio 010.

--- pythonfile/desafios/d0010.py
+++ pythonfile/desafios/d0010.py
@@ -1,4 +1,4 @@
-carteira= float(input('Quanto voce tem na carteira?'))
+carteira= float(input('\033[33;1m Quanto voce tem na carteira?\033[33;1m'))
 dólar= 3.27
 conversao= carteira/dólar
-print(f'Voce tem o valor de:{conversao:.2f} dólares')
+print(f'\033[32;1mVoce tem o valor de:{conversao:.2f} dólares\033[32;1m')
