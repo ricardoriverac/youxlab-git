de12897a3fbebbb405d79a87d573f1f701370126
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-09-15 14:06:23 -0500

    exercicio 17 feito

--- python/aula 08/017.py
+++ python/aula 08/017.py
@@ -1,4 +1,4 @@
 cateto_oposto = float(input('Comprimento do cateto adjacente: '))
 cateto_adjacente = float(input('Comprimento do cateto adjacente: '))
 hipotenusa = (cateto_oposto **2 + cateto_adjacente **2) **(1/2)
-print (f"A hipotenusa vai medir {hipotenusa}")
+print (f"A hipotenusa vai medir {hipotenusa : .2f} ")
