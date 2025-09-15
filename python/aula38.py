0e8863f21c8de4db87112d3d1fddf3394418f7e8
lais <laisseabra89@gemail.com>
2025-09-15 14:50:45 -0300

    estrutura de controle

---
+++ python/aula38.py
@@ -0,0 +1,8 @@
+n1 = int(input('primeiro numero: '))
+n2 = int(input('segundo numero: '))
+if n1 > n2:
+    print('o primeiro valor e maior')
+elif n2 > n1:
+    print('o segundo valor e maior')
+elif n1 == n2:
+    print('os dois valores sao iguais5')
