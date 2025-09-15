e4b4084cb5118acd3160b06e592396b1d56d740d
Youx <isabelamilene74@gmail.com>
2025-09-15 14:09:36 -0300

    ex017.falando sobre o cateto oposto, adjacente e a hipotenusa

---
+++ python/aula017.py
@@ -0,0 +1,5 @@
+cateto_oposto = float(input('Comprimento do cateto oposto: '))
+cateto_adjacente = float(input('Comorimento do cateto adjacente:'))
+hipotenusa = (cateto_oposto ** 2 + cateto_adjacente ** 2) **(1/2)
+print(f'A hipotenusa vai medir {hipotenusa : .2f}')
+               
