74cc7284cb7526bea127b9576e7539ee07b00187
Youx <juan.ferreira-youxlab@institutoyoux.org.br>
2025-09-15 13:59:21 -0300

    diz qual número é o maior e qual é o menor entre os três digitados

---
+++ python/exercicio33.py
@@ -0,0 +1,37 @@
+
+numero2 = int(input('Digite um segundo número: '))
+numero3 = int(input('Digite um terceiro número: '))
+
+
+
+
+if numero1 > numero2 and numero3:
+    print ('o número {} é o meior número dos três' .format(numero1))
+else:
+    print ('nad')
+if numero2 > numero1 and numero3:
+    print ('o número {} é o maior número dos três' .format(numero2))
+else:
+    print ()
+if numero3 > numero1 and numero2:
+        print ('o número {} é o maior número dos três' .format(numero3))
+else: 
+     print()
+
+
+
+
+
+
+if numero1 < numero2 and numero3:
+     print ('o numero {} é o menor número dos três'.format(numero1))
+else:
+     print()
+if numero2 < numero1 and numero3:
+     print('o numero {} é o menor número dos três'.format(numero2))
+else:
+     print()
+if numero3 < numero1 and numero2:
+     print('o numero {} é o menor número dos três'.format(numero3))
+else:
+     print()
