4e42b06814cc525166bb8b1a692728c13d91ce06
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-15 15:12:13 -0300

    Correção Exercício 41

--- estudos-python/Exercicios-feitos/Aula_12/Exercicio41.py
+++ estudos-python/Exercicios-feitos/Aula_12/Exercicio41.py
@@ -2,13 +2,13 @@ from datetime import date
 yearOfBirth = int(input('Enter your year of birth: '))
 actualYear = (date.today().year)
 yourAge = actualYear - yearOfBirth
-if 0<=yourAge<=9:
+if 0<=yourAge<=8:
     print ('You are a MIRIM!')
-elif 10<=yourAge<=14:
+elif 9<=yourAge<=13:
     print ('You are a INFANTIL!')
-elif 15<=yourAge<=19:
+elif 14<=yourAge<=18:
     print ('You are a JUNIOR!')
-elif 20<=yourAge<=21:
+elif 19<=yourAge<=20:
     print ('You are a SENIOR!')
 elif 21<yourAge:
     print ('You are a MASTER!')
