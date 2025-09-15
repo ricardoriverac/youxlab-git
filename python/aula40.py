be83e164b91a48ef8cf6f233aca01e71c2c2f562
lais <laisseabra89@gemail.com>
2025-09-15 15:13:50 -0300

    classico media

---
+++ python/aula40.py
@@ -0,0 +1,6 @@
+n1 = float(input('primeira nota: '))
+n2 = float(input('segunda nota: '))
+media = (n1 + n2) /2
+print('tirando {} e {}, a media do aluno e {}'.format(n1, n2, media))
+if 7 > media >= 5:
+    print('RECUPERACAO')
