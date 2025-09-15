3004e4eb74aaab800b226af44a6948412b26e03e
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-09-15 14:02:15 -0300

    Eu adicionei cores no meu código do desafio 08

--- pythonfile/desafios/d0008.py
+++ pythonfile/desafios/d0008.py
@@ -1,4 +1,4 @@
 medida = float(input('Uma distancia em metros'))
 cm = medida *100
 mm = medida *1000
-print('A medida de {}m corresponde a {}cm e {}mm .'.format(medida, cm, mm))
+print('\033[1;30;47m A medida de {}m corresponde a {}cm e {}mm .\033[1;30;47m'.format(medida, cm, mm))
