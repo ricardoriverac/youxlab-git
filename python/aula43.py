2278617c01ccda7623eda493c3c04b381edcf9ef
lais <laisseabra89@gemail.com>
2025-09-15 23:27:29 -0300

    indice de massa corporal

---
+++ python/aula43.py
@@ -0,0 +1,12 @@
+peso = float(input("Digite o seu peso em kg: "))
+altura = float(input("Digite a sua altura em metros: "))
+imc = peso / (altura ** 2)
+print(f"Seu IMC é: {imc:.2f}")
+if imc < 18.5:
+    print("Classificação: Abaixo do peso")
+elif 18.5 <= imc < 25:
+    print("Classificação: Peso normal")
+elif 25 <= imc < 30:
+    print("Classificação: Sobrepeso")
+else:  
+    print("Classificação: Obesidade")
