32e8304e040a5e8ff5d6d7db6d4ba549f52bfafc
João Othávio <joaoothaviofabri14@gmail.com>
2025-09-16 21:05:23 -0300

    Feito a prática da vídeo aula 011 de python

---
+++ curso-de-python/Módulo-1/exercicios/ex035.py
@@ -0,0 +1,3 @@
+reta1 = float(input('Digite o tamanho da primeira reta: '))
+reta2 = float(input('Digite o tamanho da segunda reta: '))
+reta3 = float(input('Digite o tamanho da terceira reta: '))
---
+++ curso-de-python/Módulo-1/videos-cursos/prática/curso-python-011/app4.py
@@ -0,0 +1,6 @@
+nome = 'João Othávio'
+cores = {'limpa':'\033[m', 
+        'azul':'033[34m',
+        'amarelo': '\033[33m', 
+        'pretoebranco':'\033[7;37m'}
+print('Olá! Muito prazer em te conhecer {}{}{}!!'.format(cores['pretoebranco'] , nome , cores['limpa']))
