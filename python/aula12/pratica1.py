b801591c22fed32f432b3dfd8506d0a80dd6438b
Lucas RibYouX <lucas.pereira-youxlab@institutoyoux.org.br>
2025-09-17 19:01:33 -0300

    Adicionado a prática 1 do vídeo 9 do Guanabara

---
+++ python/aula12/pratica1.py
@@ -0,0 +1,7 @@
+nome = str(input('Digite seu nome: '))
+if nome == 'Lucas':
+    print('que nome bonito')
+elif nome == 'Igor' or nome == 'Mary' or nome == 'Bruna':
+    print('Seu nome é bem popular')
+else:
+    print('Dislike')
