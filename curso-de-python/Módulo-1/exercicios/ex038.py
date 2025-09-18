9e787cda25e05041a1939f738fcccae5626dc77a
João Othávio <joaoothaviofabri14@gmail.com>
2025-09-18 16:15:05 -0300

    Exercício 038 feito

--- curso-de-python/Módulo-1/exercicios/ex038.py
+++ curso-de-python/Módulo-1/exercicios/ex038.py
@@ -1,1 +1,8 @@
-valor1 = int(input(Digite um número ))
+valor1 = int(input('Digite um número: '))
+valor2 = int(input('Digite outro número: '))
+if valor1 > valor2:
+    print(f'O número {valor1} é maior!')
+elif valor1 < valor2:
+    print(f'O número {valor2} é maior!')
+else:
+    print('Não há um número maior que o outro, os dois são iguais.')
