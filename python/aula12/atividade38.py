164d1f4a0364ff64245d691cf722187b676f070b
Lucas RibYouX <lucas.pereira-youxlab@institutoyoux.org.br>
2025-09-18 13:59:11 -0300

    adicionando atividade 38 sobre comparação de números

---
+++ python/aula12/atividade38.py
@@ -0,0 +1,9 @@
+NumeroUm = int(input('Digite um número: '))
+NumeroDois = int(input('Digite outro número: '))
+
+if NumeroUm > NumeroDois:
+    print('O primeiro valor é maior')
+elif NumeroDois > NumeroUm:
+    print('O segundo valor é maior')
+else:
+    print('Os valores são iguais')
