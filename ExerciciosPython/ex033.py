c989fbe5a1d7e26abbcbe670814d94e06c7cd476
juliaassis00 <juliaass92@gmail.com>
2025-09-18 13:56:32 -0300

    Salvado exercício 33

--- ExerciciosPython/ex033.py
+++ ExerciciosPython/ex033.py
@@ -1,8 +1,9 @@
-# Pede ao usuário para inserir um ano
-ano = int(input("Por favor, digite um ano: "))
-
-# Verifica a lógica do ano bissexto
-if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
-    print(f"O ano {ano} é bissexto.")
-else:
-    print(f"O ano {ano} não é bissexto.")
+numero1 = int(input('Digite o primeiro número: '))
+numero2 = int(input('Digite o segundo número: '))
+numero3 = int(input('Digite o terceiro número: '))
+numeros = [numero1, numero2, numero3]
+menorNumero = min(numeros)
+maiorNumero = max(numeros)
+print(f"\nOs números digitados foram: {numero1}, {numero2}, {numero3}")
+print(f"O menor número é: {menorNumero}")
+print(f"O maior número é: {maiorNumero}")
