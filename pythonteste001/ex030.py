a153bcc37a95b3cb5ff54d0c1d949df5348f563a
Youx <heronherin10@gmail.com>
2025-09-18 17:12:39 -0300

    fiz um prograa que diz se o numero fornecido é par ou impar

---
+++ pythonteste001/ex030.py
@@ -0,0 +1,6 @@
+numero=int(input('me diga um número qualquer: '))
+resultado=numero%2
+if resultado==0:
+    print('o numero {} é par'.format(numero))
+else:
+    print('o numero {} é impar'.format(numero))
