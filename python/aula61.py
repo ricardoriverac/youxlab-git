3e659bc8e35593634d5ccb1258a88919beb8516d
lais <laisseabra89@gemail.com>
2025-09-18 17:02:52 -0300

    progressao aritmetica v2.0

---
+++ python/aula61.py
@@ -0,0 +1,11 @@
+print('gerador de PA')
+print('-=' * 10)
+primeiro = int(input('primeiro termo: '))
+razao = int(input('razao da PA: '))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print(' {}'.format(termo), end='')
+    termo += razao
+    cont += 1
+    print('FIM')
