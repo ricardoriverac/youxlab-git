144c892884a6a6e6a414aa7c039ec50645fdf892
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-09-18 15:13:52 -0500

    ex022 feito

---
+++ python/aula_09/ex022.py
@@ -0,0 +1,6 @@
+nome = str(input('Digite seu nome completo: '))
+print('Analisando seu nome, pfvr aguarde...')
+print('Seu nome em maiúsculas è {}'.format(nome.upper()))
+print('Seu nome em minùsculas è {}'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras'.format(len(nome)))
+print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
