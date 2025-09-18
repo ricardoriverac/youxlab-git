61b122f4bc7c9a0684de2cd2493e3de972b7a687
agathae <agatha.silva-youxlab@institutoyoux.org.br>
2025-09-18 17:35:43 -0300

    exercicio 55 maior e menor da sequencia

---
+++ python2/ex055.py
@@ -0,0 +1,11 @@
+menorpeso = 1000
+maiorpeso = 0
+for c in range (0,5):
+    peso = float(input('Digite seu peso: '))
+    if peso > maiorpeso:
+        maiorpeso = peso
+    if peso < menorpeso:
+        menorpeso = peso
+print(f'O maior peso é {maiorpeso}\n e o menor peso é {menorpeso}')
+
+
