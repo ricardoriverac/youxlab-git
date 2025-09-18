f8eb2b32dc485c21b5830752ab7dc8d72cec643b
Youx <heronherin10@gmail.com>
2025-09-18 14:26:34 -0300

    fiz um código que mostra qual é a primeira e última palavras no seu nome

---
+++ pythonteste001.py/ex027.py
@@ -0,0 +1,5 @@
+n=str(input('digite seu nome completo ')).strip()
+nome=n.split()
+print("muito prazer em te conhecer!")
+print('seu primeiro nome é {}'.format(nome[0]))
+print('seu ultimo nome é {}'.format(nome[len(nome)-1]))
