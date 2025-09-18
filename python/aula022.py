3e5cd2f37a01ed5328c61455f68a6fb8762a6a4b
Youx <isabelamilene74@gmail.com>
2025-09-18 13:15:11 -0300

    Ordem de apresentação de cada aluno

---
+++ python/aula022.py
@@ -0,0 +1,8 @@
+nome = str(input('Digite seu nome completo pfvr: '))
+print('Analisando seu nome, pfvr aguarde...')
+print('Seu nome em maiúsculas è {}'.format(nome.upper()))
+print('Seu nome em minùsculas è {}'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras'.format(len(nome)))
+print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
+
+
