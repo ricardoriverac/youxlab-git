bf1a01f14ac27ae4ae89f747e1b44e95a97aef9f
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-18 14:01:26 -0300

    Melhora no código do exercício 45

--- estudos-python/Exercicios-feitos/Aula_12/Exercicio45.py
+++ estudos-python/Exercicios-feitos/Aula_12/Exercicio45.py
@@ -3,8 +3,8 @@ import random
 yourChoice = int(input('Make a choice:\n1-paper\n2-rock\n3-scissors\nYour choice is: '))
 machineChoice = random.randint(1,3)
 if yourChoice == 1 and machineChoice == 1 or yourChoice == 2 and machineChoice == 2 or yourChoice == 3 and machineChoice == 3:
-    print ('It is a tie!!!')
+    print (f'You choose {yourChoice} and machine choose {machineChoice}\nIt is a tie!!!')
 elif yourChoice == 1 and machineChoice == 2 or yourChoice == 2 and machineChoice == 3 or yourChoice == 3 and machineChoice == 1:
-    print ('You win!!!')
+    print (f'You choose {yourChoice} and machine choose {machineChoice}\nYou win!!!')
 elif machineChoice == 1 and yourChoice == 2 or machineChoice == 2 and yourChoice == 3 or machineChoice == 3 and yourChoice == 1:
-    print ('You lose!!!')
+    print (f'You choose {yourChoice} and machine choose {machineChoice}\nYou choose You lose!!!')
