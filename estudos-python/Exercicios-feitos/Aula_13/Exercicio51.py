29daa23e254d12a0ee64720afd913f87030d09eb
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-19 13:25:59 -0300

    Exercício 51

---
+++ estudos-python/Exercicios-feitos/Aula_13/Exercicio51.py
@@ -0,0 +1,6 @@
+firstOperator = int(input('Choose a number: '))
+numberToBeAdded = int(input('Choose a multiplicator: '))
+for cont in range(0, 10, 1):
+    print (firstOperator)
+    firstOperator += numberToBeAdded
+print ('FIM')
