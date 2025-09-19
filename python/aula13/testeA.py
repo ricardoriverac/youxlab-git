03575561da2f05cd3ad7956a427d1effb70f8eca
Lucas RibYouX <lucas.pereira-youxlab@institutoyoux.org.br>
2025-09-19 16:18:23 -0300

    testes + exercicio46 contagfem regressiva

---
+++ python/aula13/testeA.py
@@ -0,0 +1,8 @@
+print('-=-' * 8)
+i = int(input('Digite o começo: '))
+n = int(input('Digite um número: '))
+p = int(input('Digite o passo: '))
+print('-=-' * 4, 'resultados', '-=-' * 4)
+for c in range(i, n+1, p):
+    print(c)
+print('-=-' * 4, 'resultados', '-=-' * 4) 
