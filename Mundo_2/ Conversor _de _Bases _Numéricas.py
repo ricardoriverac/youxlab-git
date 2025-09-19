0bee849e634f098f3475c3492e411cad1bc4fd56
GiovaneSilvaxss <giovane.silva-youxlab@institutoyoux.org.br>
2025-09-19 16:13:17 -0300

    Convertendo numeros

--- Mundo_2/ Conversor _de _Bases _Numéricas.py
+++ Mundo_2/ Conversor _de _Bases _Numéricas.py
@@ -1,7 +1,10 @@
 numero = int(input('Digite um  numero anterior:'))
-print('Escolha uma das bases para conversão:'
+print('Escolha uma das bases para conversão:')
 [1]Binario
 [2]Octal
 [3]Hexadecimal
-opção = int(innput('Sua opção')
-    if opção == 1:
+opção = int'(input('Sua opção')
+if opção == 1:
+    print('{}convertido para BINARIO é igual a {}'.format(numero,bin(numero))
+    print('{}convertido para OCTAL é igual a {}'.format(numero,octal(numero)))
+    print('{}convertido para HEXADECIMAL é igual a {}'.format(numero,hexadecimal(numero)
