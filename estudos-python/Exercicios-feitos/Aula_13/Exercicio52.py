7ace36b166e0c514ac9f47ac65b804c3f2faa561
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-19 14:51:09 -0300

    Exercício 52

---
+++ estudos-python/Exercicios-feitos/Aula_13/Exercicio52.py
@@ -0,0 +1,13 @@
+numero = int(input('Escolha um número: '))
+vezesQueFoiDividido = 0
+for contagem in range (1, numero+1, 1):
+    if numero % contagem == 0:
+        print (f'\033[33m {contagem} \033[')
+        vezesQueFoiDividido += 1
+    else:
+        print (f'\033[31m {contagem}\033[')
+print (f'WEle foi dividido {vezesQueFoiDividido} vezes')
+if vezesQueFoiDividido == 2:
+    print ('\033[32m Ele é um número primo!!!\033[32m')
+else:
+    print ('\033[31m Ele não é primo!!!\033[31m')
