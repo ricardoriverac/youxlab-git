7193e46aab0c028215d6f9d4ae9c6ba77e379bdb
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-09-19 16:21:03 -0500

    ex033 feito

---
+++ python/aula_10/ex033.py
@@ -0,0 +1,6 @@
+numero1 = int(input('Qual é o primeiro numero? '))
+numero2 = int(input('Qual é o segundo numero? '))
+numero3 = int(input('Qual é o terceiro numero? '))
+numeros = [numero1, numero2, numero3]
+print('O maior valor digitado foi {}'.format(max(numeros)))
+print('O menor numero digitado foi {}'.format(min(numeros)))
