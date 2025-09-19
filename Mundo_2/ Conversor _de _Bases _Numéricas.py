6d22e6ae72b9b535894c5ef7fd00fbccb96a3534
GiovaneSilvaxss <giovane.silva-youxlab@institutoyoux.org.br>
2025-09-19 14:56:07 -0300

    Commit no desafio

---
+++ Mundo_2/ Conversor _de _Bases _Numéricas.py
@@ -0,0 +1,7 @@
+numero = int(input('Digite um  numero anterior:'))
+print('Escolha uma das bases para conversão:'
+[1]Binario
+[2]Octal
+[3]Hexadecimal
+opção = int(innput('Sua opção')
+    if opção == 1:
