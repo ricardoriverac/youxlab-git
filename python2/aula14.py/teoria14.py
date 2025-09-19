516ce66ca2b33dc9079056f48969653056b4254a
agathae <agatha.silva-youxlab@institutoyoux.org.br>
2025-09-19 16:13:51 -0300

    teoria/anotacaoes da aula 14

---
+++ python2/aula14.py/teoria14.py
@@ -0,0 +1,10 @@
+# while vai executar repetidamente um bloco de código enquanto uma condição especificada for verdadeira
+#c = 1
+#while c <10: #enquanto c for menor que 10,vai se repetir 
+   # print(c)
+  #  c += 1
+#print('FIM')
+n = 1
+while n != 0:#estou dando uma condiçao
+    n = int(input('Digite um valor: '))
+print('FIM')
