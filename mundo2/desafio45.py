65d84ec8a562b161e254802d7eb4cb6ea2e6c2f8
Youx <deivs.abreu-youxlab@institutoyoux.org.br>
2025-09-19 13:37:26 -0300

    desafio46

--- mundo2/desafio45.py
+++ mundo2/desafio45.py
@@ -1,11 +1,12 @@
 import random
 print ("VAMOS JOGAR PEDRA, PAPEL E TESOURA!")
-a = int(input("Considere:\n1 = PEDRA\n2 = PAPEL\n3 = TESOURA\nAgora, digite sua escolha: "))
-b = random.randint(1,3)
-print (b)
-if a == b:
+jogador = int(input("Considere:\n1 = PEDRA\n2 = PAPEL\n3 = TESOURA\nAgora, digite sua escolha: "))
+bot = random.randint('pedra', 'tesoura', 'papel')
+print (bot)
+if jogador == bot:
     print ("EMPATE")
-elif (a == 1 and b == 2) or (a == 2 and b == 3) or (a == 3 and b == 1):
+elif (jogador == 'pedra' and bot == 'tesoura') or (jogador == 'tesoura 'and bot == 'papel') or (jogador == 'papel' and bot == 'pedra'):
     print ("VOCÊ PERDEU!")
 else:
     print ("VOCÊ GANHOU")
+bot=print()
