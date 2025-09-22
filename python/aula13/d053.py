72988b2a0a4f98640b405c809f66b634fcc5d2a9
luis2025git <luis.sousa-youxlab@institutoyoux.org.br>
2025-09-22 19:49:23 -0300

    exercicio 53 (nome do commit anterior ERRADO)

--- python/aula13/d053.py
+++ python/aula13/d053.py
@@ -1,4 +1,4 @@
-frase = str(input('Digite uma frase: ')).strip().upper()
+frases = str(input('Digite uma frase: ')).strip().upper()
 palavras = frase.split()
 junto = ''.join(palavras)
 inverso = ''
