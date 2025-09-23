539870210b6bf083bc888ee70c207d1c4fa65f42
Youx <isabelamilene74@gmail.com>
2025-09-23 14:26:02 -0300

    ex036 Nome popular teste

---
+++ python/aula 12/teoria.py
@@ -0,0 +1,8 @@
+nome = str(input('Qual é seu nome? '))
+if nome == 'Gustavo':
+    print('Que nome mais bonito! ')
+elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo': 
+    print('Seu nome é bem popular no Brasil.')
+else:
+    print('Seu nome é bem normal.')
+print('Tenha um bom dia, {}!'.format(nome)) 
