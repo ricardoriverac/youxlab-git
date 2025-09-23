77309f16d808a525f5568ca009fb57e7df2896cc
Youx <isabelamilene74@gmail.com>
2025-09-23 13:21:30 -0300

    ex032 Não bissexto, é bissexto

---
+++ python/aula011/teoria.py
@@ -0,0 +1,6 @@
+print("Olá, mundo") 
+nome = input( 'qual é seu nome?' )
+idade  = input( 'qual é a sua idade?' )
+peso = input ( 'qual é seu peso?' )
+print(nome, idade, peso )
+nome = input('qual')
