103842e6248d41a84a8cc66fee01b2af5a05dc56
GiovaneSilvaxss <giovane.silva-youxlab@institutoyoux.org.br>
2025-09-23 10:24:04 -0300

    Alistamento militar,saber quanto tempo que falta para se alistar

---
+++ Mundo_2/Alistamento_Militar.py
@@ -0,0 +1,13 @@
+from datetime import date 
+atual = date.today().year
+nascimento = int(input('Ano de nascimento:'))
+idade = atual - nascimento
+print('Quem nasceu em {} tem {} anos em {}'.format(nascimento, idade, atual))
+if idade == 18:
+    print('Voce tem que se alistar!')
+elif idade < 18:
+    saldo = 18 - idade
+    print('Ainda falta {} anos para o alistamento'.format(saldo))
+elif idade > 18:
+    saldo = idade - 18
+    print('Voce ja deveria ter sido alistado há {} anos'.format(saldo))
