229ad3a58d3f5f646d5219878bf64d8209aa9aaa
Youx <isabelamilene74@gmail.com>
2025-09-23 13:13:26 -0300

    ex034 o aumento no salario será, o salario do novo funcionario será

---
+++ python/ex035.py
@@ -0,0 +1,7 @@
+primeiro = float(input('Digite um valor: '))
+segundo = float(input('Digite um valor: '))
+terceiro = float(input('Digite um valor: '))
+if primeiro - segundo < terceiro < primeiro + terceiro:
+    print('É possivel formar um triângulo')
+else: 
+    print('Não é possivel formar um triângulo')
