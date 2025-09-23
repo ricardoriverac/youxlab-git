ad2df04f115698366ad92b420290ee6e74da9466
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-09-23 14:12:34 -0300

    Eu terminei o desafio 56 do mundo 2

---
+++ pythonfile/mundo2/desafios_mundo2/d0056.py
@@ -0,0 +1,22 @@
+somaidade = 0
+mediaidade = 0
+maioridadehomem = 0
+nomevelho = ''
+totalmulher = 0
+for p in range(1, 5):
+    nome =str(input('Digite seu nome: ')).strip()
+    idade = int(input('Digite sua idade'))
+    sexo = str(input('Sexo [M/F]: ')).strip()
+    somaidade += idade
+    if p == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Mm' and  idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totalmulher += 1
+mediaidade = somaidade / 4 
+print(f'A media de idade do grupo é de {mediaidade}')
+print(f'O Homem mais velho do grupo tem {maioridadehomem} e se chama {nomevelho} ')
+print(f'Ao todo são {totalmulher} com menos de 20 anos')
