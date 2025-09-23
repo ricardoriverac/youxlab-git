05e8f1c3948c3a2a11dc6daf2c4d5331de7c7713
pedro-couto1-git <pedrocouto.profissional@gmail.com>
2025-09-23 13:13:46 -0300

    Desafio 57- Validação de dados

---
+++ Estudos/python2/aula 14/desafio57.py
@@ -0,0 +1,5 @@
+PerguntaSexualidade= str(input('Qual sua sexualidade? [M ou F]')).upper().strip()
+while PerguntaSexualidade not in 'MF':
+    print('Por favor redigite seu sexo novamente! [M ou F]')
+    print(str(input('Qual sua sexualidade? [M ou F]')))
+print(f'Sexo {PerguntaSexualidade} registrado com sucesso')
