25b2614e8a66754b2605110bbf1e08e8ea4c6450
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-09-22 19:36:34 -0500

    aula 11 concluida

---
+++ python/aula_11/pratica.py
@@ -0,0 +1,37 @@
+# cor do texto (vermelho)
+print('\033[0;31mOlá, mundo!')
+
+# vermelho com fundo amarelo 
+print('\033[0;31;43mOlá, mundo!')
+
+# vermelho com fundo amarelo em negrito 
+print('\033[1;31;43mOlá, mundo!')
+
+# diminuir o espaço da cor de fundo - não foi
+print('\033[1;31;43mOlá, mundo!\033[m') 
+
+# Código  Estilo
+0       None 
+1       Bold 
+4       Underline
+7       Negative
+
+# cor do texto
+30 - Preto
+31 - Vermelho
+32 - Verde
+33 - Amarelo
+34 - Azul
+35 - Magenta
+36 - Ciano
+37 - Cinza claro
+
+# fundo do texto 
+40 - Preto
+41 - Vermelho
+42 - Verde
+43 - Amarelo
+44 - Azul
+45 - Magenta
+46 - Ciano
+47 - Cinza claro
