9bd62598c930b57762ad8ec1c966cf84bf546d5a
Youx <isabelamilene74@gmail.com>
2025-09-23 14:27:54 -0300

    teste

--- python/aula 11/teste.py
+++ python/aula 11/teste.py
@@ -10,31 +10,31 @@ print('\033[1;31;43mOlá, mundo!')
 # diminuir o espaço da cor de fundo - não foi
 print('\033[1;31;43mOlá, mundo!\033[m') 
 
-# Código  Estilo
-0 'None' 
-1 'Bold' 
-4 'Underline'
-7 'Negative'
+# # Código  Estilo
+# 0 'None' 
+# 1 'Bold' 
+# 4 'Underline'
+# 7 'Negative'
 
 # cor do texto
-30 - Preto
-31 - Vermelho
-32 - Verde
-33 - Amarelo
-34 - Azul
-35 - Magenta
-36 - Ciano
-37 - Cinza 
+# 30 - Preto
+# 31 - Vermelho
+# 32 - Verde
+# 33 - Amarelo
+# 34 - Azul
+# 35 - Magenta
+# 36 - Ciano
+# 37 - Cinza 
 
-# fundo do texto 
-40 - Preto
-41 - Vermelho
-42 - Verde
-43 - Amarelo
-44 - Azul
-45 - Magenta
-46 - Ciano
-47 - Cinza 
+# # fundo do texto 
+# 40 - Preto
+# 41 - Vermelho
+# 42 - Verde
+# 43 - Amarelo
+# 44 - Azul
+# 45 - Magenta
+# 46 - Ciano
+# 47 - Cinza 
 
 # duas ou mais variaveis 
 a = 3
