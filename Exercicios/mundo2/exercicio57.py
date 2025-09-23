1e17c0d1d0f52cc55d2e1dbba42e2f8262aa4fd4
daniella <daniella.flor-youxlab@institutoyoux.org.br>
2025-09-23 14:28:52 -0300

    exercicio que repete caso a resposta nao esteja correta

---
+++ Exercicios/mundo2/exercicio57.py
@@ -0,0 +1,4 @@
+sexo=str(input('Digite seu gênero: ')).upper()
+while (sexo != "M" and sexo != "F"):
+    print("Digite novamente utilizando [F ou M]: ")
+    sexo=input("Digite seu gênero: ").upper()
