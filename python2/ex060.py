d4e8e7a0fa15820efe3fa045f61d3fd65a297317
agathae <agatha.silva-youxlab@institutoyoux.org.br>
2025-09-23 14:27:59 -0300

    exercicio 60 calculo fatorial

---
+++ python2/ex060.py
@@ -0,0 +1,7 @@
+from math import factorial
+numero = int(input('Digite um numero '))
+fatorial = factorial(numero)
+while numero < 0:
+    fatorial *= numero
+    numero -= 1
+print(f'O fatorial de {numero} é {fatorial} ')
