cc5ac7245c550b57202ade3d584433217140efab
Youx <isabelamilene74@gmail.com>
2025-09-23 13:38:45 -0300

    ex035 Formando um triângulo

--- python/ex035.py
+++ python/ex035.py
@@ -2,6 +2,6 @@ primeiro = float(input('Digite um valor: '))
 segundo = float(input('Digite um valor: '))
 terceiro = float(input('Digite um valor: '))
 if primeiro - segundo < terceiro < primeiro + terceiro:
-    print('É possivel formar um triângulo')
+    print('É possivel formar um triângulo ')
 else: 
-    print('Não é possivel formar um triângulo')
+    print('Não é possivel formar um triângulo ')
