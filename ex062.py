d0286c593847451d0f53788ac741d3c196477a6b
agathae <agatha.silva-youxlab@institutoyoux.org.br>
2025-09-23 16:24:17 -0300

    exercicio 62 progressao aritmetica

---
+++ ex062.py
@@ -0,0 +1,16 @@
+
+primeiroTermo = int(input('Digite o primeiro termo da PA: '))
+razao = int(input('De quanto em quanto está indo: '))
+termoAtual = primeiroTermo
+contar = 1
+total = 0
+mais = 10
+while mais != 0:
+    total = total + mais
+    while contar <= total:
+        print(f'{termoAtual} -> ', end='')
+        termoAtual = termoAtual + razao 
+        contar += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+print(f'Progressão finalizada com {total} termos mostrados.')
