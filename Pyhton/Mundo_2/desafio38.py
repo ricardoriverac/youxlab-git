d9b94d8f83a1a2bd9990c32024ba10f3c0c41d62
Emily <emily.ferreira-youxlab@institutoyoux.org.br>
2025-09-23 13:21:19 -0300

    desafio 38

---
+++ Pyhton/Mundo_2/desafio38.py
@@ -0,0 +1,8 @@
+numero1 = int(input('Digite o primeiro número: '))
+numero2 = int(input('Digite o segundo número: '))
+if numero1 > numero2:
+    print('O primeiro número é maior!')
+elif numero1 < numero2:
+    print('O segundo número é o maior!')
+else:
+    print('Não existe número maior, os dois são iguais!')
