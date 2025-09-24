c060ada96eb27b875ccb6a97a05b8336f72e3d48
Youx <isabelamilene74@gmail.com>
2025-09-24 14:07:17 -0300

    ex042 escaleno, equilátero e isósceles

--- python/ex041.py
+++ python/ex041.py
@@ -1,3 +1,15 @@
 from datetime import date 
 atual = date.today().year 
-nascimento = int(input())
+nascimento = int(input('Digite o ano que vc Nasceu: '))
+idade = atual - nascimento
+print('O atleta tem {} anos'.format(idade))
+if idade <= 9:
+    print('Classificação: mirim')
+elif idade <=14:
+    print('Classificação; infantil')
+elif idade <= 19:
+    print('Classificação: junior')
+elif idade <= 25:
+    print('Classificação: sênior')
+else:
+    print('Classificação: master')
