90fc560580f5a8f425c7f8ba4a58e92b79ef51dc
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-24 17:47:42 -0300

    Melhoria no código do Exercício 58

--- estudos-python/Exercicios-feitos/Aula_14/Exercicio58.py
+++ estudos-python/Exercicios-feitos/Aula_14/Exercicio58.py
@@ -1,10 +1,15 @@
 import random
-meuNumero = int(input('Tente adivinhar o número entre 0 e 10 que a máquina está pensando: '))
 numeroDaMaquina = random.randint(0,10)
 tentativas = 0
-while meuNumero != numeroDaMaquina:
-    print ('Tente denovo')
-    meuNumero = int(input('Tente adivinhar o número novamente entre 0 e 10 que a máquina está pensando: '))
-    numeroDaMaquina = random.randint(0,10)
+acertou = False
+while not acertou:
+    meuNumero = int(input('Tente adivinhar o número entre 0 e 10 que a máquina está pensando: '))
     tentativas += 1
+    if meuNumero == numeroDaMaquina:
+        acertou = True
+    else:
+        if meuNumero > numeroDaMaquina:
+            print ('Tente novamente é um número menor')
+        elif meuNumero < numeroDaMaquina:
+            print ('Tente novamente é um número maior')
 print (f'Você acertou! Parabéns teve {tentativas} tentativas!')
