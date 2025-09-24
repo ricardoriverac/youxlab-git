ea3dcf1e4f3dcb1a6bbefca1ae86e024094dff4a
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-24 15:13:31 -0300

    Melhoria no código do Exercício 56

--- estudos-python/Exercicios-feitos/Aula_13/Exercicio56.py
+++ estudos-python/Exercicios-feitos/Aula_13/Exercicio56.py
@@ -7,8 +7,11 @@ nomeHomemMaisVelho = ''
 for pessoa in range(1, 5):
     nome = input('Insira o nome da pessoa: ')
     idade = int(input('Insira a idade da pessoa: '))
-    sexo = input('Você é homem ou mulher:\nInsira H para homem ou M para mulher').strip().upper()
+    sexo = input('Você é homem ou mulher:\nInsira H para homem ou M para mulher: ').strip().upper()
     somaIdade += idade
+    print(f'O nome da pessoa é {nome}')
+    print(f'A idade dessa pessoa é {idade}')
+    print(f'O sexo dessa pessoa é {sexo}')
     if sexo == 'M':
         mulheres += 1
         if idade<20:
@@ -19,6 +22,6 @@ for pessoa in range(1, 5):
             maisVelho == idade
             nomeHomemMaisVelho = nome
 media = float(somaIdade/4)
-print(f'A média de idade do grupo é de {media} anos')
-print(f'O homem mais velho é {nomeHomemMaisVelho}')
-print(f'Tem {mulheresMaisNovas} menores de 20 anos')
+print(f'A média de idade do grupo é de {media} anos.')
+print(f'O homem mais velho é {nomeHomemMaisVelho}.')
+print(f'Tem {mulheresMaisNovas} menores de 20 anos.')
