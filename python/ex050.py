5d19b415b12852e32edef4659a1a4cec15e7375a
Youx <isabelamilene74@gmail.com>
2025-09-24 17:29:05 -0300

    ex050 somando apenas os numeros pares

--- python/ex050.py
+++ python/ex050.py
@@ -1,4 +1,8 @@
 soma = 0 
 conta = 0
 for c in range(1, 7):
-    numero = 
+    numero = int(input('Digite o {} valor: '.format(c)))
+    if numero % 2 == 0:
+        soma = soma + numero
+        conta = conta + 1
+    print('Você informou {} números  pares e a soma foi {}'.format(conta, soma))
