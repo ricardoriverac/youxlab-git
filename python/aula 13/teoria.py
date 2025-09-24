d2bdbeaadcfad1261f3dd7e0bd5e865793d6085e
Youx <isabelamilene74@gmail.com>
2025-09-24 17:11:25 -0300

    ex049 tabuada de um jeito facil

---
+++ python/aula 13/teoria.py
@@ -0,0 +1,83 @@
+for c in range(1, 6):
+    print('Oi')
+print('Fim')
+
+
+
+
+
+for c in range(1, 6):
+    print('Oi')
+    print('Fim')
+
+
+
+    for c in range(0, 6):
+        print(c)
+print('Fim')
+
+
+
+
+for c in range(1, 7):
+        print(c)
+print('Fim')
+
+
+
+for c in range(6, 0, -1):
+        print(c)
+print('Fim')
+
+
+
+
+for c in range(0, 7, 2):
+        print(c)
+print('Fim')
+
+
+
+numero = int(input('Digite um número: '))
+for c in range(0, numero):
+      print(c)
+print('Fim')
+      
+
+
+
+numero = int(input('Digite um número: '))
+for c in range(0, numero+1):
+      print(c)
+print('Fim')
+
+
+
+i = int(input('Início: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+for c in range(i, f+1, p):
+      print(c)
+print('Fim')
+
+
+
+for c in range(0, 3):
+      n = int(input('Digite um valor: '))
+print('Fim')
+
+
+
+for c in range(0, 10):
+      n = int(input('Digite um valor: '))
+print('Fim')
+
+
+
+
+
+s = 0 
+for c in range(0, 4):
+      n = int(input('Digite um valor: '))
+      s += n 
+print('O somatório de todos os valores foi {}'.format(s))
