e47cb1a9b5189b2ffea8c0b11e1ed99fb538a269
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-24 14:43:35 -0300

    Exercício 56

---
+++ estudos-python/Exercicios-feitos/Aula_13/Exercicio56.py
@@ -0,0 +1,24 @@
+homens = 0
+mulheres = 0
+somaIdade = 0
+maisVelho = 0
+mulheresMaisNovas = 0 
+nomeHomemMaisVelho = ''
+for pessoa in range(1, 5):
+    nome = input('Insira o nome da pessoa: ')
+    idade = int(input('Insira a idade da pessoa: '))
+    sexo = input('Você é homem ou mulher:\nInsira H para homem ou M para mulher').strip().upper()
+    somaIdade += idade
+    if sexo == 'M':
+        mulheres += 1
+        if idade<20:
+            mulheresMaisNovas += 1
+    if sexo == 'H':
+        homens += 1
+        if idade>maisVelho:
+            maisVelho == idade
+            nomeHomemMaisVelho = nome
+media = float(somaIdade/4)
+print(f'A média de idade do grupo é de {media} anos')
+print(f'O homem mais velho é {nomeHomemMaisVelho}')
+print(f'Tem {mulheresMaisNovas} menores de 20 anos')
