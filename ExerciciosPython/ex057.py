58257a0630f12a16e7d6a09d7fb672a148a11945
juliaassis00 <juliaass92@gmail.com>
2025-09-25 14:48:01 -0300

    Salvando exercicio 57 do mundo 2

---
+++ ExerciciosPython/ex057.py
@@ -0,0 +1,5 @@
+
+sexo = str(input('Digite o seu sexo? [F/M] ')).upper()
+while sexo != 'F' and sexo != 'M' :
+    sexo = str(input('Letra inválida, digite as letras a seguir novamente até acerta-las [F/M] ')).upper()
+    
