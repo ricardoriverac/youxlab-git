6e6d3b0c962af789ed617084c8a4acc297246392
Lucas RibYouX <lucas.pereira-youxlab@institutoyoux.org.br>
2025-09-25 15:46:47 -0300

    adicionando atividades61 e 62, ambas sobre PA

--- python/aula14/exercicio61.py
+++ python/aula14/exercicio61.py
@@ -1,9 +1,9 @@
 primeiro = int(input('Digite o primeiro termo '))
 razao = int(input('Digite a razão: '))
-n = int(input('Quer q vá até qual termo? '))
 termo = primeiro
 count = 1 
 while count <= 10:
     print(f'{termo} ->', end=' ')
     termo += razao
     count += 1
+    print('\npausa')
---
+++ python/aula14/exercicio62.py
@@ -0,0 +1,19 @@
+primeiro = int(input('Digite o primeiro número: '))
+razao = int(input('Digite a razão'))
+termo = primeiro
+count = 1
+total = 0
+mais = 10
+while mais != 0:
+    total += mais
+    while count <= total:  
+        print(f'{termo} ', end = '')    
+        termo += razao  
+        count += 1  
+    print('pausa')
+    mais = int(input('Quer adicionar mais algum número? '))
+print(f'O total de termos é {total}')  
+'''an = a1 + r
+o código terá 2 laços de repetição
+1 -> verá se o programa tem uma resposta agradável a condição de repetição
+2 -> permitirá contar os 10 primeiros termos'''
