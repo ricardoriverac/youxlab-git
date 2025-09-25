73356d82c28a307d64ff0226c4687bc25db240ad
João Othávio <joaoothaviofabri14@gmail.com>
2025-09-25 17:51:01 -0300

    Exercício 055 feito

---
+++ curso-de-python/Módulo-1/exercicios/ex055.py
@@ -0,0 +1,14 @@
+menorPeso = 0
+maiorPeso = 0
+for c in range(1, 6):
+    peso = float(input(f'Digite o peso da {c}ª pessoa: '))
+    if c == 1:
+        maiorPeso = peso
+        menorPeso = peso
+
+    if peso > maiorPeso:
+        maiorPeso = peso
+    if peso < menorPeso:
+        menorPeso = peso
+print(f'A pessoa mais pesada tem {maiorPeso} Kg.')
+print(f'A pessoa mais leve tem {menorPeso} Kg.')
