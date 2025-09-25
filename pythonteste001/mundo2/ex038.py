15bd51ef4e843a8967f7a0e5f2ef348485b0d71e
Youx <heronherin10@gmail.com>
2025-09-25 16:05:45 -0300

    fiz um programa que compara números

---
+++ pythonteste001/mundo2/ex038.py
@@ -0,0 +1,8 @@
+n1=int(input('prineiro número: '))
+n2=int(input('segundo número: '))
+if n1>n2:
+    print('o primeiro número é maior')
+elif n1==n2:
+    print('nao existe número maior,os dois são iguais')
+else:
+    print('o segundo número é maior')
