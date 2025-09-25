4eac484ab6f93d8695f6cd1effccf7ce2ffcff62
Youx <isabelamilene74@gmail.com>
2025-09-25 14:09:15 -0300

    ex055 maior e menor da sequencia

--- python/ex055.py
+++ python/ex055.py
@@ -1,6 +1,8 @@
-for peso in range(1, 6):
-    peso = float(input('Peso da {}ª pessoa: '.format(peso)))
-    if peso == 1:
+maior = 0
+menor = 0
+for y in range(1, 6):
+    peso = float(input('Peso da {}ª pessoa: '.format(y)))
+    if y == 1:
         maior = peso
         menor = peso
     else: 
@@ -9,4 +11,6 @@ for peso in range(1, 6):
         if peso < menor:
             menor = peso 
 print('O maior peso lido foi de {}Kg'.format(maior))
+print('O manor peso lido foi de {}Kg'.format(menor))
+
 
