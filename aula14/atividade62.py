bcf578cb392fbdf02b81400339e2027f7afb3819
joao gabriel borges <joaogabrielborges052@gmail.com>
2025-09-25 13:38:39 -0300

    melhorando o exercio 61

---
+++ aula14/atividade62.py
@@ -0,0 +1,8 @@
+#numero = int(input("digite um termo:"))
+primeiro_termo = int(input("digite o primeiro termo: "))
+razao = int(input("digite uma razao:")) 
+c = 1
+while c < 10:
+    print(f"{primeiro_termo}")
+    primeiro_termo += razao
+    c += 1 
