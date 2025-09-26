aa03ceb4fb8377c11783b6a192f7852822c5a593
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-26 13:59:58 -0300

    Correção Exercício 64

--- estudos-python/Exercicios-feitos/Aula_14/Exercicio64.py
+++ estudos-python/Exercicios-feitos/Aula_14/Exercicio64.py
@@ -1,6 +1,9 @@
 number = int(input('Choose a number to add: '))
 some = 0
+count = 0
 while number != 999:
     some += number
     number = int(input('Choose another number to add: '))
+    count +=1
 print (f'The total some is {some}!')
+print (f'{count} terms where needed to this!')
