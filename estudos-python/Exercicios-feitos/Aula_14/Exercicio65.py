3c9e2e21615d6dd28a709c6de6366c222e75a752
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-26 17:37:01 -0300

    Exercício 65 mais simples

--- estudos-python/Exercicios-feitos/Aula_14/Exercicio65.py
+++ estudos-python/Exercicios-feitos/Aula_14/Exercicio65.py
@@ -1,22 +1,26 @@
-number = int(input('Choose a number:\n>'))
-count = 1
-average = 0
+count = 0
 some = 0
-highest = number
-lowest = number
-print ('\nDO YOU WANT TO CONTINUE?\n')
-continueOrNot = str(input('[N] No:\n[Y] Yes:\n\n')).strip().upper()
+highest = 0
+lowest = 0
+continueOrNot = str(input('DO YOU WANT TO CONTINUE? [N] No  [Y] Yes: ')).strip().upper()
+firstContinue = continueOrNot
 while continueOrNot == 'Y':
-    number = int(input('\nChoose a number:\n\n>'))
-    if number > highest:
+    number = int(input('Choose a number: '))
+    count += 1
+    if count == 1:
         highest = number
-    if number < lowest:
+        lowest = number
+    else:
+        if number > highest:
+            highest = number
+        if number < lowest:
             lowest = number
     some += number
     average = some / count
-    count += 1
-    print ('\nDO YOU WANT TO CONTINUE?\n\n[N] No:\n[Y] Yes:\n')
+    print ('DO YOU WANT TO CONTINUE?  [N] No [Y] Yes:')
     continueOrNot = str(input('')).strip().upper()
-print (f'\nThe average of all {count} choosen numbers is {average}!')
-print (f'\nThe highest number was {highest}!')
-print (f'\nThe lowest number was {lowest}!')
+if firstContinue == 'Y':
+    print (f'The average of all {count} choosen numbers is {average}!')
+    print (f'The highest number was {highest}!')
+    print (f'The lowest number was {lowest}!')
+print('FIM DO PROGRAMA')
