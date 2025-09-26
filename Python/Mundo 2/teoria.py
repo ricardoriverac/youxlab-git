397dc614bf3e7d298369db0ba7f6c11aad37ae51
juliaassis00 <juliaass92@gmail.com>
2025-09-26 18:42:44 -0300

    Salvando  a  teoria da aula 15 do mundo 2

--- Python/Mundo 2/teoria.py
+++ Python/Mundo 2/teoria.py
@@ -3,35 +3,36 @@
 #   if        >>>>>>>>>>>>>>>>>>>>>>>>> se 
 # break       >>>>>>>>>>>>>>>>>>>>>>>>> interrompa
 
-# PRÁTICA  
+# PRÁTICA
+# EX.:1  
 # cont = 1
 # while cont <= 10:
 #   print(cont, '...', end='')
 #   cont += 1
 # print('Acabou')
-
+# EX.:2
 # cont = 1
-# while True <= 10:
+# while True <= 10: # o comando 'True' é infinito
 #   print(cont, '-> ', end='')
 #   cont += 1
 # print('Acabou')
-
-numero = int(input('Digite um número: '))
- 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+# EX.:3
+# numero = soma = 0
+# while numero != 999:
+#     numero = int(input('Digite um número: '))
+#     soma += numero
+# soma -= 999 
+# print(f'A soma vale {soma}.')
+# EX.:4
+# numero = soma = 0
+# while True:
+#     numero = int(input('Digite um número: '))
+#     if numero == 999:
+#         break # interrompa
+#     soma += numero 
+# print(f'A soma vale {soma}.')
+# EX.:5
+nome = 'Júlia'
+idade = 16
+salario = 300.00
+print(f'O {nome:-^20} tem {idade} anos  e ganha R${salario:.2f}')
