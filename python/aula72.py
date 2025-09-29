0bb71772e4609d84a2efe7510901c0903f2f6d9f
lais <laisseabra89@gemail.com>
2025-09-29 14:39:11 -0300

    72 numero por extenso

--- python/aula72.py
+++ python/aula72.py
@@ -1,2 +1,8 @@
-lanche = ('batata frita','refrigerante','pizza')
-print(len(lanche[0:3]))
+
+num_extenso = ('zero','um','dois','tres','quatro','cinco','seis','sete','oito','nove','dez','onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte' )
+num = int(input('digite um numero entre 0 e 20:'))
+while num <= 0 or num >= 20:
+    print('tente novamente. digite um numero ntre 0 e 20:')
+    num = int(input('digite um numero entre 0 e 20: '))
+
+print(num_extenso[num]voce digitou)
