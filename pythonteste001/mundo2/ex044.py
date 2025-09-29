de08027a82fb1b279aba25b000762f10ea024458
Youx <heronherin10@gmail.com>
2025-09-29 15:19:59 -0300

    ultima atualização do código do exercício 44

--- pythonteste001/mundo2/ex044.py
+++ pythonteste001/mundo2/ex044.py
@@ -1,3 +1,5 @@
+import pygame
+
 print('{:=^60}'.format(' LOJAS JUBRISCREU '))
 preço = float(input('Preço das compras: R$'))
 print('''FORMAS DE PAGAMENTO
@@ -8,20 +10,56 @@ print('''FORMAS DE PAGAMENTO
 opção = int(input('Qual a opção? '))
 if opção == 1:
     total = preço - (preço * 10 / 100)
+    pygame.mixer.init()
+    mp3_path = "/home/youx/Downloads/para-sesi-efekti_PaUswM1.mp3"
+    pygame.mixer.music.load(mp3_path)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+         pygame.time.Clock().tick(10)
 elif opção == 2:
     total = preço - (preço * 5 / 100)
+    pygame.mixer.init()
+    mp3_path = "/home/youx/Downloads/para-sesi-efekti_PaUswM1.mp3"
+    pygame.mixer.music.load(mp3_path)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+         pygame.time.Clock().tick(10)
 elif opção == 3:
     total = preço
     parcela  = total / 2
     print('Sua compra será dividida em 2 parcelas de R${:.2f}.'.format(parcela))
+    pygame.mixer.init()
+    mp3_path = "/home/youx/Downloads/para-sesi-efekti_PaUswM1.mp3"
+    pygame.mixer.music.load(mp3_path)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+         pygame.time.Clock().tick(10)
 elif opção == 4:
     total = preço + (preço * 20 / 100)
     parcelastotais = int(input('Dejesa parcelar em quantas vezes? '))
     parcelas = total / parcelastotais
     print('Sua compra será dividida em {} parcelas de R${:.2f}, COM JUROS.'.format(parcelastotais,parcelas))
+    pygame.mixer.init()
+    mp3_path = "/home/youx/Downloads/para-sesi-efekti_PaUswM1.mp3"
+    pygame.mixer.music.load(mp3_path)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+         pygame.time.Clock().tick(10)
 else:
     total = 0
     print('\033[0;31;47mFORMA DE PAGAMENTO INVÁLIDA! TENTE NOVAMENTE!\033[m')
+    pygame.mixer.init()
+    mp3_path = "/home/youx/Downloads/buzzer-error.mp3"
+    pygame.mixer.music.load(mp3_path)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+         pygame.time.Clock().tick(10)
 print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'.format(preço, total))
 confirmação = input('Tem certeza que deseja prosseguir com esta forma de pagamento? ')
 # voltar depois para fazer um jeito de comfirmar o modo de pagamento
+pygame.mixer.init()
+mp3_path = "/home/youx/Downloads/para-sesi-efekti_PaUswM1.mp3"
+pygame.mixer.music.load(mp3_path)
+pygame.mixer.music.play()
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
