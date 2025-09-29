6d6bcf3d0c1db28201396443c46852d5ed4a8ae1
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-29 13:36:29 -0300

    Exercício 67

---
+++ estudos-python/Exercicios-feitos/Aula_15/Exercicio67.py
@@ -0,0 +1,16 @@
+number = 1
+while True:
+    number = int(input('Choose a number: '))
+    if number < -1:
+        break
+    print (f'The choosen number is number {number}')
+    print (f'{number} * 1 = {number*1}')
+    print (f'{number} * 2 = {number*2}')
+    print (f'{number} * 3 = {number*3}')
+    print (f'{number} * 4 = {number*4}')
+    print (f'{number} * 5 = {number*5}')
+    print (f'{number} * 6 = {number*6}')
+    print (f'{number} * 7 = {number*7}')
+    print (f'{number} * 8 = {number*8}')
+    print (f'{number} * 9 = {number*9}')
+    print (f'{number} * 10 = {number*10}')
