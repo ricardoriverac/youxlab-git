6605a992888d078cb88b0c14a74712b0d9f796e0
daniella <daniella.flor-youxlab@institutoyoux.org.br>
2025-09-29 16:09:38 -0300

    exercicio76 - nome e preço dos produtos em forma tabular

---
+++ Exercicios/mundo3/exercicio76.py
@@ -0,0 +1,11 @@
+nomes_preços = ()
+
+for produtos in range(2):
+    nomes = str(input(f"Qual o nome do {produtos+1}ª produto?: "))
+    preco = float(input(f"Qual o preço do {produtos+1}ª produto?: "))
+    nomes_preços += (nomes, preco)
+print("nome/preço")
+for ordem in range(0,len(nomes_preços), 2):
+   print(nomes_preços[ordem], " -> ",nomes_preços[ordem+1])
+   
+
