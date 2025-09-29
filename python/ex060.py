8164d53f40b2cf355e349c4de20e8b4d0257bc03
Youx <isabelamilene74@gmail.com>
2025-09-29 14:46:45 -0300

    ex060 fatorial

--- python/ex060.py
+++ python/ex060.py
@@ -1,3 +1,12 @@
-
+numero = int(input('Digite um número para calcular seu fatorial: '))
+c = numero
+fatorial = 1
+print(f'Vamos calcular {numero}!')
+while c > 0:
+    print(f'{c} ', end= '' )
+    print(' x ' if c > 1 else ' = ', end='')
+    fatorial = fatorial * c
+    c -=1
+print(f'{fatorial}')
 
 
