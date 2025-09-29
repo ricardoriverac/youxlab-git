3746b90f109ef1d352b2c161c33ae11654f32a4a
GiovaneSilvaxss <giovane.silva-youxlab@institutoyoux.org.br>
2025-09-29 15:06:55 -0300

    Vendo nome,idade,sexo, e qual mais valor e qual tem menos de 20

---
+++ Mundo_2/Analisador_completo.py
@@ -0,0 +1,5 @@
+for pessoa in range(1, 5):
+    print()'----- {} anos'.format((pessoa))
+    nome = str(input('Nome:')).strip()
+    idade = int(input('idade:'))
+ 
---
+++ Mundo_2/Validação_de_dados.py
@@ -0,0 +1,15 @@
+somaidade = 0
+mediaidade = 0
+maioridadehomem = 0
+for p in range(1, 5):
+    print('----- {}° PESSOA -----'.format(p))
+    nome = str(input('Nome: ')).strip()
+    idade = str(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).strip()
+    somaidade += idade 
+ if p == 1 and sexo in 'Mm'
+ maioridadehomem = idade 
+ nomevelho = nome 
+ if sexo in 'Mm' and idade  > maioridadehomem:
+mediaidade = somaidade / 4 
+print('A media de idade do grupo é de {} anos'.format(mediaidade))
