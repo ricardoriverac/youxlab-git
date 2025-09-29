b1876da28996684d28a6c9dbfb8a11e12bbe33fe
luis2025git <luis.sousa-youxlab@institutoyoux.org.br>
2025-09-29 14:50:08 -0300

    exercicio 73

---
+++ Mundo 3/aula16/d073.py
@@ -0,0 +1,5 @@
+times = 'Flamengo', 'Cruzeiro', 'Palmeiras', 'Mirassol', 'Botafogo', 'Bahia', 'São Paulo', 'Fluminese', 'Bragantino', 'Grêmio', 'Vasco', 'Corinthians', 'Ceará', 'Atlético-MG', 'Internacional', 'Santos', 'Juventude','Vitória', 'Fortaleza', 'Sport'
+print(f'Os 5 primeiros colocados da tabela: {times[0:6]}')
+print(f'Os 4 últimos colocados da tabela: {times[-4:]}')
+print(f'Ordem alfabética: {sorted(times)}')
+print(f'Posição do Vasco: {len(times[11])}')
