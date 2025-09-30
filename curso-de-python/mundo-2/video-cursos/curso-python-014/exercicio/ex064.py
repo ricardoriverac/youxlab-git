740e3dca56fbb49cec8c42c0675087ed9268db7b
João Othávio <joaoothaviofabri14@gmail.com>
2025-09-30 14:27:27 -0300

    Exercício 64 feito

---
+++ curso-de-python/mundo-2/video-cursos/curso-python-014/exercicio/ex064.py
@@ -0,0 +1,8 @@
+valor = soma = 0
+count = 0
+while valor != 999:
+    valor = int(input('Digite um número: '))
+    count += 1
+    soma += valor
+soma -= 999
+print(f'Você digitou {count} números, a soma de todos eles tem o resultado {soma}')
