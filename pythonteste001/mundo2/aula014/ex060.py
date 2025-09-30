c7b375fc14270da230f20a1bd860991a8719d499
Youx <heronherin10@gmail.com>
2025-09-30 17:52:14 -0300

    fiz um programa que calcula a fatorial de qualquer valor apresentado

---
+++ pythonteste001/mundo2/aula014/ex060.py
@@ -0,0 +1,12 @@
+numero = int(input('Digite um número: '))
+resultado = 0
+numeroMaior = numero
+numeroMenor = numero - 1
+while numero > 0:
+    if numero == numeroMaior:
+        resultado = numeroMaior * numeroMenor
+        numero -= 1
+    else:
+        resultado = resultado *  numero
+    numero -= 1
+print(f'O resultado da fatorial é {resultado}')
