f134cccb05e33b823731cb98956d6aa369f8d747
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-09-30 13:36:44 -0300

    Eu terminei o desafio 72

---
+++ pythonfile/mundo3/desafios_mundo3/d0072.py
@@ -0,0 +1,4 @@
+numero_extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'treze',
+            'quatorze', 'quinze', 'dezesseis', 'dezesete', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+numero = int(input('Digite um numero de 0 a 20: '))
+print(f'O numero {numero} por extenso é {numero_extenso[numero]} ')
