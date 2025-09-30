0c56b6f3ce21babe402bf5b44baeb0ca21c13930
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-09-30 14:39:55 -0300

    Eu terminei o desafio 74 do mundo 2

---
+++ pythonfile/mundo3/desafios_mundo3/d0074.py
@@ -0,0 +1,20 @@
+import random
+numeros = menor = 0
+numeros = ()
+print('Os numeros sorteados foram ')
+
+for c in range(0, 5):
+    numeroAleatorio = random.randint(0, 100)
+    numeros += (numeroAleatorio,)
+    print(numeroAleatorio)
+    if c == 0:
+        maior = menor = numeroAleatorio
+    else:
+        if numeroAleatorio > maior:
+            maior = numeroAleatorio
+        elif numeroAleatorio < menor:
+            menor = numeroAleatorio
+
+
+print(f'O maior é {maior} e o menor é {menor}')
+print(f'numeros completos {numeros}')        
