35125f535a6461c9b3edce48cf1815e653986d37
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-30 15:29:52 -0300

    Exercício 72

---
+++ estudos-python/Exercicios-feitos/Mundo_3/Aula_16/Exercicio_72.py
@@ -0,0 +1,3 @@
+number = ('one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen','twenty')
+position = int(input('Enter a number:'))
+print (number[position-1].capitalize())
