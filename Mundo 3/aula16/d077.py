442fc46cfea0660cc5feb93b63d1608c338877fb
lucas_paim <lucaspaim924@gmail.com>
2025-09-30 15:22:03 -0300

    Exercicio 77

--- Mundo 3/aula16/d077.py
+++ Mundo 3/aula16/d077.py
@@ -1,7 +1,9 @@
-palavras = ('Mesa', 'Aluguel', 'Paralelepipedo', 'Suco', 'Portugues', 'Matematica')
-for p in palavras:
-    for p in 'a' and 'e' and 'i' and 'o' and 'u':
-        print(palavras)
-
-
-
+minhasPalavras = ('Mesa', 'Aluguel', 'Paralelepipedo', 'Suco', 'Portugues', 'Matematica')
+vogais = 'aeiou'
+for palavra in minhasPalavras:
+    print(f'A palavra {palavra} tem as vogais: ', end='')
+    for v in vogais:
+        if v in palavra.lower():
+            print(v, end=' ')
+    print()
+        
