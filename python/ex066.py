b91f96e61dff26854d7272d2958b4950cd1639c6
Youx <isabelamilene74@gmail.com>
2025-09-30 14:34:55 -0300

    ex064 Tratando varios valores

--- python/ex066.py
+++ python/ex066.py
@@ -1,8 +1,8 @@
-soma = cont = 0 
+soma = conta = 0 
 while True:
     numero = int(input('Digite um valor (999 para quando quiser parar: '))
     if numero == 999: 
         break 
-    cont += 1
+    conta += 1
     soma += numero
-print(f'A soma dos {cont} valores foi {soma}!')
+print(f'A soma dos {conta} valores foi {soma}!')
---
+++ python/ex068.py
@@ -0,0 +1,29 @@
+from random import randint
+ganho = 0
+while True:
+    jogador = int(input('digite um valor: '))
+    computador = randint(0, 11)
+    total = jogador + computador
+    abra = ' '
+    while abra not in 'PI':
+        abra = str(input('Par ou Impar? [P/I]')).strip().upper()[0]
+    print('Você jogou {} e o computador {}. Total de {}'.format(jogador, computador, total), end='')
+    print('saiu par' if total % 2 == 0 else 'saiu impar')
+    if abra == 'P':
+         if total % 2 == 0:
+             print('Você ganhou!')
+             ganho += 1
+         else:
+            print('Você perdeu, infelizmente')
+            break
+    elif abra == 'I':
+        if total % 2 == 1:
+            print('Você ganhou!')
+            ganho += 1
+        else:
+            print('Você perdeu, infelizmente')
+            break
+    print('Tente jogar de novo')
+print('VOCÊ CONSEGUIU ME VENCEEER EM {} VEZES'.format(ganho))
+
+
