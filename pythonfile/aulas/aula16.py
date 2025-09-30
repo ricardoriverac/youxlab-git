406107a7a38a241bee3058252a96a4e15b828127
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-09-30 13:26:19 -0300

    Eu fiz a parte pratica da aula 16

---
+++ pythonfile/aulas/aula16.py
@@ -0,0 +1,15 @@
+lanche = ('hambúrguer', 'suco', 'pizza', 'pudim')
+
+for pos, comida in enumerate(lanche):
+    print(f'Eu vou comer {comida} na posição {pos}')
+
+
+
+
+#for cont in range (0, len(lanche)):
+ #   print(f'Eu vou comer {lanche[cont]}')
+
+#print(len(lanche))
+
+#for comida in lanche:
+#    print(f'eu vou comer {comida}')
