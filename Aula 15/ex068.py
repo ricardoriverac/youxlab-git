acdccef3bca6c3256bcc3748c7a362c527aa0dc6
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2025-09-30 14:53:37 -0300

    Exercicio 68, jogo par ou ímpar.

--- Aula 15/ex068.py
+++ Aula 15/ex068.py
@@ -1,22 +1,30 @@
-from random import randint 
+from random import randint
+ganho = 0
 while True:
-    jogador = int(input('Digite um valor: '))
+    jogador = int(input('digite um valor: '))
     computador = randint(0, 11)
-    total = jogador + computador 
-    tipo = ' '
-    while tipo not in 'PI':
-        tipo = str(input('Par ou ímpar? [P/I] ')).strip().upper()[0]
-    print(f'O computador jogou {computador} e você jogou {jogador}. No total de {total}')
-    if tipo == 'P':
-        if total % 2 == 0:
-            print('VOCÊ GANHOU!' )
-    else:
-        print('VOCÊ PERDEU!' )
-        break
-    elif tipo == 'I':
-    if total 
-
-
+    total = jogador + computador
+    abra = ' '
+    while abra not in 'PI':
+        abra = str(input('Par ou Impar? [P/I]')).strip().upper()[0]
+    print('Você jogou {} e o computador {}. Total de {}'.format(jogador, computador, total), end='')
+    print('saiu par' if total % 2 == 0 else 'saiu impar')
+    if abra == 'P':
+         if total % 2 == 0:
+             print('Você ganhou!')
+             ganho += 1
+         else:
+            print('Você perdeu, infelizmente')
+            break
+    elif abra == 'I':
+        if total % 2 == 1:
+            print('Você ganhou!')
+            ganho += 1
+        else:
+            print('Você perdeu, infelizmente')
+            break
+    print('Tente jogar de novo')
+print('VOCÊ CONSEGUIU ME VENCEEER EM {} VEZES'.format(ganho))
 
 
 
