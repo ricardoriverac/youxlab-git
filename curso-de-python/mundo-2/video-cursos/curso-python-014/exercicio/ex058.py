91c332550bedcf36b581a77f4fb90a9ceb470f68
João Othávio <joaoothaviofabri14@gmail.com>
2025-09-29 21:09:03 -0300

    Exercício 059 feito

--- curso-de-python/mundo-2/video-cursos/curso-python-014/exercicio/ex058.py
+++ curso-de-python/mundo-2/video-cursos/curso-python-014/exercicio/ex058.py
@@ -1,13 +1,13 @@
 import random
-for c in range(1, 11):
-    numeroAleatorio = random.randint(1,10)
-    valorUsuario = int(input('Adivinhe o numero que estou pensando de 1 a 10: '))
-    palpite = 0
-    while numeroAleatorio == valorUsuario:
+numeroAleatorio = random.randint(1,10)
+palpite = 0
+while numeroAleatorio:
+        numeroAleatorio = random.randint(1,10)
+        valorUsuario = int(input('Adivinhe o numero que estou pensando de 1 a 10: '))
         if valorUsuario != numeroAleatorio:
             print('x-X , Você errou! Tente novamente!')
             palpite += 1
-        elif valorUsuario == numeroAleatorio:
+        else:
             print('PARABÉNS!! Você acertou o número que eu estava pensando!')
 print(f'Foram necessário {palpite} palpites até você acerta o número correto.')
 '''import random 
---
+++ curso-de-python/mundo-2/video-cursos/curso-python-014/exercicio/ex059.py
@@ -0,0 +1,24 @@
+valor1 = int(input('Digite um valor: '))
+valor2 = int(input('Digite outro valor: '))
+escolhaUsuario = 0
+while escolhaUsuario != 5:
+    print('-' * 20)
+    print(f'O que você deseja fazer com os valores {valor1} e {valor2}')
+    print('[1] Somar')
+    print('[2] Multiplicar')
+    print('[3] Maior')
+    print('[4] Novos Números')
+    print('[5] Sair do programa')
+    escolhaUsuario = int(input('Escolha uma das 5 opções: '))
+    if escolhaUsuario == 1:
+        print(f'A soma entre o número {valor1} e {valor2} resulta em {valor1 + valor2}' )
+    if escolhaUsuario == 2:
+        print(f'A multiplicação do número {valor1} e o {valor2} resulta em {valor1 * valor2}')
+    if escolhaUsuario == 3:
+        maior = valor1
+        if valor2 > valor1:
+            maior = valor2
+        print(f'Entre o número {valor1} e {valor2} o maior é o número {maior}')
+    if escolhaUsuario == 4:
+        valor1 = int(input('Digite um novo valor: '))
+        valor2 = int(input('Digite um novo outro valor: '))
