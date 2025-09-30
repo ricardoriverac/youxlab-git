4e7c32c19664e22b9319341dcfdc5f56fa0308a2
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-30 13:23:45 -0300

    Exercício 69 definitivo

--- estudos-python/Exercicios-feitos/Aula_15/Exercicio69.py
+++ estudos-python/Exercicios-feitos/Aula_15/Exercicio69.py
@@ -3,16 +3,17 @@ ofLegalAge = 0
 mans = 0
 womansUnder20 = 0
 while option == 'Y':
-    while option == 'Y':
-        age = int(input('What is your age?'))
-        sex = int(input('What is your gender?\n[1]Male [2]Female'))
-        if age > 18:
-            ofLegalAge += 1
-        if sex == 1:
-            mans += 1
-        if sex == 2 and age < 20:
-            womansUnder20 += 1
-        option = input('Do you want to continue the program?\n[N]No [Y]Yes').strip().upper()
+    age = int(input('What is your age?'))
+    sex = int(input('What is your gender?\n[1]Male [2]Female'))
+    if age > 18:
+           ofLegalAge += 1
+    if sex == 1:
+        mans += 1
+    if sex == 2 and age < 20:
+        womansUnder20 += 1
+    option = input('Do you want to continue the program?\n[N]No [Y]Yes').strip().upper()
+    if option == 'N':
+        break
 print (f'There is {ofLegalAge} peoples of legal age!')
 print (f'There is {mans} mans!')
 print (f'And there are {womansUnder20} womans under 20 years!')
