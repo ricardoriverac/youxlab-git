d78f37ffe3e8a901097e1e316d4ceba18f6618d7
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-10-02 17:07:25 -0500

    ex075 feito

---
+++ python/aula_16/ex075.py
@@ -0,0 +1,20 @@
+'''
+Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:
+
+A) Quantas vezes apareceu o valor 9.
+B) Em que posição foi digitado o primeiro valor 3.
+C) Quais foram os números pares.
+'''
+
+numeros = int(input('Digite um numero:')), int(input('Digite um numero:')),int(input('Digite um numero:')),int(input('Digite um numero:'))
+print(f'Foram digitados os números: {numeros}')
+print(f'O número 9 apareceu {numeros.count(9)} vezes. ')
+if 3 in numeros:
+    print(f'O número 3 aparece na {numeros.index(3)+1} posição. ')
+else:
+    print('O número 3 não foi digitado')
+print(f'Os números pares que você digitou foram: ', end='') 
+
+for n in numeros:
+    if n % 2 == 0:
+        print(n, end= ' ')
