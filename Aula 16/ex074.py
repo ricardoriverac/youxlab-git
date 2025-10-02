6f8f537963c47fb95f098a8cf6d347c8baf8fc97
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2025-10-02 16:45:28 -0300

    Exercicio 74, sorteando numeros e mostrando o maior e o menor numero.

---
+++ Aula 16/ex074.py
@@ -0,0 +1,5 @@
+from random import randint
+numero = randint(1,10), randint(1,10), randint(1,10), randint(1,10), randint(1,10)
+print(f'Foram sorteados os números: {numero}')
+print(f'\nO maior número sorteado foi {max(numero)}')
+print(f'O menor número sorteado foi {min(numero)}')
