3c9bb0b33c81d33409a8ecfa5d73dd8bef4769f0
joao gabriel borges <joaogabrielborges052@gmail.com>
2025-10-02 13:13:13 -0300

    fazendo a atividade79 da aula 17

---
+++ mundo 3/aula17/atividade79.py
@@ -0,0 +1,20 @@
+recebedor = []
+while True:
+    valor = int(input("digite um numero: "))
+    recebedor.append(valor) 
+    recebedor.sort()
+    resposta = str(input("quer continuar? [s/n] ")).lower()
+    while resposta != "s" and resposta != "n":
+          resposta = str(input("quer continuar? [s/n] ")).lower()
+    while resposta == "s":
+              valor = int(input("digite um numero: ")) 
+              if valor not in recebedor:
+                     recebedor.append(valor)
+              else:
+                print("esse numero ja foi adicionado") 
+                resposta = str(input("quer continuar? [s/n] ")).lower()           
+    if resposta == "n":
+            break
+print(f" {recebedor} esse sao os valores digitados")
+
+
