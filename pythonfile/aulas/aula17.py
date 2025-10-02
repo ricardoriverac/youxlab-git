6f3957aa93dcd7a0842b34be2ef12778fda91997
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-10-01 22:53:08 -0300

    Eu pratiquei a aula 17 do mundo 3

---
+++ pythonfile/aulas/aula17.py
@@ -0,0 +1,31 @@
+
+
+
+a = [2, 3, 4, 7]
+b = a
+b[2] = 8
+print(f'Lista A: {a}')
+print(f'Lista B: {b}')
+
+#valores = []
+#for cont in range(0, 5):
+ #   valores.append(int(input('Digite um valor: ')))
+#for c, v in enumerate(valores):
+#    print(f'Na posição {c} encontrei o valor {v}... ')
+
+
+
+#valores.append(5)
+#valores.append(9)
+#valores.append(4)
+
+
+#numero = [3, 1, 9, 5]
+#numero [2] = 8
+#numero.append(7)
+#numero.sort(reverse=True)
+#numero.insert(2, 2)
+#if 3 in numero:
+#    numero.remove(3)
+#print(numero)
+#print(f'essa lista tem {len(numero)} elementos')
