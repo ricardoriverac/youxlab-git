8ee2962657e1ccb419e95084f6806940794dbe8c
agathae <agatha.silva-youxlab@institutoyoux.org.br>
2025-10-02 14:07:00 -0300

    Exercicio 83 validando expressoes numericas

---
+++ python3/aula.16/aula.17/ex083.py
@@ -0,0 +1,6 @@
+expressao = str(input('Digite uma expressao numerica: '))
+if expressao.count('(') and expressao.count(')'):
+    print('Sua expressao é VALIDA')
+else:
+    print('Sua expressao é INVALIDA')
+
