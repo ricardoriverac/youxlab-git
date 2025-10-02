04f34b1b566c0033553cf156d1281a6bab7c5bab
lais <laisseabra89@gemail.com>
2025-10-02 17:16:21 -0300

    lista de preço

---
+++ python/aula76.py
@@ -0,0 +1,8 @@
+minhaTupla = ('pizza', 50, 'suco', 8, 'hamburguer', 27)
+print('cardapio')
+print('_'*20)
+for c in range(0, len(minhaTupla)):
+    if c % 2 == 0:
+        print(f'{minhaTupla[c]:.<30}', end = ' ')
+    else:
+        print(f'{minhaTupla[c]:.>7}')
