c23d2680d7ce349ac01a1257bdd7b7bf2def78e6
Thawane <thawane.correa@institutoyoux.org.br>
2025-10-02 17:33:07 -0300

    Aula04 desafio3 meu programa está lendo dois números como se fosse uma calculadorae somando eles e mostrando o resultado final

---
+++ python/aula04/aula04 desafio3.py
@@ -0,0 +1,9 @@
+#Crie um script Python que leia dois números e tente mostrar as soma entre eles
+
+x = input("Calculadora de adição entre dois numeros: Digite o primeiro numero")
+y = input("Agora digite o segundo numero")
+x = int(x)
+y = int(y)
+soma = x + y
+print("seu numero final é", soma)
+
