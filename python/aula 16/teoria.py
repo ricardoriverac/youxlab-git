cffb454dc03131ac2a3a440e6aa56e8c71a22234
Youx <isabelamilene74@gmail.com>
2025-10-02 15:10:54 -0300

    ex031 calculando o valor da passagem com o km que vc vai percorrer

---
+++ python/aula 16/teoria.py
@@ -0,0 +1,80 @@
+lanche = 'Hambúrguer'
+lanche = 'Suco'
+print(lanche)
+
+
+
+
+lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+print(lanche)
+
+
+
+
+lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
+print(lanche [1])
+
+
+
+
+lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
+print(lanche [3])
+
+
+
+
+lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
+print(lanche [-2])
+
+
+
+lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
+print(lanche [1:3])
+
+
+
+lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
+print(lanche [2:])
+
+
+
+
+lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
+print(lanche [:2])
+
+
+lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
+print(lanche [-2])
+
+
+
+lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
+print(lanche [-3:])
+
+
+lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
+#Tuplas são imutáveis
+#lanche[1] = 'Refrigerante'
+print(lanche)
+
+
+
+
+
+lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
+for comida in lanche:
+    print(f'Eu vou comer {comida}')
+print('Comi pra caramba!')
+
+
+
+
+
+
+
+lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim', 'batata Frita')
+
+print(len(lanche))
+#for comida in lanche:
+#    print(f'Eu vou comer {comida}')
+print('Comi pra caramba!')
