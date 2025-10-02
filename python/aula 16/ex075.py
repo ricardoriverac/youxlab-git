bd1122fe89b6da1ee60330a158d1307f51a85dd4
Youx <isabelamilene74@gmail.com>
2025-10-02 17:24:09 -0300

    ex075 análise de dados em uma tupla

---
+++ python/aula 16/ex075.py
@@ -0,0 +1,12 @@
+numeros = int(input('Digite um numero:')), int(input('Digite um numero:')),int(input('Digite um numero:')),int(input('Digite um numero:'))
+print(f'Foram digitados os números: {numeros}')
+print(f'O número 9 apareceu {numeros.count(9)} vezes. ')
+if 3 in numeros:
+    print(f'O número 3 aparece na {numeros.index(3)+1} posição. ')
+else:
+    print('O número 3 não foi digitado')
+print(f'Os números pares que você digitou foram: ', end='') 
+
+for n in numeros:
+    if n % 2 == 0: 
+        print(n, end='')
