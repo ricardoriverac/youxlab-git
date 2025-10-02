f95477045ab3b26195a370ea154ed8ca1353f24b
agathae <agatha.silva-youxlab@institutoyoux.org.br>
2025-10-02 19:25:54 -0300

    exercicio 86 matriz em python

---
+++ python3/aula.18/ex086.py
@@ -0,0 +1,9 @@
+matriz = [[0,0,0], [0,0,0], [0,0,0]]
+for i in range(0, 3):
+    for j in range(0, 3):
+        matriz[i][j] = int(input(f'Digite um valor para a linha {i} e coluna {j}: '))
+print('-='*30)        
+for i in range(0, 3):
+    for j in range(0, 3):
+      print(f'[{matriz[i][j]}]', end ='')   
+    print()  
