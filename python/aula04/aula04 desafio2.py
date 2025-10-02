503ede2967798be751d3bc2de0a72aa1e0b87391
Thawane <thawane.correa@institutoyoux.org.br>
2025-10-02 17:23:42 -0300

    Aula 04 desafio2 o meu programa está lendo a data de nascimento da pessoa e agradecendo pelas informações

---
+++ python/aula04/aula04 desafio2.py
@@ -0,0 +1,7 @@
+#Crie um script Python que leia o dia,mes e o anao do nascimento de uma pessoa e mostra uma mensagem com a data formatada
+
+dia=input('Dia do nascimento')
+mes=input ('Mes do nascimento')
+ano=input('Ano do nascimento')
+print ("Você nasceu no dia", dia,"de", mes, "de", ano)
+print('Obrigado pelas informações!')
