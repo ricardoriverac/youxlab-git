1669d59ce68c2723345243613bbbfeb1cad3cb99
Youx <heronherin10@gmail.com>
2025-10-02 13:37:05 -0300

    fiz um programa que simula um caixa eletronico que contém notas de 50,20,10 e 1 real

---
+++ pythonteste001/mundo2/aula015/ex071.py
@@ -0,0 +1,20 @@
+valor = int(input('que valor você pretende sacar? R$'))
+total = valor
+cedula = 50
+totalcedulas = 0
+while True:
+    if total >= cedula:
+        total -= cedula
+        totalcedulas += 1
+    else:
+        if totalcedulas > 0:
+            print(f'{totalcedulas} cedulas de R${cedula:.2f}')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
+        totalcedulas = 0
+        if total == 0:
+            break
