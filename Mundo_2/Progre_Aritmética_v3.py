ccc40a62deb55202810ab419813625e14bc18f83
GiovaneSilvaxss <giovane.silva-youxlab@institutoyoux.org.br>
2025-10-01 22:29:44 -0300

    Termos de PA

---
+++ Mundo_2/Progre_Aritmética_v3.py
@@ -0,0 +1,17 @@
+prin('Gerador de PA')
+print('-=' * 10)
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão de PA:'))
+termo = primeiro
+cont = 1 
+total = 0 
+mais = 10
+while mais != 0: 
+    total = total + mais
+    while cont <= total:
+        print('{} '.format(termo), end='')
+        termo += razão
+        cont += 1 
+    print('Pausa')
+    mais = int(input('Quantos termos voce quer mostrar a mais? '))
+print('Progressão finalizada com {} termos mostrados'.format(total))
