83154585cf6b7d8d714248e3cc0979caf8ca7a7d
Youx <heronherin10@gmail.com>
2025-10-02 15:22:58 -0300

    fiz um programa que recebe um número de 0 à 20 e depois mostra ele por extenso

---
+++ pythonteste001/mundo3/aula16/ex072.py
@@ -0,0 +1,3 @@
+numeros = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+escolha = int(input('Qual número entre 0 e 20 vocẽ quer ver por extenso? '))
+print(numeros[escolha])
