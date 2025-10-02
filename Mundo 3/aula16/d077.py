59bb5a5eee9e29ae847b87d2769420a3a13fd084
luis2025git <luis.sousa-youxlab@institutoyoux.org.br>
2025-10-02 13:21:13 -0300

    Exercicio 77

--- Mundo 3/aula16/d077.py
+++ Mundo 3/aula16/d077.py
@@ -1,7 +1,4 @@
 palavras = ('Mesa', 'Aluguel', 'Paralelepipedo', 'Suco', 'Portugues', 'Matematica')
 for p in palavras:
     for p in 'a' and 'e' and 'i' and 'o' and 'u':
-        print(palavras)
-
-
-
+        
