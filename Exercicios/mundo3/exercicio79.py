59fdfd71c932516ed00c7ff8298d746ae884d7cf
daniella <daniella.flor-youxlab@institutoyoux.org.br>
2025-10-02 16:26:15 -0300

    exercicio79- refazendo

--- Exercicios/mundo3/exercicio79.py
+++ Exercicios/mundo3/exercicio79.py
@@ -1,16 +1,18 @@
-lista = []
+listaDnumeros = []
 continuar = 'S'
 
+while continuar == 'S':
+    numero = int(input("Digite um número: "))
+    if numero in listaDnumeros:
+        while numero in listaDnumeros:
+            numero = int(input("Esse número já foi adicionado. Adicione outro: "))
+        listaDnumeros.append(numero)
+    else:
+        listaDnumeros.append(numero)
+    print(listaDnumeros)
+    continuar = str(input('Voce quer continuar [S/N]? ')).upper()     
 
-numero = int(input("Digite um número: "))
-lista.append(numero)
 
-while continuar != 'N':
-    continuar = str(input('Quer continuar? [S/N]: ')).upper()
-
-
-
-
-lista.sort()
-print(f"Sua lista em ordem crescente é: {lista}")
+for numero in sorted(listaDnumeros):
+    print(f'{numero} -> ', end=' ') 
     
