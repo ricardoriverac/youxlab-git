bb0f3ebd0b665db8a6add7a416dc0b11d1bc4e0c
Youx <heronherin10@gmail.com>
2025-10-03 18:41:44 -0300

    fiz um programa que verifica se expressões usando '()' estão digitadas de forma correta

---
+++ pythonteste001/mundo3/aula17/ex083.py
@@ -0,0 +1,15 @@
+expressao = str(input('Digite uma expressão: '))
+pilha = []
+for simbolo in expressao:
+    if simbolo == '(':
+        pilha.append('(')
+    elif simbolo == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está correta!')
+else:
+    print('\033[31mSua expressão está errada!\033[m')
