cf500b8e14e842f230945fbf515353513157e647
luis2025git <luis.sousa-youxlab@institutoyoux.org.br>
2025-10-06 17:00:03 -0300

    Aula 18 criada com os exercícios e parte teórica criada e pronta

---
+++ Python/Mundo 3/aula 18/teorias.py
@@ -0,0 +1,49 @@
+print('-'*35)
+print('     TEORIAS PARTE 1    ')
+
+#teste = list()
+#teste.append('Luis')
+#teste.append(15)
+#galera = list()
+#galera.append(teste[:])
+#teste[0] = 'Maria'
+#teste[1] = 15
+#galera.append(teste[:])
+#print(galera)
+print('-'*35)
+print('     TEORIAS PARTE 2     ')
+
+#pessoas = [['João', 19], ['Igor', 17], ['Emily', 15], ['Pablo', 16]]
+#print(pessoas)
+#print('-'*20)
+#print(pessoas[0])
+#print('-'*20)
+#print(pessoas[0][0])
+#print('-'*20)
+#print(pessoas[2][1])
+#for p in pessoas:
+    #print(f'{p[0]} tem {p[1]} anos de idade.')
+    #print(p[0])
+    #rint(p[1])
+print('-'*35)
+print('     TEORIAS PARTE 3     ')
+
+galera = list()
+dado = list()
+totmaior = totmenor = 0
+for cont in range(0, 3):
+    dado.append(str(input('Nome: ')))
+    dado.append(int(input('Idade: ')))
+    print('-'*15)
+    galera.append(dado[:])
+    dado.clear()
+
+for p in galera:
+    if p[1] >= 18:
+        print(f'{p[0]} é maior de idade.')
+        totmaior += 1
+    else:
+        print(f'{p[0]} é menor de idade.')
+        totmenor += 1
+
+print(f'Temos {totmaior} maiores e {totmenor} menores de idade.')
