69201f3ab9292513711e401ff121348979e01fa9
luis2025git <luis.sousa-youxlab@institutoyoux.org.br>
2025-10-06 17:00:52 -0300

    Parte teórica da aula 18 pronta

--- Python/Mundo 3/aula 18/teorias.py
+++ Python/Mundo 3/aula 18/teorias.py
@@ -28,22 +28,20 @@ print('     TEORIAS PARTE 2     ')
 print('-'*35)
 print('     TEORIAS PARTE 3     ')
 
-galera = list()
-dado = list()
-totmaior = totmenor = 0
-for cont in range(0, 3):
-    dado.append(str(input('Nome: ')))
-    dado.append(int(input('Idade: ')))
-    print('-'*15)
-    galera.append(dado[:])
-    dado.clear()
-
-for p in galera:
-    if p[1] >= 18:
-        print(f'{p[0]} é maior de idade.')
-        totmaior += 1
-    else:
-        print(f'{p[0]} é menor de idade.')
-        totmenor += 1
-
-print(f'Temos {totmaior} maiores e {totmenor} menores de idade.')
+#galera = list()
+#dado = list()
+#totmaior = totmenor = 0
+#for cont in range(0, 3):
+#    dado.append(str(input('Nome: ')))
+#    dado.append(int(input('Idade: ')))
+#    print('-'*15)
+#    galera.append(dado[:])
+#    dado.clear()
+#for p in galera:
+#    if p[1] >= 18:
+#        print(f'{p[0]} é maior de idade.')
+#        totmaior += 1
+#    else:
+#        print(f'{p[0]} é menor de idade.')
+#        totmenor += 1
+#print(f'Temos {totmaior} maiores e {totmenor} menores de idade.')
