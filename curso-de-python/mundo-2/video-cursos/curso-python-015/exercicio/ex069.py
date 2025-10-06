a7afb49576cbb8d91edad0aacdfe68fce50c7859
João Othávio <joaoothaviofabri14@gmail.com>
2025-10-06 17:01:26 -0300

    Exerícico 069 feito

--- curso-de-python/mundo-2/video-cursos/curso-python-015/exercicio/ex069.py
+++ curso-de-python/mundo-2/video-cursos/curso-python-015/exercicio/ex069.py
@@ -1,27 +1,40 @@
-idade = 1
-sexo = 'MF'
+idade = 0
+
 idadeCount = 1
 masculino = 0
 feminino = 0
 maior = menor = 0
 while True:
     idade = int(input(f'Digite a idade da {idadeCount}ª pessoa: '))
-    if idade > 0:
-        idadeCount += 1
+
+    idadeCount += 1
+
     sexo = str(input('Digite qual o seu sexo: [M/F]  ')).upper()
-    if sexo == 'M':
-        masculino += 1
-    if sexo == 'F':
-        feminino += 1
+    while True:
+        if sexo == 'M':
+            masculino += 1
+            break
+        elif sexo == 'F':
+            feminino += 1
+            break
+        else:
+            print("Inválido. Tente novamente.")
+            sexo = str(input('Digite qual o seu sexo: [M/F]  ')).upper()
+
     continuar = str(input('Deseja continuar? [S/N]  ')).upper()
     if continuar == 'N':
         break
+
     if idade >= 18:
         maior += 1
-    if sexo == 'F':
-        if idade < 20:
-            feminino += 1
+
+    if sexo == 'F' and idade < 20:
+        menor += 1
+
 
 print(f'No total, há {maior} pessoas maiores que 18 anos.')
 print(f'{masculino} Homens foram cadastrados.')
-print(f'{feminino} Mulheres são menos que 20 anos.')
+if menor <= 1:
+    print(f'{menor} Apenas uma Mulher possui menos que 20 anos.')
+elif menor >= 1:
+    print(f'{menor} Mulheres são menos que 20 anos.')
