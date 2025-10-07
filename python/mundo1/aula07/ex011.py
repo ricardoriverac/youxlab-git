1756832cf8bc621ff89a8337c9f6d136701d19a5
Thawane <thawane.correa@institutoyoux.org.br>
2025-10-07 17:30:23 -0300

    ex011

---
+++ python/mundo1/aula07/ex011.py
@@ -0,0 +1,7 @@
+largura=float(input('Digite a largura em metros: '))
+altura=float(input('Digite a altura em metros: '))
+area=largura*altura
+print('Sua parede tem o tamanho de {}x{} e sua área tem {}'.format(largura, altura, area))
+tinta= area / 2
+print('Para pintar a parede você vai precisar de {}l de tinta'.format(tinta))
+
