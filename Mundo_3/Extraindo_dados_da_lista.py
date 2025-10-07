165220a3bd943f3f59a0d28cd8338d904042d96d
GiovaneSilvaxss <giovane.silva-youxlab@institutoyoux.org.br>
2025-10-07 14:47:39 -0300

    Mostra quantos elementos foi digitado,e a sua ordem decrescente que foi digitado

---
+++ Mundo_3/Extraindo_dados_da_lista.py
@@ -0,0 +1,14 @@
+valores = []
+while True:
+    valores.append(int(input('Digite um valior:'))  
+    resp = str(input('Quer continuar?'))
+        in resp in 'Nn'
+            break
+print('-=' * 30)
+print('Você digitou {len(elementos)}')
+valores.sort(reverse=True)
+print(f'Os valores em ordem decrescente são {valores}')
+if 5 in valores:
+    print('O valor 5 faz parte da lista')
+else:
+    print('O valor 5 não faz parte da lista')
