167240538782bde3c078b4de38c2e335cf880f08
Emily <emily.ferreira-youxlab@institutoyoux.org.br>
2025-10-07 17:26:32 -0300

    desafio 73 da aula 16

---
+++ Pyhton/Mundo_3/desafios/desafio73.py
@@ -0,0 +1,11 @@
+brasil2025 = ("Flamengo", "Cruzeiro", "Palmeiras", "Mirassol", "Botafogo", "Bahia", "São Paulo", "Fluminense", "Red Bull Bragantino",
+"Grêmio", "Ceará", "Vasco da Gama", "Atlético Mineiro", "Corinthians", "Internacional", "Santos", "Juventude", "Vitória", "Fortaleza",
+"Sport")
+print('Os 5 primeiros colocados: ')
+print(brasil2025[0:5])
+print('Os últimos 4 colocados da tabela: ')
+print(brasil2025[-4:])
+print('Lista com os times em ordem alfabética: ')
+print(sorted(brasil2025))
+print('Em que posição na tabela está o time do Mirassol?: ')
+print(brasil2025.index('Mirassol'))
