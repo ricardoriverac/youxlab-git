a1ae168a604bc00843af04d98238e8247af6546e
Youx <isabelamilene74@gmail.com>
2025-10-06 21:04:11 -0300

    ex078 Maior e Menor valores na Lista

--- python/aula17/ex078.py
+++ python/aula17/ex078.py
@@ -1,2 +1,13 @@
-listanum = []
-mai
+lista = list()
+for valores in range(0, 5):
+    lista.append(int(input('Adicione um valor: ')))
+print(f'Os valores informados foi: {lista}')
+print(f'O maior valor foi {max(lista)} na posição ', end='')
+for int, v in enumerate(lista):
+    if max(lista) == v:
+        print(f'{int}', end='ª ')
+print()
+print(f'O menor valor foi {min(lista)} na posição ', end='')
+for int, v in enumerate(lista):
+    if min(lista) == v:
+        print(f'{int}', end='ª ')
