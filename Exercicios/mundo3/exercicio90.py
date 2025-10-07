881b560db91dbea385491b783ed048c715de40dd
daniella <daniella.flor-youxlab@institutoyoux.org.br>
2025-10-07 13:43:05 -0300

    exercicio90 - guardar em dicionario a nota, media e situaçao

--- Exercicios/mundo3/exercicio90.py
+++ Exercicios/mundo3/exercicio90.py
@@ -1,4 +1,14 @@
 aluno = dict()
 aluno['nome'] = str(input("Digite seu nome: "))
-aluno['media'] = float(input(f"Media de {aluno["nome"]}: "))
+aluno['media'] = float(input(f"Media de {aluno['nome']}: "))
+
+if aluno['media'] > 7.0:
+    aluno['situacao'] = 'Aprovado!'
+else:
+    aluno['situacao'] = 'Reprovado!'
+
+print(aluno)
+
+for chave,valor in aluno.items():
+    print(f"{chave} = {valor}")
 
