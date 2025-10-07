8e256181106b8dde74ae4a1ba04daddb6bc775ea
GiovaneSilvaxss <giovane.silva-youxlab@institutoyoux.org.br>
2025-10-07 17:16:34 -0300

    Validando expressões,mostra se a expressão esta correta.

---
+++ Mundo_3/Validando_expressões_matematicas.py
@@ -0,0 +1,15 @@
+expr = str(input('Digite a expressão: '))
+pilha = []
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Expressão correta!'.title())
+else:
+    print('Expressão errada, você nao fechou os parenteses correntamente.'.title())
