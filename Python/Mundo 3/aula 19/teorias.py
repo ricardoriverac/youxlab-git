f5aff2f17b03054816da069be8de0f7b97c6f30a
luis2025git <luis.sousa-youxlab@institutoyoux.org.br>
2025-10-08 14:24:34 -0300

    Parte teórica da aula 19 pronta

---
+++ Python/Mundo 3/aula 19/teorias.py
@@ -0,0 +1,62 @@
+print('-'*26)
+print('    TEORIAS PARTE 1    ')
+#pessoas = {'Nome': 'Luis', 'Sexo': 'M', 'Idade': 15}
+#print(f'-> O {pessoas["Nome"]} tem {pessoas["Idade"]} anos.')
+print('-'*26)
+print('    TEORIAS PARTE 2    ')
+#pessoas = {'Nome': 'Luis', 'Sexo': 'M', 'Idade': 15}
+#print(pessoas.keys())
+#print(pessoas.values())
+#print(pessoas.items())
+print('-'*26)
+print('    TEORIAS PARTE 3    ')
+print(''' MOSTRAR VALOR, CHAVES E ITENS DO DICIONÁRIO: ''')
+#pessoas = {'Nome': 'Luis', 'Sexo': 'M', 'Idade': 15}
+#for k in pessoas.keys():
+#    print(k)
+#print('=-'*13)
+#for v in pessoas.values():
+#    print(v)
+#print('=-'*13)
+#for k, v in pessoas.items():
+#    print(f'{k} = {v}')
+print('-'*26)
+print('    TEORIAS PARTE 4    ')
+print(''' DELETAR UM ELEMENTO DO DICIONÁRIO: ''')
+#pessoas = {'Nome': 'Luis', 'Sexo': 'M', 'Idade': 15}
+#del pessoas['Sexo']
+#for k, v in pessoas.items():
+#   print(f'{k} = {v}')
+print('-'*26)
+print('    TEORIAS PARTE 5    ')
+print(''' MUDAR QUALQUER PARTE DO DICIONÁRIO: ''')
+#pessoas = {'Nome': 'Luis', 'Sexo': 'M', 'Idade': 15}
+#pessoas['Nome'] = 'Leandro'
+#for k, v in pessoas.items():
+#    print(f'{k} = {v}')
+print('-'*26)
+print('    TEORIAS PARTE 6    ')
+print(''' ADICIONAR ELEMENTOS NO DICIONÁRIO: ''')
+#pessoas = {'Nome': 'Luis', 'Sexo': 'M', 'Idade': 15}
+#pessoas['Peso'] = 43.3
+#for k, v in pessoas.items():
+#    print(f'{k} = {v}')
+print('-'*26)
+print('    TEORIAS PARTE 6    ')
+print('''  CRIAR UM DICIONÁRIO DENTRO DE UMA LISTA: ''')
+#brasil = []
+#estado1 = {'UF': 'Minas Gerais', 'sigla': 'MG'}
+#estado2 = {'UF': 'Rio de Janeiro', 'sigla': 'RJ'}
+#brasil.append(estado1)
+#brasil.append(estado2)
+#print(brasil)
+print('-'*26)
+print('    TEORIAS PARTE 6    ')
+print('''  PEDIR AO USUÁRIO PARA ADICIONAR UM ELEMENTO POR VEZ: ''')
+estado = {}
+brasil = []
+for c in range(0, 3):
+    estado['UF'] = str(input('Unidade Federativa: '))
+    estado['sigla'] = str(input('Sigla do Estado: '))
+    brasil.append(estado.copy)
+print(brasil)
