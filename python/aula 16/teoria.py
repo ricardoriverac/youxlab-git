8cf6d3ba046f911cece34b659600ecf1aec2f807
Youx <isabelamilene74@gmail.com>
2025-10-08 14:30:26 -0300

    teoria.py da aula 19

--- python/aula 16/teoria.py
+++ python/aula 16/teoria.py
@@ -2,75 +2,42 @@ lanche = 'Hambúrguer'
 lanche = 'Suco'
 print(lanche)
 
-
-
-
 lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
 print(lanche)
-
-
-
 
 lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
 print(lanche [1])
 
-
-
-
 lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
 print(lanche [3])
 
-
-
-
 lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
 print(lanche [-2])
-
-
 
 lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
 print(lanche [1:3])
 
-
-
 lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
 print(lanche [2:])
-
-
-
 
 lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
 print(lanche [:2])
 
-
 lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
 print(lanche [-2])
 
-
-
 lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
 print(lanche [-3:])
-
 
 lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
 #Tuplas são imutáveis
 #lanche[1] = 'Refrigerante'
 print(lanche)
 
-
-
-
-
 lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
 for comida in lanche:
     print(f'Eu vou comer {comida}')
 print('Comi pra caramba!')
-
-
-
-
-
-
 
 lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim', 'batata Frita')
 
