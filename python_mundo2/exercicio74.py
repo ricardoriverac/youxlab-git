006d1383d96a325e5a89ad9516be17406c3f02f2
Youx <juan.ferreira-youxlab@institutoyoux.org.br>
2025-10-08 17:18:17 -0300

    escolhe 5 numeros aleatorios e vê qual o maior eo menor

---
+++ python_mundo2/exercicio74.py
@@ -0,0 +1,4 @@
+from random import randint
+na = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10))
+print (f'os n´umero são {na}')
+print (f'O maior número digitado foi {max(na)} e o menor número foi {min(na)}')
