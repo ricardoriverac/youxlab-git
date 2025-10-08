cb5674f735950253bcf17e0f85564e6e65f72495
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2025-10-08 17:47:38 -0300

    Correção no desafio 083 - Validando expressões matemáticas

--- Estudos/python3/aula17/desafio083.py
+++ Estudos/python3/aula17/desafio083.py
@@ -3,7 +3,7 @@ expressao=str(input('Digite a expressão: '))
 lista.append(expressao)
 contadorParenteses= expressao.count('(')
 contadorParenteses2= expressao.count(')')
-if contadorParenteses % 2 == 0 and contadorParenteses % 2 == 0:
+if contadorParenteses ==  contadorParenteses2:
     print('Expressão numérica válida! ')
-elif contadorParenteses % 2 != 0 and contadorParenteses2 % 2 != 0:
+else:
     print('Expressão numérica inválida!')
