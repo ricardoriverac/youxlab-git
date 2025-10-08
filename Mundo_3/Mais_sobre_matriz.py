b63ad37de1082c748dec7818d9ee634d26f6d781
GiovaneSilvaxss <giovane.silva-youxlab@institutoyoux.org.br>
2025-10-08 13:16:19 -0300

    Digite os numeros,a soma dos valores pares,a soma da terceira coluna e o maior valor da segunda colona

---
+++ Mundo_3/Mais_sobre_matriz.py
@@ -0,0 +1,16 @@
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+spar = mai = scol = 0
+for l in range(0, 3):
+    for c in range(0, 3)
+        print(f'[{matriz[l][c]:^5}]', end='')
+        if matriz[l][c] % 3 == 0:
+            spar += matriz[l][c]
+    print()
+print('-=' * 30)
+print(f'A soma dos números pares é {spar}')
+for l in range(0, 3):
+    if c == 0:
+        mai = matriz[1][c]
+    elif matriz[1][c] > mai:
+        mai = matriz[1][c]
+print(f'O maior valor da sunda linha é {mai}.')
