9a99c2e59dbfcf931ddf85e7dabd4de966ba26e9
oigorromao <igor.faustino-youxlab@institutoyoux.org.br>
2025-10-08 15:20:14 -0300

    Atualizei o 100

--- Python/Mundo3/aula.20/exercicio100.py
+++ Python/Mundo3/aula.20/exercicio100.py
@@ -1,9 +1,18 @@
 import random
+
+def sorteia(numeros_disponivel):
+    return  random.sample(numeros_disponivel, 5)
+
+def somaPar(numeros_sorteados):
+    soma = 0
+
+    for numero in numeros_sorteados:
+        if numero % 2 == 0:
+            soma += numero
+    return soma
+
 numeros_disponivel = range(1,101)
-numeros_sorteados = random.sample(numeros_disponivel, 5)
-numeros_pares = 0
+numeros_sorteados = sorteia(numeros_disponivel)
 print(f'Os numeros sorteados foram {numeros_sorteados}')
-for numero in numeros_sorteados:
-    if numero % 2 == 0:
-        numeros_pares += numero
+numeros_pares = somaPar(numeros_sorteados)
 print(f'Sendo os valores pares de {numeros_sorteados}, temos {numeros_pares}')
