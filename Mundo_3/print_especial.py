21d49bf1e4bc5ad2a13702c404b0dc78ee8f03bc
GiovaneSilvaxss <giovane.silva-youxlab@institutoyoux.org.br>
2025-10-08 17:22:08 -0300

    Um print especial

---
+++ Mundo_3/print_especial.py
@@ -0,0 +1,11 @@
+def escreva(mensagem):
+    tam = len(mensagem) + 4
+    print('~' * tam)
+    print(f' {mensagem}')
+    print('~' * tam)
+
+
+# PROGRAMA PRINCIPAL
+escreva('Giovane Silva')
+escreva('Giovane cursando mundo 3')
+escreva('Py')
