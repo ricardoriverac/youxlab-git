01bbafd4a26b4bae946ffced632b117f01ef1fef
agathae <agatha.silva-youxlab@institutoyoux.org.br>
2025-10-08 16:46:40 -0300

    EXERCICIO 109,108,MOEDA aula 22

--- python3/aula.22/moeda.py
+++ python3/aula.22/moeda.py
@@ -1,22 +1,24 @@
-def aumentar (num):
-  return num +1
+def aumentar (preco=0, taxa=0, formato = False):
+  res = preco + (preco* taxa/100)
+  return res if formato  is False else real(preco)
     
 
-def real(num = 0,real = 'R$'):
-   return f'{real}{num}'.replace('.',',')
-
-
-def diminuir(num):
-    return num -1
+def diminuir(preco= 0, taxa=0, formato = False):
+   res = preco - (preco * taxa/100)
+   return res if formato is False else real(res)
     
 
 
-def dobro(num):
-   return num *2
+def dobro(preco =0, formato = False):
+   res = preco *2
+   return res if not formato else real(res)
    
 
 
-def metade(num):
-   return num  / 2
+def metade(preco=0,formato = False):
+   res = preco / 2
+   return res if not formato else real(res)
 
 
+def real(num = 0,real = 'R$'):
+   return f'{real}{num}'.replace('.',',')
