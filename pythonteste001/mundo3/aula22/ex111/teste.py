11869d54d5ed0230a264a83d4804dbd52918400f
Youx <heronherin10@gmail.com>
2025-10-07 22:32:58 -0300

    Exercício 111

--- pythonteste001/mundo3/aula22/ex111/teste.py
+++ pythonteste001/mundo3/aula22/ex111/teste.py
@@ -1,8 +1,5 @@
-import moeda
+from utilidadescev import moeda
 
 p = float(input('Preço: R$'))
 t = int(input('Qual será a taxa sobre o preço? '))
-print(f'O aumento de {t}% em {moeda.moeda(p)} é {moeda.aumentar(p, t, True)}')
-print(f'O desconto de {t}% em {moeda.moeda(p)} é {moeda.diminuir(p, t, True)}')
-print(f'O dobro de {moeda.moeda(p)} é {moeda.dobro(p, True)}')
-print(f'A metade de {moeda.moeda(p)} é {moeda.metade(p, True)}')
+moeda.resumo(p, t)
