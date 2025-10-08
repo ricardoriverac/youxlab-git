777b2ff5de604ceba5973ecd5946f9a2cf94fc5f
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-10-08 13:27:22 -0300

    eu terminei o desafio 106 do mundo 3

---
+++ pythonfile/mundo3/desafios_mundo3/d0106.py
@@ -0,0 +1,36 @@
+# Dicionário de cores
+cores = {
+    'limpa': '\033[m',
+    'azul': '\033[1;34m',
+    'verde': '\033[1;32m',
+    'vermelho': '\033[1;31m',
+    'amarelo': '\033[1;33m',
+    'fundo': '\033[7;37m'
+}
+
+
+def titulo(msg, cor='limpa'):
+
+    print(cores[cor], end='')
+    print('-' * (len(msg) + 4))
+    print(f"  {msg}")
+    print('-' * (len(msg) + 4))
+    print(cores['limpa'], end='')
+
+
+def mostrar_ajuda(comando):
+    
+    titulo(f"Acessando o manual de '{comando}'", 'amarelo')
+    print(cores['fundo'], end='')
+    help(comando)
+    print(cores['limpa'], end='')
+
+
+while True:
+    titulo("SISTEMA DE AJUDA PyHELP", 'azul')
+    opc = input(f"{cores['verde']}Função ou biblioteca > {cores['limpa']}").strip()
+    if opc.upper() == 'FIM':
+        break
+    mostrar_ajuda(opc)
+
+titulo("ATÉ LOGO! ", 'vermelho')
