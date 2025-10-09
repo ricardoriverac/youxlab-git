f0ed8c654341f752e009e75ad51210ab53d159e3
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2025-10-09 14:51:51 -0300

    Desafio 84 - Lista composta e análise de dados

--- Estudos/python3/aula16/desafio076.py
+++ Estudos/python3/aula16/desafio076.py
@@ -1,12 +1,12 @@
 print('LISTAGEM DE PREÇOS')
 pontos= '.' * 15
-tupla= f"""
+tupla= (f"""
            Lápis {pontos}R$  1.75
         Borracha {pontos}R$  2.00
          Caderno {pontos}R$ 15.90
           Estojo {pontos}R$ 25.00
     Transferidor {pontos}R$  4.20
         Compasso {pontos}R$  9.99
-        Mochila {pontos}R$ 120.32"""
+        Mochila {pontos}R$ 120.32""")
 print(tupla)
         
--- Estudos/python3/aula18/desafio084.py
+++ Estudos/python3/aula18/desafio084.py
@@ -1,23 +1,35 @@
 dados= list()
 bancoDados= list()
 listaMaior = list()
+nome=list()
+NomeMaior= list()
+nomeMenor=list()
 listaMenor = list()
-continuacao= 'dsada'
+pessoas=list()  
+continuacao= 'S'
 maior = 0
 menor = 9999
 while continuacao not in 'N':
-    nome= (str(input('Cadastre o nome da pessoa: ')))
-    idade= (int(input(f'Cadastre o peso de {dados}')))
-    dados.append(nome)
-    dados.append(idade)
+    nome.append((str(input('Cadastre o nome da pessoa: '))))
+    dados.append(nome[:])
+    peso= (float(input(f'Cadastre o peso de {dados}')))
+    dados.append(peso)
     bancoDados.append(dados[:])
-    for nome in dados:
-        if idade > maior:
-            maior= idade
-        if idade < menor:
-            menor= idade
+    if peso > maior:
+        maior= peso
+        NomeMaior.clear()
+        NomeMaior.append(nome[:])
+    elif peso == maior:
+        NomeMaior.append(nome[:])
+    if peso < menor:
+        nomeMenor.clear()
+        nomeMenor.append(nome[:])
+        menor= peso
+    elif peso == menor:
+        nomeMenor.append(nome[:])
+    nome.clear()
     dados.clear()
     continuacao= str(input('Deseja continuar? [S/N]'))
 print(f'Ao todo você cadastrou {len(bancoDados[0])} pessoas')
-print(f'O maior peso foi  {maior}. Peso de {nome}')
-print(f'O menor peso foi {menor}. Peso de {nome} ')
+print(f'O maior peso foi  {maior}. Peso de {NomeMaior}')
+print(f'O menor peso foi {menor}. Peso de {nomeMenor}')
