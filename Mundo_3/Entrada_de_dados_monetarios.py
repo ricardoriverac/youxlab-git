1e87d1bc3a6c8fa57f6749931027f47cc69b59bc
GiovaneSilvaxss <giovane.silva-youxlab@institutoyoux.org.br>
2025-10-09 15:22:51 -0300

    Preço valid e preço invalido

---
+++ Mundo_3/Entrada_de_dados_monetarios.py
@@ -0,0 +1,5 @@
+from utilidadescev import moeda
+from utilidadescev import dados
+
+p = dados.leiadinheiros('Digite um preço:')
+moeda.resumo(p, 35, 22)
--- Mundo_3/Formatando_moedas.py
+++ Mundo_3/Formatando_moedas.py
@@ -1,4 +1,4 @@
-from ex108 import moeda
+from utilidadescev import moeda
 p = float(input('Digite o valor: '))
 print(f'A metade de {moeda.moeda(p)} é {moeda.moeda(moeda.metade(p))}')
 print(f'O dobro de {moeda.moeda(p)} é {moeda.moeda(moeda.dobro(p))}')
