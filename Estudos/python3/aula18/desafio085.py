164ef478f22ba774da8d9ac2c8664caa2f1201f0
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2025-10-09 15:08:13 -0300

    Desafio 85 - Pares e impares com lista

---
+++ Estudos/python3/aula18/desafio085.py
@@ -0,0 +1,16 @@
+pares=list()
+impares=list()
+numeros=list()
+count=0
+for c in range (0, 7+1):
+    count+=1
+    numero=(int(input(f'Digite o {count}o valor: ')))
+    numeros.append(numero)
+    if numero % 2 ==0:
+        pares.append(numeros[:])
+    if numero % 2 == 1:
+        impares.append(numeros[:])
+    numeros.clear()
+print(f'Os numeros pares digitados foram {pares}')
+print(f'Os numeros ímpares digitados foram {impares}')
+    
