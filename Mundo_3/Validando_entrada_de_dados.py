1997d9522d5876f29c44a289b719d78c493364d6
GiovaneSilvaxss <giovane.silva-youxlab@institutoyoux.org.br>
2025-10-09 13:02:08 -0300

    Digite um numero inteiro,se não for enteiro vai dar erro!

---
+++ Mundo_3/Validando_entrada_de_dados.py
@@ -0,0 +1,18 @@
+def leiaInt(mensagem):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(mensagem))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[0;31mERRO! Digite  um número inteiro válido.\033[m')
+        if ok:
+            break
+    return valor 
+
+
+# PROGRAMA PRINCIPAL 
+n = leiaInt('Digite um número:')
+print(f'Você acabou de digitar o número {n}')
