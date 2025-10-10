61d93887d1444df17b58d1f9d8712f4efd8e6b44
Youx <isabelamilene74@gmail.com>
2025-10-10 16:58:26 -0300

    ex092 Carteira de trabalho

---
+++ python/aula 19/ex092.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+dados = dict()
+dados['Nome'] = str(input('Digite seu nome: '))
+ano_nasc = int(input('Digite seu ano de nascimento: '))
+dados['Idade'] = datetime.now().year - ano_nasc
+dados['CTPS'] = int(input('Sua carteira de trabalho (Digite 0 se não tiver): '))
+if dados['CTPS'] !=0:
+    dados['Contratação'] = int(input('Ano de Contratação: '))
+    dados['Salário'] = float(input('Salário: R$'))
+    dados['Aposentadoria'] = dados['Idade'] + ((dados['Contratação'] + 35) - datetime.now().year)
+for k, v in dados.items():
+    print(f'{k} é {v}')
