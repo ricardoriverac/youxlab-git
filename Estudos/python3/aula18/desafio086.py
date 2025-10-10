fb48a613a9aa352f55f26f04cec210ed8c7e5292
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2025-10-10 14:23:26 -0300

    Modificação desafio 086 - Matriz com listas compostas

--- Estudos/python3/aula18/desafio086.py
+++ Estudos/python3/aula18/desafio086.py
@@ -1,6 +1,9 @@
 matriz= [[0,0,0], [0,0,0], [0,0,0]]
+maior=0
+soma=0
+sum=0
 for i in range (3):
     for j in range (3):
-        matriz[i][j]= input(f'Digite um valor para {i} x {j}')
+        matriz[i][j] = int(input(f'Digite um valor para [{i}] x [{j}]: '))
 for e in matriz: 
             print(e)
