9453875bae4bdc69fd66edea7aaaefaf7bef87a7
Emily <emily.ferreira-youxlab@institutoyoux.org.br>
2025-10-10 16:47:45 -0300

    exemplo 4 da aula 18

--- Pyhton/Mundo_3/aula_18/exemplo3.py
+++ Pyhton/Mundo_3/aula_18/exemplo3.py
@@ -1,3 +1,4 @@
 galera = [['João', 9], ['Ana, 10'], ['Mavi', 15]]
 for p in galera:
     print(p)
+    #mostra tudo, incluindo os colchetes
---
+++ Pyhton/Mundo_3/aula_18/exemplo4.py
@@ -0,0 +1,5 @@
+galera = [['João', 9], ['Ana, 10'], ['Mavi', 15]]
+for p in galera:
+    print(p)
+    print(f'{p[0]} tem {p[1]} anos de idade.')
+    #mostra a mesma coisa dos últimos dois, só acrescenta tudo em uma frase.
