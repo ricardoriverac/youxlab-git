8e0cfb12a7d9f5b4dea84305727ef87baa219d71
GiovaneSilvaxss <giovane.silva-youxlab@institutoyoux.org.br>
2025-10-10 14:17:38 -0300

    Update Site_acessivel?.py

--- Mundo_3/Site_acessivel?.py
+++ Mundo_3/Site_acessivel?.py
@@ -1,9 +1,1 @@
-import urllib
-import urllib.request
 
-try:
-    site = urllib.request.urlopen('http://www.pudim.com.br')
-except urllib.error.URLERROR:
-    print('O site Pudim esta acessivel no momento.')
-else:
-    print('Consegui acessar o site Pudim com sucesso!')
