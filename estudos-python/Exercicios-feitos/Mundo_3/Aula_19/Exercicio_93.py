6847efa7a05e8560c7497ce1f36edaefce0cc44b
pietromarinho22 <pietrovmarinho@gmail.com>
2025-10-10 17:48:58 -0300

    Exercício 93

--- estudos-python/Exercicios-feitos/Mundo_3/Aula_19/Exercicio_93.py
+++ estudos-python/Exercicios-feitos/Mundo_3/Aula_19/Exercicio_93.py
@@ -1,9 +1,18 @@
-contador = 0
+jogador = {}
+golsFeitos = []
+jogador['nome'] = input('Qual o nome do jogar?')
+quantidadePartida = int(input('Quantas partidas'))
+contagem = 0
 soma = 0
-while True:
-    numero = int(input('Escreva'))
-    if numero == 999:
-        break
-    contador += 1
-    soma += numero
-print (soma/contador)
+for c in range (quantidadePartida):
+    golsFeitos.append(int(input(f'Quanto gols ele fez na partida {c}: ')))
+    jogador['gols'] = (golsFeitos.copy())
+    soma += golsFeitos[c]
+jogador['total'] = soma
+print (jogador)
+for k, v in jogador.items():
+    print (f'O campo {k} tem o valor {v}')
+print (f'O jogador {jogador["nome"]} jogou {quantidadePartida} partidas')
+for p in range (0,quantidadePartida):
+    print (f'Na partida {p}, fez {jogador["gols"][p]}')
+print (f'Foi um total de {jogador["total"]} gols')
