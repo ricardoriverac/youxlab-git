0f92067810337c351a3ce9a2dfa8af58c188f6d4
Youx <deivs.abreu-youxlab@institutoyoux.org.br>
2025-10-10 14:15:16 -0300

    finalmente conclui o 93

---
+++ mundo3/desafio92.py
@@ -0,0 +1,22 @@
+nome = (input('qual seu nome? '))
+nascimento = (int(input('quando voce nasceu? ')))
+idade = 2025 - nascimento 
+if idade < 18:
+    print(f'sua idade é de {idade}')
+    print('você ainda não pode trabalhar!!')
+else:
+    cdt = input('carteira de trabalho: ')
+    contratadoem = input('ano de contratação: ')
+    salario = int(input('Qual seu salario?: '))
+    idade = 2025 - nascimento
+print('=' * 35)
+print (f'seu nome é {nome}')
+print('-' * 35)
+print (f'você nasceu em {nascimento}')
+print('-' * 35)
+print (f'sua carteira de trabalho é {cdt}')
+print('-' * 35)
+print (f'você foi contratado em {contratadoem}')
+print('-' * 35)
+print (f'seu salario é de {salario}')
+print('=' * 35)
---
+++ mundo3/desafio93.py
@@ -0,0 +1,9 @@
+jogador = []
+partidas = []
+nome = str(input('jogador: '))
+tot = int(input('quantas partidas?: '))
+for c in range(0,tot):
+    partidas.append(int(input(f'quantos gols na partida {c}?')))
+total = sum(partidas)
+print (f'o total de gols em {tot} partidas foi de {total} sendo:')
+print(f'{partidas}')
