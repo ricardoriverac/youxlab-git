07f30d5f4f3e2b22972dada818558007b7ec3570
Lucas RibYouX <lucas.pereira-youxlab@institutoyoux.org.br>
2025-10-12 16:53:30 -0300

    Adicionando exercício 104 corrigido

--- python/aula21/exercicio105.py
+++ python/aula21/exercicio105.py
@@ -1,27 +1,17 @@
 def notas(*n, sit=False):
     boletim = {}
-    nota = float(n)
-    boletim['notas'] = nota
-    maior = menor = count = media = valor = 0
-    for c in range(0, notas):
-        count += 1
-        if count == 1:
-            maior = menor = c
-        else: 
-            if c > maior: 
-                maior = c
-                boletim['maior'] = maior
-            if c < menor:
-                menor = c
-                boletim['menor'] = menor
-    while True:
-        if boletim['media'] >= 5:
-            print('TURMA APROVADA')
-            sit = True
+    boletim['total'] = len(n)
+    boletim['maior'] = max(n)
+    boletim['menor'] = min(n)
+    boletim['media'] = sum(n)/len(n)
+    if sit:
+        if boletim['media'] >= 7:
+            boletim['situação'] = 'BOA'
+        elif boletim['media'] >= 5:
+            boletim['situação'] = 'RAZOÁVEL'
         else:
-            print('TURMA REPROVADA')
-        if sit:
-            break
-    print(boletim)
-resp = notas(5, 2, 1, sit=True)
-        
+            boletim['situação'] = 'RUIM'
+    return boletim
+
+resp = notas(5.5, 2.5, 1.5, sit=True)
+print(resp)        
