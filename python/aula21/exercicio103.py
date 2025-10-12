b82eef4fae7f92b4169db4ee00d3fe04632b8c52
Lucas RibYouX <lucas.pereira-youxlab@institutoyoux.org.br>
2025-10-12 15:28:25 -0300

    Novo versionamento exercicio103

--- python/aula21/exercicio103.py
+++ python/aula21/exercicio103.py
@@ -1,10 +1,17 @@
-def jogador(nome = ' ', gols = ' ' ):
-    if nome in '':
-        nome = '<desconhecido'
-    if gols < '1':
-        gols = '0'
+def jogador(nome = '<desconhecido>', gols = 0 ):
     print(f'O jogador {nome} fez {gols} gol(s).')
 
 jog = str(input('Nome: '))
 gol = str(input('Gols: '))
 jogador(jog, gol)
+if gol.isnumeric():
+    gol = int(gol)
+else:
+    gol = 0
+if jog.strip() == '':
+    jogador(gols=gol)
+else: 
+    jogador(jog, gol)
+'''
+o isnumeric irá ler a string digitad e fará a "validação" para descobrir se o que foi digitado é ou não é um número
+'''
