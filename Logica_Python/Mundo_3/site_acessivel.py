49996edee7e5ce4cb9796a500c337640b4c90fbc
GiovaneSilvaxss <giovane.silva-youxlab@institutoyoux.org.br>
2025-10-13 13:21:59 -0300

    lista de numeros digitados

--- Logica_Python/Mundo_3/site_acessivel.py
+++ Logica_Python/Mundo_3/site_acessivel.py
@@ -1,25 +1,9 @@
-import requests
+import urllib
+import urllib.request
 
-
-def testsite(url):
-    '''
-    testar sites de internet
-    :param url: endereço para teste ex: www.ninoseg.com.br
-    :return: informação se está acessivel ou nao
-    '''
-
-    try:
-        response = requests.get(f'http://{url}', timeout=5)
-    except:
-        print('Error 404: Page not found.')
-    else:
-        print('A página requisitada está acessível.')
-
-
-while True:
-    url = str(input('Digite um site: '))
-    if url == "0":
-        print('\033[30;41mObrigado volte novamente.\033[m')
-        break
-    else:
-        testsite(url)
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except urllib.error.URLERROR:
+    print('O site Pudim consegue ter acesso.')
+else:
+    print('Consegui acessar o site Pudim!')
