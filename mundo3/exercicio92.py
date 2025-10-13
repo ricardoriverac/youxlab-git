4843e64440677fa2e2ce79ff8c713a39c1a17fef
Youx <juan.ferreira-youxlab@institutoyoux.org.br>
2025-10-13 12:35:17 -0300

    adiciona e mostra vario valores sobre a carteira de trabalho

---
+++ mundo3/exercicio92.py
@@ -0,0 +1,20 @@
+dici = {}
+
+dici['nome'] = str(input('Nome: '))
+anodn = int(input('Ano de nascimento: '))
+dici['idade'] = 2025 - anodn
+dici['carteira'] = int(input('Carteira de trabalho (0 para nenhuma): '))
+if dici['carteira'] != 0:
+    dici['ano de contratação'] = int(input('Ano de contratação: '))
+    dici['salario'] = int(input('Salario: R$ '))
+else:
+    pass
+
+print (dici)
+print (f'O nome tem o valor {dici["nome"]}')
+print (f'A idade tem o valor {dici["idade"]}')
+if dici['carteira'] != 0: 
+    print (f'A ctps tem o valor {dici["carteira"]}')
+    print (f'Contratação tem o valor {dici["ano de contratação"]}')
+    print (f'O salalrio tem o valor {dici["salario"]}')
+
