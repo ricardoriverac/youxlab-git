c1deb4552bab925e272949bf6ea1262d3f0c832c
joao gabriel borges <joaogabrielborges052@gmail.com>
2025-10-13 18:45:09 -0300

    atividade107

---
+++ mundo 3/aula22/atividade107.py
@@ -0,0 +1,3 @@
+import moeda
+preço = float(input("digite o preço: "))
+print(f"a metade de {preço} e {moeda.metade(preço)}")
---
+++ mundo 3/aula22/moeda.py
@@ -0,0 +1,13 @@
+def aumentar(preço=0,taxa=0):
+    res = preço + (preço*taxa/100)
+
+def diminuir(preço=0,taxa = 0):
+    res = preço - (preço*taxa/100)
+
+def dobro(preço=0)  :
+    return preço * 2
+
+def metade(preço=0):
+
+    return preço / 2
+      
