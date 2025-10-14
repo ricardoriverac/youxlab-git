24a3cc698e9eadefca9dd079247384cf0ddee18e
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2025-10-14 09:17:33 -0300

    Desafio 91, ranking de jogo de dados

--- Estudos/python3/aula19/desafio91.py
+++ Estudos/python3/aula19/desafio91.py
@@ -1,8 +1,14 @@
 import random
+import operator
 jogadores=dict()
-for c in range(0,5):
+jogadoresOrdenados=[]
+for c in range(1,6):
     sorteio= random.randint(0,6)
     jogadores[f'jogador{c}']= sorteio
 for jogador, valor in jogadores.items():
     print(f'O {jogador} tirou o valor {valor} no dado')
-sorted(jogadores, reverse=True)
+
+jogadoresOrdenados = (sorted(jogadores.items(), key = operator.itemgetter(1), reverse=True))
+print(jogadoresOrdenados)
+for i, v in enumerate (jogadoresOrdenados):
+    print(f'{i+1}º lugar: {v[0]} com {v[1]}')
