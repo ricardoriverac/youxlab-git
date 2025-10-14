7f12441ffcb22996661091f8caeb676509a6086e
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-10-14 18:54:14 -0500

    ex100 feito - corrigido

--- python/aula_20/ex100.py
+++ python/aula_20/ex100.py
@@ -6,8 +6,19 @@ soma entre todos os valores pares sorteados pela função anterior.
 
 from random import randint
 
+def sorteio(lista):
+    for i in range(0, 5):
+        lista.append(randint(0, 5))
+    print(f'Lista com os números sorteados: {numeros}')
 
-def sorteia(num):
-    randint (0,6)
 
-lista_numero = [sorteia]
+def somapar(num):
+    soma = 0
+    for valor in numeros:
+        if valor % 2 == 0:
+            soma += valor
+    print(f'A soma dos números pares é: {soma}')
+
+numeros = []
+sorteio(numeros)
+somapar(numeros)
