2dfaa5930ef00493e6361b64e1ac9e6392fb1978
João Othávio <joaoothaviofabri14@gmail.com>
2025-10-14 11:22:35 -0300

    Exercício 082 feito

---
+++ curso-de-python/mundo-3/video-cursos/curso-python-017/exercicios/ex082.py
@@ -0,0 +1,32 @@
+numeros = []
+pares = []
+impares = []
+continuar = ''
+
+while True:
+    numero = int(input('Digite um número: '))
+    numeros.append(numero)
+
+    if numero % 2 == 0:
+        pares.append(numero)
+
+    if numero % 2 != 0:
+        impares.append(numero)
+
+    continuar = str(input('Deseja continuar? [S/N] ')).upper()
+    if continuar == 'N':
+        print('-' * 25)
+        print('\033[31mPrograma Finalizado.\033[m')
+        print('-' * 25)
+        break
+
+    if continuar != 'S':
+        print('-' * 25)
+        print('\033[31mResposta Inválida.\033[m')
+        print('-' * 25)
+        break
+
+
+print(f'O números que \033[33mvocê\033[m digitou são: \033[33m{numeros}\033[m')
+print(f'Os números \033[36mpares\033[m digitados são: \033[33m{pares}\033[m')
+print(f'Os números \031[35mimpares\033[m digitados são: \033[33m{impares}\033[m')
