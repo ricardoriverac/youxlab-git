5488602fb7eb97d3a7ca3bccfc552930eb9c278c
pietromarinho22 <pietrovmarinho@gmail.com>
2025-10-14 15:27:36 -0300

    Melhora no Exercício 97

--- estudos-python/Exercicios-feitos/Mundo_3/Aula_20/Exercício_97.py
+++ estudos-python/Exercicios-feitos/Mundo_3/Aula_20/Exercício_97.py
@@ -1,5 +1,5 @@
 def escreva(escrita):
-    print('='*(len(escrita) + 4))
-    print(f'  {escrita}')
-    print('='*(len(escrita) + 4))
+    print('|','='*(len(escrita) + 4),'|')
+    print('|',f'  {escrita}  ','|')
+    print('|','='*(len(escrita) + 4),'|')
 escreva(str(input('Escreva alguma coisa!:\n->')))
