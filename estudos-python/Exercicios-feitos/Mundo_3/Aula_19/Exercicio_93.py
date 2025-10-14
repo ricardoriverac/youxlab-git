1c50586a9edd6bf43ee8838593719a91be612243
pietromarinho22 <pietrovmarinho@gmail.com>
2025-10-14 11:44:49 -0300

    Exercício 95

--- estudos-python/Exercicios-feitos/Mundo_3/Aula_19/Exercicio_93.py
+++ estudos-python/Exercicios-feitos/Mundo_3/Aula_19/Exercicio_93.py
@@ -1,15 +1,21 @@
 jogador = {}
 golsFeitos = []
-jogador['nome'] = input('Qual o nome do jogar?')
-quantidadePartida = int(input('Quantas partidas'))
 contagem = 0
 soma = 0
-for c in range (quantidadePartida):
-    golsFeitos.append(int(input(f'Quanto gols ele fez na partida {c}: ')))
-    jogador['gols'] = (golsFeitos.copy())
-    soma += golsFeitos[c]
-jogador['total'] = soma
-print (jogador)
+while True:
+    jogador['nome'] = input('Qual o nome do jogador?')
+    quantidadePartida = int(input('Quantas partidas?'))
+    for c in range (quantidadePartida):
+        golsFeitos.append(int(input(f'Quanto gols ele fez na partida {c}?: ')))
+        jogador['gols'] = (golsFeitos.copy())
+        soma += golsFeitos[c]
+    jogador['total'] = soma
+    soma = 0
+    print (jogador)
+    golsFeitos.clear()
+    opcao = input('Você quer continuar?\n Se não for digite "N" ou "n": ')
+    if opcao == 'N':
+        break
 for k, v in jogador.items():
     print (f'O campo {k} tem o valor {v}')
 print (f'O jogador {jogador["nome"]} jogou {quantidadePartida} partidas')
---
+++ estudos-python/Exercicios-feitos/Mundo_3/Aula_19/Exercicio_95.py
@@ -0,0 +1,30 @@
+jogador = {}
+todoJogador = []
+golsFeitos = []
+contagem = 0
+soma = 0
+while True:
+    jogador['nome'] = input('Qual o nome do jogador?\n->').strip().capitalize()
+    quantidadePartida = int(input('Quantas partidas?\n->'))
+    for c in range (quantidadePartida):
+        golsFeitos.append(int(input(f'Quanto gols ele fez na partida {c}?\n->')))
+        jogador['gols'] = (golsFeitos.copy())
+        soma += golsFeitos[c]
+    jogador['total'] = soma
+    soma = 0
+    todoJogador.append(jogador.copy())
+    print (jogador)
+    golsFeitos.clear()
+    opcao = input('Você quer continuar?\n Se não for digite "N" ou "n": ').strip().upper()
+    if opcao == 'N':
+        break
+busca = 0
+while busca != 999:
+    busca = int(input('Qual jogador você gostaria de ver os dados?\nSe quiser encerrar digite 999!\n->'))
+    while busca<0 or busca>len(todoJogador):
+        print('Esse número está fora do alcance da lista!')
+        print('Escreva um número dentro do alcance da lista!')
+        busca = int(input('Qual jogador você gostaria de ver os dados?\nSe quiser encerrar digite 999!\n->'))
+        if busca == 999:
+            break
+    print(todoJogador[busca])
