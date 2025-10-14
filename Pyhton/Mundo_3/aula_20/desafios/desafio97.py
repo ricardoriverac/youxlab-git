fe879af5ec63434dc563bcc7c217c1c164772e72
Emily <emily.ferreira-youxlab@institutoyoux.org.br>
2025-10-14 13:24:58 -0300

    desafio 97

---
+++ Pyhton/Mundo_3/aula_20/desafios/desafio97.py
@@ -0,0 +1,10 @@
+def escreva(palavra):
+    tamanho = len(palavra) + 4
+    print(tamanho * '~')
+    print(f'  {palavra}')
+    print(tamanho * '~')
+
+# iniciando o programa:
+
+escreva('Olá mundo! ')
+escreva('Tenha um bom dia! ')
