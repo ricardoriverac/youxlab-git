423d9762a918ae5e225f6e3e03571a05e7c665bd
Youx <juan.ferreira-youxlab@institutoyoux.org.br>
2025-10-14 01:39:07 -0300

    faz uma função para saber a area

---
+++ mundo3/exercicio96.py
@@ -0,0 +1,6 @@
+def area():
+    larg = int(input('Digite a largura: '))
+    alt = int(input('Digite a altura: '))
+    mq = larg * alt
+    print (f'A largura é {larg}, a altura é {alt}, e a area é {mq}')
+area()
