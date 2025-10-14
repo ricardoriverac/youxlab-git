5beb65f996c8baa76b81f2045f5e67a2f3aaa83d
juliaassis00 <juliaass92@gmail.com>
2025-10-14 08:32:39 -0300

    Salvando teoria

---
+++ Python/Mundo 3/aula20/teoria.py
@@ -0,0 +1,57 @@
+# Curso Python #20 - Funções (Parte 1)
+# Nessa aula, vamos aprender o que são funções ou rotinas e como utilizar funções em Python. 
+# Funções são trechos de código que podem ser executados em momentos diferentes de nossos códigos
+#  em Python. Veja como funciona o comando def em Python e como utilizá-lo com parâmetros simples e múltiplos.
+
+# def mostrar_mensagem():
+#     print('Olá, isso é uma função!')
+
+# mostrar_mensagem()
+# mostrar_mensagem()
+# mostrar_mensagem()
+
+# def saudacao(nome):
+#     print(f'Olá, {nome}! Bem-vindo(a)!')
+
+# saudacao('Maria')
+# saudacao('João')
+
+# def soma(a, b):
+#     return a + b
+
+# res = soma(5, 7)
+# print(f'O resultado da soma é {res}')
+# def exibir_numero(n=0):
+#     print(f'O número é {n}')
+
+# exibir_numero(10)
+# exibir_numero()  # usará o valor padrão 0
+
+
+# def par_impar(n):
+#     if n % 2 == 0:
+#         return 'Par'
+#     else:
+#         return 'Ímpar'
+
+# numero = int(input('Digite um número: '))
+# print(f'O número {numero} é {par_impar(numero)}.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
