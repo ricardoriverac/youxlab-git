8d1591ad00ff2e30ebeede8d4d88432d09db28ff
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-10-14 11:22:15 -0500

    ex099 feito - corrigido

--- python/aula_20/ex099.py
+++ python/aula_20/ex099.py
@@ -14,13 +14,7 @@ numeros = [0, 9,3, 2, 6, 5, 1, 10]
 maior_ = max(numeros)
 menor = min(numeros)
 
-print(' ')
 
-print(f'O maior número da lista foi: {maior_}')
-
-print('--' * 20)
-
-print(f'O menor número da lista foi: {menor}')
-
-print(' ')
-print('==' * 20)
+maior(maior_)
+maior(menor)
+print(maior_)
---
+++ python/aula_20/ex100.py
@@ -0,0 +1,13 @@
+'''
+ Faça um programa que tenha uma lista chamada números e duas funções chamadas sorteia() e somaPar().
+A primeira função vai sortear 5 números e vai colocá-los dentro da lista e a segunda função vai mostrar a
+soma entre todos os valores pares sorteados pela função anterior.
+'''
+
+from random import randint
+
+
+def sorteia(num):
+    randint (0,6)
+
+lista_numero = [sorteia]
