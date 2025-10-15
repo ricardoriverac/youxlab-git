14c5e064072023a25a19cee8198c8af093c9cb80
Emily <emily.ferreira-youxlab@institutoyoux.org.br>
2025-10-15 14:09:10 -0300

    desafio 108

---
+++ Pyhton/Mundo_3/aula_22/desafio108.py
@@ -0,0 +1,6 @@
+from desafio107 import moeda
+
+preco = float(input('Digite um valor:'))
+print(f'A metade de R${moeda.real(preco)} é {moeda.real(moeda.metade(preco))}')
+print(f'O dobro de {moeda.real(preco)} é {moeda.real(moeda.dobro(preco))}')
+print(f'Aumentando 10%, temos {moeda.real(moeda.aumentar(preco+10))}')
