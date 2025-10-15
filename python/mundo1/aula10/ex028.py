37c97168638b6f0935130ae00eecd8c43426ee47
Thawane <thawane.correa@institutoyoux.org.br>
2025-10-15 15:16:50 -0300

    ex028

--- python/mundo1/aula10/ex028.py
+++ python/mundo1/aula10/ex028.py
@@ -2,3 +2,18 @@
 # e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.
 # O programa deverá escrever na tela se o usuário venceu ou perdeu.
 
+import random
+numero_advinhar = random.randint(0,5)
+tentativa = 1
+max_tentativas = 5
+while tentativa < max_tentativas:
+    tentativa += 1
+palpite = int(input('Advinhe o número que estou pensando: '))
+if palpite < numero_advinhar:
+    print('Número muito baixo, tente novamente!')
+elif palpite > numero_advinhar:
+    print('Muito alto, tente novamente')
+else:
+    print(f'Muito bem, você acertou {numero_advinhar}, em {tentativa} tentativas!')
+
+
