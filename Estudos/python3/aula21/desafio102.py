f66a0f6817f765e83bf684e663744f8bb3d3b20b
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2025-10-15 11:27:01 -0300

    Desafio 102- Função fatorial

---
+++ Estudos/python3/aula21/desafio102.py
@@ -0,0 +1,20 @@
+def fatorial (num=1, show=False):
+    operacao=[]
+    """fatorial(n,show=false)
+           -> Calcula o fatorial de um número.
+              :param num: o numero a ser calculado
+              : param show: (opcional) Mostrar ou não a conta
+              : param return: o valor do Fatorial é um número n"""
+    ocultamento=str(input('Escolha se quer ver apenas o resultado final ou a operação completa [RF/O]').upper())
+    f=1
+    for c in range (num, 0, -1):
+        f*=c
+        operacao.append(f"{c}")
+    if ocultamento == 'O':
+        resultado = f"{' x '.join(operacao)}"
+        return f"{resultado} = {f}"
+
+    else:
+        return f
+n=int(input('Escolha seu numero fatorial: '))
+print(f'O resultado da fatoração de {n} é {fatorial(n)}')
