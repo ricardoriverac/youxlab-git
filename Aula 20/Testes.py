a40346e2f40ce0148b9ae2b739e242f9a95e20b9
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2025-10-15 08:47:29 -0300

    Testes aula 20.

---
+++ Aula 20/Testes.py
@@ -0,0 +1,16 @@
+print('-' * 30)
+print('  CURSO EM VÍDEO  ')
+print('-' * 30)
+
+
+
+
+print('-' * 30)
+print('  CURSO EM VÍDEO  ')
+print('-' * 30)
+print('-' * 30)
+print('  APRENDA PYTHON  ')
+print('-' * 30)
+print('-' * 30)
+print('  MARCELA GUANABARA ')
+print('-' * 30)
