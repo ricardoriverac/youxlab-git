5b89d70b20bee2f2b5d2b1c5d988941ab48333f0
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2025-10-15 16:50:50 -0300

    Desafio 107 - Exercitando módulos em python

---
+++ Estudos/python3/aula22/desafio107/desafio107.py
@@ -0,0 +1,13 @@
+import moeda
+
+n=float(input('Digite o preço: R$ '))
+
+
+
+
+
+
+
+print(f'A metade do preço de R${n} é R${moeda.metade(n)}')
+print(f'O dobro do preço R${n} é R${moeda.dobro(n)}')
+print(f'O preço R${n} com aumento de 10% passa a valer R${moeda.aumento(n)}')
---
+++ Estudos/python3/aula22/desafio107/moeda.py
@@ -0,0 +1,12 @@
+def metade (m):
+    m=m/2
+    return m
+
+def dobro (d):
+    d=d*2
+    return d
+
+
+def aumento (a):
+    a=a * 1.10
+    return a
