a80837df33c0e0912c18f9a3f67c8d853b74f586
pietromarinho22 <pietrovmarinho@gmail.com>
2025-10-15 08:07:31 -0300

    Exercício 98

---
+++ estudos-python/Exercicios-feitos/Mundo_3/Aula_20/Exercicio_98.py
@@ -0,0 +1,17 @@
+from time import sleep
+contagem1 = 1
+contagem2 = 10
+print('1 até 10 de 1 em 1:')
+while contagem1 < 11:
+    print(contagem1)
+    contagem1 += 1
+print('10 até 0 de 2 em 2')
+while contagem2 > -1:
+    print(contagem2)
+    contagem2 -= 2
+print('Personalize sua pŕopria sequência:')
+def contador(a, b, c):
+    while a != b:
+        print(a, end=' > ')
+        a += c
+contador(a=int(input('Insira o número inicial: ')), b=int(input('Insira o número final: ')), c=int(input('Insira o número e a passagem: ')))
