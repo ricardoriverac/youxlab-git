01675850356c528a39e5c6eb392af4c06e830b44
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2025-10-15 11:02:24 -0300

    Exercicio 103, ficha nome do jogador e quantos gols foram feitos.

---
+++ Aula 21/ex103.py
@@ -0,0 +1,13 @@
+def ficha(nome, gols):
+    if not nome:
+        nome = 'desconhecido'
+    if gols.isnumeric():
+        gols = int(gols)
+    else:
+        gols = 0
+    return f'O jogador {nome} fez {gols} gols no campeonato!'
+
+
+nome = str(input('Nome do jogador: ')).strip()
+gols = str(input('Número de gols: '))
+print(ficha(nome, gols))
