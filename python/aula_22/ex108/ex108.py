a189016da48655a0796d28b2d3cc41a284a1de89
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-10-16 14:13:46 -0500

    ex108 feito

---
+++ python/aula_22/ex108/ex108.py
@@ -0,0 +1,5 @@
+import moeda
+n = float(input("Digite o preço:R$"))
+moeda.metade(n)
+moeda.dobro(n)
+moeda.aumentar(n)
---
+++ python/aula_22/ex108/moeda.py
@@ -0,0 +1,21 @@
+#MÓDULO:
+
+def dobro(a):
+    return print(f"O dobro de R${a} é R${a * 2:.2f}")
+def metade(a):
+    return print(f"A metade de R${a} é R${a /  2:.2f} ")
+
+def aumentar(a):
+    b = a * 10/100
+    return print(f"Aumentando em 10%, temos R${b + a:.2f}") 
+
+
+def porcento(n):
+    porc = (n * 10) / 100
+    valor = n + porc
+    return valor
+
+def porcentomenos(n):
+    porc = (n * 10) / 100
+    valor = n - porc
+    return valor
