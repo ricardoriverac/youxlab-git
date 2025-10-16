ac2c9949935eb08be403c1cdb50a4c7234b995ac
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-10-16 14:03:38 -0500

    teoria aula 22

--- python/aula_20/ex099.py
+++ python/aula_20/ex099.py
@@ -3,18 +3,27 @@ Faça um programa que tenha uma função chamada maior(), que receba vários par
 Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
 '''
 
-def maior(lista):
-    if not lista:
-        return None, None
 
-print('==' * 20)
+from time import sleep
 
-numeros = [0, 9,3, 2, 6, 5, 1, 10]
+def maior(*  num):
+    maximo = 0
+    print('-=' * 40)
+    print(f'Analisando os valores passados...')
+    for k, v in enumerate(num):
+        print(v, end=' ')
+        if k == 0:
+            maximo = v
+        else:
+            if v > maximo:
+                maximo = v
+        sleep(0.5)
+    print(f'Foram informados {len(num)} valores ao todo.')
+    print(f'O maior valor inserido foi {maximo}')
 
-maior_ = max(numeros)
-menor = min(numeros)
 
-
-maior(maior_)
-maior(menor)
-print(maior_)
+maior(5, 4, 7, 3, 1)
+maior(8, 9, 2, 7)
+maior(0, 6, 5)
+maior(10, 9)
+maior(1)
