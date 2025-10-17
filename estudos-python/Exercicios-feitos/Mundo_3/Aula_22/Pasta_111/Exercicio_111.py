f9586d5db406484d55ebff46e4fba4347635800c
pietromarinho22 <pietrovmarinho@gmail.com>
2025-10-17 09:08:31 -0300

    Exercício 111

---
+++ estudos-python/Exercicios-feitos/Mundo_3/Aula_22/Pasta_111/Exercicio_111.py
@@ -0,0 +1,4 @@
+from Utilidades.moeda import moeda
+
+num = float(input('Enter a number:\n->'))
+print(moeda.moneyator(num))
---
+++ estudos-python/Exercicios-feitos/Mundo_3/Aula_22/Pasta_111/Utilidades/dados/dados.py
@@ -0,0 +1,18 @@
+def money(n):
+    return f'R${n:.2f}'.replace('.',',')
+
+def increase(i, show=False):
+    show=True
+    return f'R${(i * 1.10):.2f}'.replace('.',',')
+
+def decrease(m, show=False):
+    show=True
+    return f'R${(m * 0.8):.2f}'.replace('.',',')
+
+def doubler(o, show=False):
+    show=True
+    return f'R${(o * 2):.2f}'.replace('.',',')
+
+def halver(p, show=False):
+    show=True
+    return f'R${(p / 2):.2f}'.replace('.',',')
