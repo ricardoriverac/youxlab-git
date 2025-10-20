b564d1e6a736f27d9723a4d44cdc53456145e4c3
luis2025git <luis.sousa-youxlab@institutoyoux.org.br>
2025-10-20 19:40:08 -0300

    exercicio 115, porem na hora de exportar a biblioteca que criei não está dando certo e está dando erro (fiz igual o guanabara)

---
+++ Python/Mundo 3/aula 23/d115/sistema.py
@@ -0,0 +1,30 @@
+from d115.lib.interface import *
+from d115.lib.arquivo import *
+from time import sleep
+
+arq = 'luislab.txt'
+
+if not arquivoexiste(arq): #Se não estiver criar um arquivo "arq"
+    criararquivo(arq) #cria um arquivo "arq"
+
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
+    if resposta == 1:
+        #Opção de listar o conteúdo do arquivo.
+        lerarquivo(arq)
+
+    elif resposta == 2:
+        #Opção de cadastrar uma pessoa nova.
+        cabecalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaint('Idade: ')
+        cadastrar(arq, nome, idade)
+
+    elif resposta == 3:
+        #Opção de sair do sistema.
+        cabecalho('Saindo do sistema... Até Logo!')
+        break
+    else:
+        #Digitou uma opção errada no menu.
+        print('\033[31mERRO! Digite uma opção válida!\033[m')
+    sleep(2)
