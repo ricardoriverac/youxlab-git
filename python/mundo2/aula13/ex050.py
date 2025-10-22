489ca05fa7516d308157ed5660602722bafec98b
Thawane <thawane.correa@institutoyoux.org.br>
2025-10-22 15:01:06 -0300

    ex050

---
+++ python/mundo2/aula13/ex050.py
@@ -0,0 +1,8 @@
+#Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares.
+# Se o valor digitado for ímpar, desconsidere-o.
+soma = 0
+for c in range(0, 6):
+    numero = int(input('Digite um número: '))
+    if numero % 2 == 0:
+     soma += numero
+print(f'A soma dos números pares é: {soma}')
