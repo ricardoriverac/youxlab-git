ef4f1f70ba28fb000197cdf82f5a47760069b999
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2025-10-29 18:17:21 -0300

    Aula 12 + Aula 13 concluidas

---
+++ Estudos/postgres/seção 2/aula12/aula12.sql
@@ -0,0 +1,76 @@
+
+CREATE TABLE clientes (
+    idcliente INTEGER NOT NULL,
+    nome VARCHAR(2253),
+    cpf CHAR(11),
+    rg VARCHAR(20),
+    data_nascimento DATE,
+    genero CHAR(1),
+    profissao VARCHAR(29),
+    nacionalidade VARCHAR(32),
+    logradouro VARCHAR(179),
+    numero VARCHAR(15),
+    complemento VARCHAR(200),
+    bairro VARCHAR(150),
+    municipio VARCHAR(300),
+    uf VARCHAR(5),
+    observacoes TEXT,
+
+	--Primary key
+    CONSTRAINT pk_cln_idcliente PRIMARY KEY (idcliente)
+);
+
+INSERT INTO clientes (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+VALUES (1, 'Manoel', '88828383821', '32323', '10/10/2001', 'M', 'Estudante', 'Brasileira', 'Rua Joaquim Nabuco', '23', 'Casa', 'Cidade Nova', 'Porto União', 'SC');
+
+INSERT INTO clientes (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+VALUES (2, 'Geraldo', '12343299291', '56565', '04/01/1987', 'M', 'Engenheiro', 'Brasileira', 'Rua das Limas', '200', 'AP.', 'Cientro', 'P. União', 'SC');
+
+INSERT INTO clientes (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+VALUES (3, 'Carlos', '87732323227', '55463', '01/10/1967', 'M', 'Pedreiro', 'Brasileira', 'Rua das Laranjeiras', '300', 'Apart.', 'Cto.', 'Canoinhas', 'SC');
+
+INSERT INTO clientes (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+VALUES (4, 'Adriana', '12321222122', '98777', '10/09/1989', 'F', 'Jornalista', 'Brasileira', 'Rua das Limas', '240', 'Casa', 'São Pedro', 'Porto Vitória', 'PR');
+
+INSERT INTO clientes (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+VALUES (5, 'Amanda', '99982838828', '28382', '04/03/1991', 'F', 'Jorn.', 'Italiana', 'Av.Central', '100', NULL, 'São Pedro', 'General Carneiro', 'PR');
+
+INSERT INTO clientes (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+VALUES (6, 'Ângelo', '99982828181', '12323', '01/01/2000', 'M', 'Professor', 'Brasileiro', 'Av. Beira Mar', '300', NULL, 'Ctr.', 'São Paulo', 'SP');
+
+INSERT INTO clientes (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+VALUES (7, 'Anderson', NULL, NULL, NULL, 'M', 'Prof.', 'Italiano', 'Av.Brasil', '100', 'Apartamento', 'Santa Rosa', 'Rio de Janeiro', 'SP');
+
+INSERT INTO clientes (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+VALUES (8, 'Camila', '9998282828', NULL, '10/10/2001', 'F', 'Professora', 'Norte americana', 'Rua da Central', '4333', NULL, 'Centro', 'Uberlância', 'MG');
+
+INSERT INTO clientes (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+VALUES (9, 'Cristiano', NULL, NULL, NULL, 'M', 'Estudante', 'Alemã', 'Rua do Centro', '877', 'Casa', 'Centro', 'Porto Alegre', 'RS');
+
+INSERT INTO clientes (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+VALUES (10, 'Fabricio', '8828282828', '32323', NULL, 'M', 'Estudante', 'Brasileiro', NULL, NULL, NULL, NULL, 'PU', 'SC');
+
+INSERT INTO clientes (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+VALUES (11, 'Fernanda', NULL, NULL, NULL, 'F', NULL, 'Brasileira', NULL, NULL, NULL, NULL, 'Porto União', 'SC');
+
+INSERT INTO clientes (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+VALUES (12, 'Gilmar', '88881818181', '888', '10/02/2000', 'M', 'Estud.', NULL, 'Rua das Laranjeiras', '200', NULL, 'Cidade N.', 'Canoinhas', 'SC');
+
+INSERT INTO clientes (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+VALUES (13, 'Diego', '1010191919', '111939', NULL, 'M', 'Professor', 'Alemão', 'Rua Central', '455', 'Casa', 'Cidade N.', 'São Paulo', 'SP');
+
+INSERT INTO clientes (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+VALUES (14, 'Jeferson', NULL, NULL, '01/07/1983', 'M', NULL, 'Brasileiro', NULL, NULL, NULL, NULL, 'União da Vitória', 'PR');
+
+INSERT INTO clientes (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+VALUES (15, 'Jessica', NULL, NULL, NULL, 'F', 'Estudante', NULL, NULL, NULL, NULL, NULL, 'União da Vitória', 'PR');
+
+select * from clientes;
+
+select nome, cpf from clientes;
+
+select nome, data_nascimento as "Data de Nascimento" from clientes;
+
+select 'CPF :' || cpf || 'RG: ' || rg as "CPF e RG" from clientes;
+
+select * from clientes limit 3;
