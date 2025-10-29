9d15196dc0f4983054a3f3fc636895e2008c2ffc
wendel <wendelsilvaalves6@gmail.com>
2025-10-29 16:13:50 -0300

    dasafio58

--- python/desafio58.py
+++ python/desafio58.py
@@ -1,5 +1,4 @@
 sexo = str(input('Informe seu sexo [M/F]: ')).strip().upper()
 while sexo not in ['M', 'F']:
     sexo = str(input('Dados inválidos. Por favor, informe seu sexo corretamente [M/F]: ')).strip().upper()
-
 print(f'Sexo {sexo} registrado com sucesso!')
