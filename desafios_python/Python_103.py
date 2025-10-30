60d3192c5ff72c6efd50b8f9b02995943f04999e
caioibarra-youxlab-dev <caio.ibarra-youxlab@institutoyoux.org.br>
2025-10-30 13:31:07 -0300

    exercicio python 103

---
+++ desafios_python/Python_103.py
@@ -0,0 +1,14 @@
+def ficha(nome='<desconhecido>',gols=0):
+    print(f'O jogador {nome} fez {gols} gol(s) no campeonato')
+
+
+nome = str(input("Nome do jogador:"))
+gols = str(input("Número de gols:"))
+if gols.isnumeric():
+    gols = int(gols)
+else:
+    gols = 0
+if nome.strip() == '':
+    ficha(gols=gols)
+else:
+    ficha(nome,gols)
