37f81e21d2931b44237c5eb2817604696c71dc57
wendel <wendelsilvaalves6@gmail.com>
2025-10-30 15:09:46 -0300

    desafio74

--- python/desafio74.py
+++ python/desafio74.py
@@ -1,8 +1,12 @@
-from random import randintg
+from random import randint
+
 numeros = (randint(0, 10), randint(0, 10), randint(0, 10),
            randint(0, 10), randint(0, 10))
+
 print('Os números sorteados foram:', end=' ')
 for n in numeros:
     print(n, end=' ')
+
 print(f'\nO maior valor sorteado foi {max(numeros)}')
 print(f'O menor valor sorteado foi {min(numeros)}')
+
