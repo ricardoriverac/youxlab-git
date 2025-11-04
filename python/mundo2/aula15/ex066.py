8eb464e487e66323947d2cd8cf8fcece7b333d17
Thawane <thawane.correa@institutoyoux.org.br>
2025-11-04 14:56:11 -0300

    ex066.py

--- python/mundo2/aula15/ex066.py
+++ python/mundo2/aula15/ex066.py
@@ -2,3 +2,12 @@
 # o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados
 # e qual foi a soma entre elas (desconsiderando o flag).
 
+number = 0
+soma = 0
+while number != 999:
+    number = int(input('Digite um número inteiro: '))
+    if number == 999:
+     print('Você digitou o número de parada "999".')
+    soma += number
+print(f'A soma dos valores é {soma}.')
+print('Você chegou ao fim do programa, até a próxima.')
