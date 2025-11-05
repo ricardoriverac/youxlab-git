105b2a376899341227eb27519eed12147b26cce2
wendel <wendelsilvaalves6@gmail.com>
2025-11-05 13:32:33 -0300

    desafio96

---
+++ python/desafio96.py
@@ -0,0 +1,8 @@
+def escreva(texto):
+    tamanho = len(texto) + 4
+    print('~' * tamanho)
+    print(f'  {texto}')
+    print('~' * tamanho)
+escreva('Olá, Mundo!')
+escreva('Curso de Python')
+escreva('Wendel Silva')
