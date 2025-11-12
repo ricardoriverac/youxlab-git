89dddd8826ec107a9cb1b55d26467396d8a83865
Thawane <thawane.correa@institutoyoux.org.br>
2025-11-12 17:12:25 -0300

    teoria.py

---
+++ python/mundo3/aula16/teoria.py
@@ -0,0 +1,9 @@
+#Teoria aula16 Mundo3 de python
+#TUPLAS SÃO IMTÁVEIS!!!
+
+print('----RESTAURANTE DA THATHA---')
+jantar = ('Arroz', 'Batata Frita', 'Lasanha', 'Coca-Cola')
+print('O cardápio de hoje será!')
+for cont in range(0, len(jantar)):
+    print(jantar[cont])
+print('APROVEITE O JANTAR, TENHA UMA BOA NOITE.')
