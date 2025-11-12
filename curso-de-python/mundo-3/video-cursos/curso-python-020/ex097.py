053d18d9719ca3a4ea5587a3010c435728435b58
João Othávio <joaoothaviofabri14@gmail.com>
2025-11-12 13:41:18 -0300

    Exercício 097 feito

---
+++ curso-de-python/mundo-3/video-cursos/curso-python-020/ex097.py
@@ -0,0 +1,9 @@
+def escreva(mensagem):
+    tamanhoPalavra = len(mensagem) + 2
+    print('~' * tamanhoPalavra)
+    print(f' {mensagem}')
+    print('~' * tamanhoPalavra)
+
+escreva('João Othávio')
+escreva('Curso de Python no Youtube')
+escreva('CeV')
