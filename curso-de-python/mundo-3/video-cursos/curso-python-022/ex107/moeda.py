33259aa65243f79cadda15c0cfe5b6e93bd09638
João Othávio <joaoothaviofabri14@gmail.com>
2025-11-13 16:10:37 -0300

    Exercício 107 feito

---
+++ curso-de-python/mundo-3/video-cursos/curso-python-022/ex107/moeda.py
@@ -0,0 +1,15 @@
+def aumentar(a, b):
+    resultado = a + (a * b / 100)
+    return resultado
+
+def diminuir(a, b):
+    resultado = a - (a * b / 100)
+    return resultado
+
+def dobro(a):
+    resultado = a * 2
+    return resultado
+
+def metade(a):
+    resultado = a / 2
+    return resultado
