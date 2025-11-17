c3cdd701f88b232cea3197506c974839f4ab6cdb
oigorromao <igor.faustino-youxlab@institutoyoux.org.br>
2025-11-17 17:05:35 -0300

    Exercicios 1 2 e 3

--- javascript/aula03.js
+++ javascript/aula03.js
@@ -1,4 +1,21 @@
 "use strict"
-if(true){
-    var nome = "Bruno"
+
+/*function teste() {
+    let nome = "Bruno"
+    if (true) {
+     console.log("dentro do if do teste: " + nome)   
+    }
+    console.log("dentro de teste: " + nome)
 }
+
+teste()*/
+let nome = "Buno"
+nome = "CFB Curso"
+nome = 10
+
+const curso = "Javascript"
+
+/*curso = "HTML" > DA ERRO, POR QUE ESTOU TENTANDO ATRIBUIR UM NOVO VALOR */
+
+console.log(curso)
+
