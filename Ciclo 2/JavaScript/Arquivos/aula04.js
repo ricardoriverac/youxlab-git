a48eaa178640cda690d4e2bbc9671cc0bbbe3935
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-11-17 17:24:13 -0500

    aula04 - Javascript

---
+++ Ciclo 2/JavaScript/Arquivos/aula04.js
@@ -0,0 +1,24 @@
+let num1=72
+let num2=5
+res= num1 + num2
+
+
+console.log(num1)
+console.log(num2)
+console.log(res)
+console.log(num1 - num2)
+console.log(res*2)
+
+/*
+| Operador | Função           |
+| -------- | ---------------- |
+| `+`      | soma             |
+| `-`      | subtração        |
+| `*`      | multiplicação    |
+| `/`      | divisão          |
+| `%`      | resto da divisão |
+| `**`     | potência         |
+| `++`     | incrementa       |
+| `--`     | decrementa       |
+*/
+
