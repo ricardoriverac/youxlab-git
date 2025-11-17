e6b3c3e7e406ca0ed5b065aa9cea716cb2033c0a
Thawane <thawane.correa@institutoyoux.org.br>
2025-11-17 15:24:57 -0300

    teoria.py

---
+++ python/mundo3/aula17/ex078.py
@@ -0,0 +1,7 @@
+# Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
+# No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
+
+valor = [1,14,15,99]
+for c, v in enumerate(valor):
+    print(f'Na posição {c} encontrei o valor "{v}".')
+print('Você chegou ao fim do programa!!')
---
+++ python/mundo3/aula17/teoria.py
@@ -0,0 +1,5 @@
+# teoria da aula 17.
+
+valor = [1,14,15,990,78,54]
+for c, v in enumerate(valor):
+    print(f'Na posição {c} encontrei o valor "{v}".')
