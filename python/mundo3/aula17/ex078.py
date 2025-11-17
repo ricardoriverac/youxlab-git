6ce4b9d90a602a4d72b0e59ada3fd10788f5fcbb
Thawane <thawane.correa@institutoyoux.org.br>
2025-11-17 16:59:43 -0300

    ex078.py

--- python/mundo3/aula17/ex078.py
+++ python/mundo3/aula17/ex078.py
@@ -1,7 +1,14 @@
 # Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
 # No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
 
-valor = [1,14,15,99]
-for c, v in enumerate(valor):
-    print(f'Na posição {c} encontrei o valor "{v}".')
-print('Você chegou ao fim do programa!!')
+
+minha_lista =[]
+for c in range(5):
+    valores_nume = int(input('Digite um valor: '))
+    minha_lista.append(valores_nume)
+print(f'A lista dos valores digitados são: {minha_lista}')
+maior_num = max(minha_lista)
+menor_num = min(minha_lista)
+print(f'O maior número digitado é {maior_num}, e o menor é {menor_num}')
+for c, v in enumerate(minha_lista):
+    print(f'Na posição {c} temos o valor {v}.')
