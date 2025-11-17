efd53c2692e8da1bedcd4345ecf95cfdf9e1a9bd
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2025-11-17 17:05:10 -0300

    Aulas 01,02 e 03 feitas

---
+++ Ciclo 2/JavaScript/Arquivos/aula02.js
@@ -0,0 +1,6 @@
+"use strict" //Modo estrito
+
+let nome="Bruno" // let declara a variável
+console.log("CFB Cursos")
+console.log(nome)
+console.log("Nome: " + nome)
---
+++ Ciclo 2/JavaScript/Arquivos/aula03.js
@@ -0,0 +1,26 @@
+/*"use strict"
+
+function teste(){
+    let nome="Bruno"
+    if(true){
+        console.log("dentro do IF do teste: " + nome)
+    }
+    console.log("dentro de teste: " + nome)
+}
+
+teste()
+
+console.log("fora de teste: " + nome)
+ */
+
+"use strict"
+
+let nome="Bruno"
+nome="CFB cursos"
+nome=10
+
+const curso="JavaScript"
+
+
+curso="HTML"
+console.log(curso)
