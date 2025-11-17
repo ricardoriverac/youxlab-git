c59e31abf4537172dbe872b371642c3ecefcdcac
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2025-11-17 17:50:12 -0300

    Aula 04 feita

---
+++ Ciclo 2/JavaScript/Arquivos/aula04.js
@@ -0,0 +1,18 @@
+let num1=0,num2=0,res=0
+
+num1=10
+num1*=2 
+// num1 ++ num1=num1+5
+/*num1++
+num1++
+num1++
+num1++
+num1++
+/*++ acrescenta 1 */
+/*num1--
+num1--
+num1--
+num1--
+num1--     -- tira 1*/
+console.log(num1)
+
