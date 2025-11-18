4c7692beeafff153397c3b51f4e3c6f8a4cb5256
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2025-11-18 13:37:53 -0300

    Aula 05 feita.

--- Ciclo 2/JavaScript/Arquivos/aula04.js
+++ Ciclo 2/JavaScript/Arquivos/aula04.js
@@ -1,8 +1,10 @@
+//operadores matemáticos
+
 let num1=0,num2=0,res=0
 
 num1=10
 num1*=2 
-// num1 ++ num1=num1+5
+// num1 ++ //+=num1=num1+5
 /*num1++
 num1++
 num1++
---
+++ Ciclo 2/JavaScript/Arquivos/aula05.js
@@ -0,0 +1,17 @@
+//operadores relacionais
+
+let num1=10
+let num2=5
+let num3=10
+
+//console.log(num1 > num2)  /* > faz uma comparação se num1 é maior que num2 */
+//console.log(num1 < num2)   /* < num1 é menor q num2*/
+//console.log(num1 >= num3)  /* >= maior ou igual */
+//console.log(num1 <= num3)   /* <= menor ou igual */
+//console.log(num1 == num3)  /* == igual */
+//console.log(!(num1 == num3)) /* ! negação */
+//console.log(num1 != num3) /* != diferente de */
+//console.log(!(num1 != num3)) /* num1 NÃO é diferente de  */
+
+
+//   > >=  < <= == !=
