960c862191b08d6771ad32bdd506f3be1958a64a
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2025-11-18 16:18:13 -0300

    Aula 07 feita.

---
+++ Ciclo 2/JavaScript/Arquivos/aula07.js
@@ -0,0 +1,21 @@
+//operadores lógicos
+
+/*&& --> and e
+|| --> or ou 
+! --> not não*/
+
+let n1,n2,n3,n4
+n1=10
+n2=5
+n3=15
+n4=2
+
+//console.log((n1>n2)&&(n1>n3)) //false
+//console.log((n1>n2)||(n1>n3)) //true
+//console.log(!((n1>n2)||(n1>n3))) // ! inverção da operação
+
+if( !(n1>n2) && (n3>n4)){
+    console.log("verdadeiro")
+}else{
+    console.log("falso")
+}
