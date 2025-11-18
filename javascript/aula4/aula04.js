727aa1df223e2add8b4d3f2be863bc1d1b4ef991
oigorromao <igor.faustino-youxlab@institutoyoux.org.br>
2025-11-18 15:09:55 -0300

    Exercicios 1 ao 5

--- javascript/aula4/aula04.js
+++ javascript/aula4/aula04.js
@@ -1,2 +1,24 @@
+/* let num1 = 0
+let num2 = 0
+let res = 0
+
+num1 = 5
+num2 = 10
+
+res = num1+num2
+res2 = (num1+num2)*2
+
+console.log(res2)
+console.log(res)
+console.log(num1-num2)
+*/
+
 let num1 = 0
 let num2 = 0
+let res = 0
+
+num1 = 10
+num1*=2
+
+
+console.log(num1)
