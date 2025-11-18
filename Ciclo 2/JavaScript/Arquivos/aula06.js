405b09c8e3eba40aa4e5673253b11d30506d7066
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2025-11-18 14:30:31 -0300

    Aula 06 feita.

---
+++ Ciclo 2/JavaScript/Arquivos/aula06.js
@@ -0,0 +1,13 @@
+/*let n1=1
+let n2="1"
+
+console.log(n1 == n2) // == se é igual
+console.log(n1 === n2) //=== considera o tipo de dado
+*/
+
+
+/*let nome=prompt("Digite seu nome")
+console.log(nome)*/
+
+
+
