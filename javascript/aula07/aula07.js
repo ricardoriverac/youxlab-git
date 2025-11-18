8c216f4804cac32a4721b9374831a3144a9a96a5
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-11-18 16:48:15 -0300

    eu terminei a aula 07 de javascript

---
+++ javascript/aula07/aula07.js
@@ -0,0 +1,17 @@
+// && --> and, e
+ // || --> or, ou
+//!  --> not, não
+
+let num1, num2, num3, num4
+num1 = 10
+num2 = 5
+num3 = 15
+num4 = 2
+
+if((num1>num2) || (num3>num4)){
+    console.log("verdadeiro")
+}
+else
+    {
+    console.log("falso")
+}
