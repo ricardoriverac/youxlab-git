c1581f6eb561ddd069708b7dff833aac49917ae9
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2025-11-19 16:03:36 -0300

    Aula 25 concluida - Casting e saida de dados

---
+++ Estudos/java/untitled1/src/a_25.java
@@ -0,0 +1,23 @@
+public class a_25 {
+    public static void main(String[] args) {
+        int x, y;
+        x = 5;
+        y = 2 * x;
+        System.out.println(x);
+        System.out.println(y);
+        int b;
+        double a;
+        b = 5;
+        a = 2 * 5;
+        System.out.println(b);
+        System.out.println(a);
+
+        double ba,BA, h, area;
+        ba = 6;
+        BA = 8;
+        h = 5;
+
+        area = (ba+BA) / 2.0 * h;
+        System.out.println(area);
+    }
+}
