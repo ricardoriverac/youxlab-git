2870b54defbee09517654f50f3b08ce6f6f671cd
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2025-11-19 15:43:25 -0300

    Exercicio aula 24 corrigido

--- Estudos/java/untitled1/src/ex_a24.java
+++ Estudos/java/untitled1/src/ex_a24.java
@@ -10,7 +10,7 @@ public class ex_a24 {
         double preco1 = 2100.0;
         double preco2 = 650.50;
         double medida = 53.234567;
-        System.out.printf("Products:%n %s, which price is %f %n %s, which price is %f %n %n", produto1, preco1, produto2, preco2);
+        System.out.printf("Products:%n %s, which price is %.2f %n %s, which price is %.2f %n %n", produto1, preco1, produto2, preco2);
         System.out.printf("Record: %d years old, code %d and gender: %s", idade, codigo, genero);
         System.out.printf("Measue with height decimal places: %f %n rouded(three decimal places): %.3f %n", medida, medida);
         Locale.setDefault(Locale.US);
