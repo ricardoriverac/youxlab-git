9276eeac6934ffbdefbaf487749af8a5c9c99015
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2025-11-19 17:38:24 -0300

    Aula 27 - Entrada de dados em Java parte 2

---
+++ Estudos/java/untitled1/src/a_27.java
@@ -0,0 +1,19 @@
+import  java.util.Scanner;
+
+public class a_27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1, s2, s3;
+        int l;
+        l= sc.nextInt();
+        sc.nextLine();
+        s1= sc.nextLine();
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
+        System.out.println(l);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+    }
+}
