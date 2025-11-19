a048d523df17031b2a7dbc2778c6d2b55d1dee4d
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2025-11-19 19:51:12 -0300

    Aula 30 - Exercicio 1

---
+++ Estudos/java/untitled1/src/a_30.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class a_30 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        int parcela1, parcela2, soma;
+        parcela1 = sc.nextInt();
+        parcela2 = sc.nextInt();
+        soma = parcela1 + parcela2;
+        System.out.printf("Soma = %d", soma);
+
+    }
+}
