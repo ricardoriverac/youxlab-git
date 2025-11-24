2976b4050c05b700d875a3f2563f2aece0ffc40a
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2025-11-24 16:27:21 -0300

    Aula 37 exercicio 4 concluido

---
+++ Estudos/java/untitled1/src/a_37_ex4.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class a_37_ex4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int horaInicial, horaFinal;
+
+        System.out.print("Caro usuário, por favor digite a hora inicial deste evento: ");
+        horaInicial = sc.nextInt();
+        System.out.print("Caro usuário, por favor digite a hora final deste evento: ");
+        horaFinal = sc.nextInt();
+
+        if (horaInicial >= horaFinal) {
+            int duracao;
+            duracao = (24 - horaInicial) + horaFinal;
+            System.out.printf("A duração deste evento foi %d horas", duracao);
+        }
+        else{
+            int duracao;
+            duracao = horaFinal - horaInicial;
+            System.out.printf("A duração deste evento foi %d horas", duracao);
+        }
+    }
+}
