3e71cb3356067e5be2ed8fb9807fe2f3e16da250
Thawane <thawane.correa@institutoyoux.org.br>
2025-11-24 15:13:48 -0300

    teoria.py

---
+++ python/mundo3/aula19/teoria.py
@@ -0,0 +1,4 @@
+#Teoria da aula 19.
+
+dados = {'NOME':'Thawane', 'IDADE':'15 anos','SEXO':'Feminino', 'TRABALHO':'YouX LAB'}
+print(f'Meu nome é {dados["NOME"]}, tenho {dados["IDADE"]}, meu sexo é {dados["SEXO"]} e faço parte da {dados["TRABALHO"]}.')
