0da1bc09c1fc701fcef84be9f44f911c61b7f30a
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-11-24 17:44:27 -0300

    eu terminei a aula012 de javascript

---
+++ javascript/aula012/aula012.js
@@ -0,0 +1,10 @@
+const objs1=document.getElementsByTagName("div")
+const objs2=[...document.getElementsByTagName("div")]
+
+
+objs2.forEach(element => {
+    element.innerHTML=""
+});
+
+console.log(objs1)
+console.log(objs2)
