bca05de553387f9c90255528b79029c447fba3d8
oigorromao <igor.faustino-youxlab@institutoyoux.org.br>
2025-11-25 14:22:40 -0300

    Exercicio 13

---
+++ javascript/aula13/aula13.js
@@ -0,0 +1,26 @@
+/*let num = 100
+
+if (num > 10){
+    console.log("Numeral maior que 10")
+if(num > 50){
+    console.log("Numeral maior que 50")
+}
+}else if(num > 5){
+    console.log("Numeral esta entre 6 e 10")
+}else{
+    console.log("Numenral menor ou igual a 5");
+}
+
+console.log("Fim do programa")*/
+
+
+let num = 100
+let clima = "sol"
+
+if (enrgia > 70 && clima =="sol") {
+    console.log("Eu vou a praia")
+}else{
+    console.log("Vou ao cinema");
+}
+
+console.log("Fim do programa");
