7713426a49f0eb4f6af83ebbf760493944029fe2
Thawane <thawane.correa@institutoyoux.org.br>
2025-11-25 16:31:36 -0300

    teoria.py

--- python/mundo3/aula20/teoria.py
+++ python/mundo3/aula20/teoria.py
@@ -1,1 +1,8 @@
-#
+#Teoria da aula20.
+
+def cont(*num):
+    tam = len(num)
+    print(f'Recebi os valores {num} e tem ao todo {tam} números.')
+cont(12, 13, 8,)
+cont(9, 0, 89, 34)
+cont(17, 6, 3)
