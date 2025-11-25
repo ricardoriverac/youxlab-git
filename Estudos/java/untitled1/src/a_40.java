56bf56253a6c68624768e8bfb6204dd31ede149e
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2025-11-25 15:29:08 -0300

    Aula 40 - Condições ternárias

---
+++ Estudos/java/untitled1/src/a_40.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+
+public class a_40 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        double preco = 34.5;
+        double desconto;
+        /*if (preco < 20.0){
+            desconto = preco * 0.1;
+        }
+        else{
+            desconto = preco * 0.05;
+        }*/
+
+         desconto = (preco < 20.0) ?  preco * 0.1 :  preco * 0.05;
+        System.out.println(desconto);
+         sc.close();
+    }
+}
