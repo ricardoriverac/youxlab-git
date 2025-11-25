6dead98df856bf4e1cdc15898667eab07de08255
oigorromao <igor.faustino-youxlab@institutoyoux.org.br>
2025-11-25 14:47:21 -0300

    Exercicio 14

---
+++ javascript/aula14/aula14.js
@@ -0,0 +1,17 @@
+let colocacao = 1
+switch (colocacao) {
+    case 1:
+        console.log("Primeiro Lugar")
+        break
+    case 2:
+        console.log("Segundi Lugar")
+        break
+    case 3:
+        console.log("Terceiro Lugar")
+        break
+    default:
+        console.log("Nao subiu ao podio")
+        break
+        case 4: case 5: case 6:
+            console.log("Premio de participacao"); 
+}
