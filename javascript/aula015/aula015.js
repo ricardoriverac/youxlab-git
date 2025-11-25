0b69078741f72dc8917c3b57e2f06aefe2aa8f3f
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-11-25 16:21:28 -0300

    eu terminei a aula016 de javascript

---
+++ javascript/aula015/aula015.js
@@ -0,0 +1,13 @@
+console.log("Inicio do programa");
+
+for (let i=0; i < 100; i++){
+    if(i%2==0){
+        console.log(i + " é par ");
+    }else{
+        console.log(i + " é impar ");
+    }
+
+    
+}
+
+console.log("Fim do programa");
