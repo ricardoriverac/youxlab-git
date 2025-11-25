3ef66851628ce7084e620e921bc179e78647de22
Youx <juan.ferreira-youxlab@institutoyoux.org.br>
2025-11-25 15:29:24 -0300

    aula de javascript numero 16

---
+++ curso_java/aula15/aula15.js
@@ -0,0 +1,14 @@
+//console.log("Inicio do programa");
+//
+//for(let i=0; i < 10; i++){
+//    console.log("CBF Cursos - valor do i:" + i);
+//}
+//console.log("Fim do programa");
+
+for(let i=0 ; i < 100; i++){
+    if(i%2==0){
+        console.log(i + "É par")
+    }else{
+        console.log(i + "é impar");
+    }
+}
