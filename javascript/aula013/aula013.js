b13ef3856e9436c2330c440ec683cd991649c97b
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-11-25 14:29:13 -0300

    eu terminei a aula 013 de javascript

---
+++ javascript/aula013/aula013.js
@@ -0,0 +1,12 @@
+let energia=100
+let clima="chovendo"
+
+if(energia > 70 && clima=="sol"){
+    console.log("Vou a Praia")
+    
+}else{
+    console.log("Vou ao cinema");
+}
+
+
+console.log("Fim do programa")
