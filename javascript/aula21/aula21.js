9cb90a5c2b107bb389227838df115e4775e8b30e
oigorromao <igor.faustino-youxlab@institutoyoux.org.br>
2025-11-27 16:50:20 -0300

    Exercicio 21

---
+++ javascript/aula21/aula21.js
@@ -0,0 +1,27 @@
+/*
+function mudarTexto(){
+    console.log("CFB Cursos")
+    return
+}
+
+canal()
+canal()
+canal()
+*/
+
+function canal() {
+    let n1 = 10
+    let n2 = 2
+    let res = n1 * n2
+    if (res % 2 == 0)
+        return "Par"
+    else
+        return "Impar"
+}
+
+let num = canal()
+
+console.log(num)
+
+
+// LEMBRAR QUE APENAS UM RETURN PODE SER EXECUTADO
