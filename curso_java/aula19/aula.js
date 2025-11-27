56645cc3a49efcf71ddf26cd068f477bd9aa93d6
Youx <juan.ferreira-youxlab@institutoyoux.org.br>
2025-11-27 17:19:25 -0300

    aula20

---
+++ curso_java/aula19/aula.js
@@ -0,0 +1,14 @@
+let n=0
+let pares=0
+let max=1000
+
+for (let i=n;i<max;i++){
+    console.log("bababui " + i);
+    if(i%2!=0){
+        continue
+    }
+    pares++
+}
+
+console.log("os numeros pares são "+pares);
+console.log("Fim do programa");
---
+++ curso_java/aula20/aula20.js
@@ -0,0 +1,10 @@
+function mudartexto(){
+    let d1=document.getElementById("d1")
+    let d2=document.getElementById("d2")
+    let d3=document.getElementById("d3")
+    d1.innerHTML="jota"
+    d2.innerHTML="jota"
+    d3.innerHTML="jota"
+}
+
+mudarTexto()
