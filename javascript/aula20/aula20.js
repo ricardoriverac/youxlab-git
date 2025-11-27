bf01f1a1e8364ef4065710522214972949f0dd67
oigorromao <igor.faustino-youxlab@institutoyoux.org.br>
2025-11-27 15:13:28 -0300

    Exercicio 20, e tambem corrigi alguns erros

---
+++ javascript/aula20/aula20.js
@@ -0,0 +1,23 @@
+/*
+function nome(){
+    let n1 = 2
+    let n2 = 10
+    let soma = n1+n2
+    console.log("soma")
+}
+
+for (let i=0; i<10; i++){
+    soma2_10()
+}
+*/
+
+function mudarTexto() {
+    let d1 = document.getElementById("d1")
+    let d2 = document.getElementById("d2")
+    let d3 = document.getElementById("d3")
+    d1.innerHTML = "CFB Cursos"
+    d2.innerHTML = "CFB Cursos"
+    d3.innerHTML = "CFB Cursos"
+}
+
+//mudarTexto()
