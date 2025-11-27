be196854c980bb16bebce77a48e1bd0ea442ed84
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-11-27 14:58:46 -0500

    aula08 - Javascript

---
+++ Ciclo 2/JavaScript/Arquivos/aula08.js
@@ -0,0 +1,24 @@
+let n1 = 10
+let n2 = 11
+
+//let res = n1 & n2
+
+//console.log(res)
+
+
+//let res = n1 & n2
+
+//console.log(res)
+
+
+/* let res = n1  n2
+
+console.log(res)
+*/
+
+let n3 = 20
+
+let res = n3 << 2
+
+console.log(res)
+
