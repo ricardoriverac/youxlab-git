7a112a764819ea7a7be20045637bff411f86047e
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2025-11-27 17:04:51 -0300

    Aula 12 feita.

---
+++ Ciclo 2/JavaScript/Arquivos/aula12.js
@@ -0,0 +1,25 @@
+/*const jogador1={nome: "Bruno",energia:100,vidas:3,magia:150}
+const jogador2={nome: "Bruce",energia:100,vidas:5,velocidade:80}
+const jogador3={...jogador1,...jogador2}
+
+
+console.log(jogador3)
+const soma=(v1,v2,v3)=>{
+    return v1+v2+v3
+}
+
+let valores=[1,5,4,10]
+
+console.log(soma(...valores))*/
+
+const objs1=document.getElementsByTagName("div")
+const objs2=[...document.getElementsByTagName("div")]
+
+objs2.forEach(element => {
+    element.innerHTML="curso"
+});
+
+
+console.log(objs1)
+console.log(objs2)
+
