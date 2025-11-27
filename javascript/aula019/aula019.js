72983d2b4d930608d4759b6d01ca800f29e40257
PabloJr01 <pablo.junior-youxlab@institutoyoux.org.br>
2025-11-27 17:15:30 -0300

    eu terminei aula020 de javascript

--- javascript/aula019/aula019.js
+++ javascript/aula019/aula019.js
@@ -1,14 +1,12 @@
 let n=0
 let max=1000
 let pares=0
-for(let i=n;i<max,i++){
-    console.log("cfb cursos -" + i);
+for(let i=n;i<max;i++){
     if(i%2==0){
-        continue 
-    
+        continue
     }
     pares++
 }
 
 console.log("Quantidade de pares" + pares);
-console.log("Fim do programa"); 
+console.log("Fim do programa");
---
+++ javascript/aula020/aula020.js
@@ -0,0 +1,10 @@
+function mudarTexto(){
+    let d1=document.getElementById("d1")
+    let d2=document.getElementById("d2")
+    let d3=document.getElementById("d3")
+    d1.innerHTML="cfb cursos"
+    d2.innerHTML="cfb cursos"
+    d3.innerHTML="cfb cursos"
+}
+
+mudarTexto()
