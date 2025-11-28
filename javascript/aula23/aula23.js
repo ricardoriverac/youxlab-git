ea27c4d5dbfe0b083ef24c5b9a0d4ed8ad054ceb
oigorromao <igor.faustino-youxlab@institutoyoux.org.br>
2025-11-28 14:42:58 -0300

    Exercicio 22

---
+++ javascript/aula23/aula23.js
@@ -0,0 +1,23 @@
+/*
+ESTRUTURA 1
+function soma(...valores){
+    let tam = valores.length
+    let res = 0
+    for(let i=0;i<tam;i++){
+        res+=valores[i]
+    }
+    return res
+}
+
+console.log(soma(10, 5, 2, 8))
+*/
+
+function soma(...valores){
+    let res = 0
+    for(let v of valores){
+        res+=v
+    }
+    return res
+}
+
+console.log(soma(10, 5, 2, 8, 15))
