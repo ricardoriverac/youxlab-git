6e8cee581f98e1740080d0183051175196935da4
oigorromao <igor.faustino-youxlab@institutoyoux.org.br>
2025-11-28 17:12:25 -0300

    Exercicio 25

---
+++ javascript/aula25/aula25.js
@@ -0,0 +1,12 @@
+//const soma = function(v1, v2){return v1+v2}
+
+const soma = (v1, v2) => { return v1 + v2 }
+console.log(soma(10,5));
+
+const nome = n => n 
+console.log("Bruno")
+
+const add = n =>n+10
+console.log(add(10))
+
+
