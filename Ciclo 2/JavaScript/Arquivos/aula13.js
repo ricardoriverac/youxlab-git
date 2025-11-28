756f3cc97df6dc4fdb5a9d0aff56c7da32841dce
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-11-28 17:28:56 -0500

    aula13 concluida

---
+++ Ciclo 2/JavaScript/Arquivos/aula13.js
@@ -0,0 +1,54 @@
+/* if e if else  
+    
+if (condicao1) {
+   código se condicao1 for verdadeira
+} else if (condicao2) {
+   código se condicao2 for verdadeira
+} else {
+    código se nenhuma for verdadeira
+}
+
+
+*/
+
+let num= 10
+
+if(num > 10){
+    console.log("Numero maior que 10")
+    console.log("comando 2")
+}else{
+    console.log("Numero menor ou igual a 10")
+}
+
+
+console.log("Fim do programa")
+
+
+console.log("===============================" )
+
+
+/* outro jeito */
+
+let num2= 77
+
+if(num2 > 10){
+    console.log("Numero maior que 10")
+    console.log("comando 2")
+}else if(num2 > 5) {
+    console.log("Numero esta entre 6 e 10")
+}else{
+    console.log("Numeral menor ou igual a 5")
+}
+
+
+console.log("Fim do programa") 
+
+
+/* Pode ser acrescentado outro if dentro de if
+
+
+if ....
+    if...
+    
+*/
+
