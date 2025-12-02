44bb720471ef11ab44a03270f793a4c1c916596a
Thawane <thawane.correa@institutoyoux.org.br>
2025-12-02 19:43:28 -0300

    ex106.py

--- python/mundo3/aula21/ex106.py
+++ python/mundo3/aula21/ex106.py
@@ -2,3 +2,7 @@
 # o comando e o manual vai aparecer. Quando o usuário digitar a palavra 'FIM', o programa se encerrará.
 # Importante: use cores.
 
+
+nome = input('\033[1;31mDigite seu nome: ')
+print(f'\033[1;31mSeja Bem-Vindo {nome}.')
+help(print)
