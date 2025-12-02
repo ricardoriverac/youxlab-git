9ffa4e17121ccd8e7922e90c2ae13f957f1e9096
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2025-12-02 09:41:59 -0300

    Aula 55 concluida

--- Estudos/java/untitled1/src/a_55.java
+++ Estudos/java/untitled1/src/a_55.java
@@ -5,22 +5,17 @@ public class a_55 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        char resp;
 
-        System.out.print("Caro usuário, por favor digite quantos celsius está hoje: ");
-        double celsius = sc.nextDouble();
-        double fahrenheit = (celsius * 1.80 + 32);
-        System.out.printf("Caro usuário, está %.2f fahrenheit\n", fahrenheit);
-        System.out.print("Caro cliente, deseja continuar? [s/n]");
-        char resp = sc.next().charAt(0);
-
-        while (resp != 'n'){
+        do {
             System.out.print("Caro usuário, por favor digite quantos celsius está hoje: ");
-            celsius = sc.nextDouble();
-            fahrenheit = (celsius * 1.80 + 32);
-            System.out.printf("Caro usuário, está %.2f fahrenheit", fahrenheit);
+            double celsius = sc.nextDouble();
+            double fahrenheit = (celsius * 1.80 + 32);
+            System.out.printf("Caro usuário, está %.2f fahrenheit\n", fahrenheit);
+            System.out.print("Caro cliente, deseja continuar? [s/n]");
             resp = sc.next().charAt(0);
-
         }
+         while (resp != 'n');
         sc.close();
     }
 }
