cf1262e8f24e281f068d5180e7710cc11b396daf
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2025-12-03 14:20:53 -0300

    Aula 66 - Criando um método para obtermos os beneficios de reaproveitamento e delegação

--- Estudos/java/untitled1/src/application/entities/a_65.java
+++ Estudos/java/untitled1/src/application/entities/a_65.java
@@ -4,4 +4,9 @@ public class Triangle {
     double a;
     double b;
     double c;
+
+    public double area(){
+        double p = (a + b+ c) / 2.0;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
 }
