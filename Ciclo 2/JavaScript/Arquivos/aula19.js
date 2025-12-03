1e62f052abe09bbafd016c0f36569a13ed485623
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2025-12-03 14:53:02 -0300

    Aula 19, feita.

---
+++ Ciclo 2/JavaScript/Arquivos/aula19.js
@@ -0,0 +1,36 @@
+let n=0
+let max=1000
+let pares=0
+
+for(let i=n;i<max;i++){
+    console.log("CFB Cursos - " +i)
+    if(i%2!=0){
+        continue  
+    }
+     pares++
+}
+
+console.log("Quantidade de pares: " + pares)
+console.log("Fim do programa ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*while(n<max){
+    console.log("CFB Cursos - " + n)
+    if(n>10){
+        break
+    }
+    n++
+}
+console.log("FIM DO PROGRAMA")*/
