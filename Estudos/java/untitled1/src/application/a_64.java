63c07b9b9db067e31b296bba8bd2dfb26173b4b9
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2025-12-03 14:03:47 -0300

    Aula 65 - Criando uma classe com três atributos para representar melhor o triângulo

--- Estudos/java/untitled1/src/application/a_64.java
+++ Estudos/java/untitled1/src/application/a_64.java
@@ -1,5 +1,7 @@
 package application;
 
+import application.entities.Triangle;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,20 +10,22 @@ public class a_64 {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        double xA, xB, xC, yA, yB, yC;
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
         System.out.println("Insira as medidas do triângulo X: ");
-        xA = sc.nextDouble();
-        xB = sc.nextDouble();
-        xC = sc.nextDouble();
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
         System.out.println("Insira as medidas do triângulo Y: ");
-        yA = sc.nextDouble();
-        yB = sc.nextDouble();
-        yC = sc.nextDouble();
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
 
-        double p = (xA + xB + xC) / 2.0;
-        double areaX = Math.sqrt(p * (p - xA) * (p * xB) * (p - xC));
-        p = (yA + yB + yC) / 2.0;
-        double areaY = Math.sqrt(p * (p - yA) * (p * yB) * (p - yC));
+        double p = (x.a + x.b + x.c) / 2.0;
+        double areaX = Math.sqrt(p * (p - x.a) * (p * x.b) * (p - x.c));
+        p = (y.a + y.b + y.c) / 2.0;
+        double areaY = Math.sqrt(p * (p - y.a) * (p * y.b) * (p - y.c));
         System.out.printf("Área do triângulo X: %.4f\n", areaX);
         System.out.printf("Área do triângulo Y: %.4f\n", areaY);
 
---
+++ Estudos/java/untitled1/src/application/entities/a_65.java
@@ -0,0 +1,7 @@
+package application.entities;
+
+public class Triangle {
+    double a;
+    double b;
+    double c;
+}
