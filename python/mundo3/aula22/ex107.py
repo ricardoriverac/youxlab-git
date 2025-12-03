4b8a133f849cf6f11230c3f72b2fe0415e256f65
Thawane <thawane.correa@institutoyoux.org.br>
2025-12-03 14:27:50 -0300

    ex107.py

--- python/mundo3/aula22/ex107.py
+++ python/mundo3/aula22/ex107.py
@@ -2,5 +2,9 @@
 # dobro() e metade(). Faça também um programa que importe esse módulo e use algumas dessas funções.
 
 
-p = float(input('Digite o preço:R$'))
-print(f'A metade de {p} é ')
+import moeda
+preco = float(input('Digite o valor da sua compra:R$'))
+print(f'A metade de R${preco} é:R${moeda.metade(preco)}')
+print(f'O dobro de R${preco} é:R${moeda.dobro(preco)}')
+print(f'O valor da compra com o aumento de 10% é:R${moeda.aumento(preco)}')
+print(f'O valor da compra com o reduzimento de 13% é:R${moeda.diminuir(preco)}')
