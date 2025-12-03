b4d46bf19cb3af8158b9b93c5b665db331dca5ee
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2025-12-03 14:10:44 -0300

    Aula 16 feita.

---
+++ Ciclo 2/JavaScript/Arquivos/aula16.js
@@ -0,0 +1,15 @@
+const objs=document.getElementsByTagName(("div"))
+
+let num=[10,20,30,40,50]
+
+for(let i=0; i<num.length; i++ ){
+    console.log(num[i])
+}
+
+for(o of objs){ //elementos
+    console.log(o.innerHTML="Curso") 
+}
+for(o in objs){
+    console.log(objs[o].innerHTML)
+    //console.log(o)
+}
