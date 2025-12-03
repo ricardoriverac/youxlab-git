3a487205deeb1a5a145549a94e911dc3e344b3d9
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-12-03 13:32:16 -0500

    aula15 - Javascript

---
+++ Ciclo 2/JavaScript/Arquivos/aula15.js
@@ -0,0 +1,14 @@
+/*Loop FOR*/
+
+// for(inicio; condição; cont) 
+
+
+console.log("Inicio do programa")
+
+
+for(let i = 0; i < 10; i++){
+    console.log("CFB Cursos - valor do i: "+ i)
+} 
+
+
+console.log("Fim do programa")
