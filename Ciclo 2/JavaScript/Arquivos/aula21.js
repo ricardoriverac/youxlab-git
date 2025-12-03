13fea29b9dfdfe61268dfe71d32930d487bf0a8f
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-12-03 16:43:38 -0500

    aula2 - Javascript

---
+++ Ciclo 2/JavaScript/Arquivos/aula21.js
@@ -0,0 +1,16 @@
+function canal(){
+    console.log("CFB Cursos")
+    return
+}
+
+console.log(canal())
+
+
+function resto(){
+    let n1 = 22
+    let n2 = 34
+    let res = n1*n2
+    return res
+}
+
+console.log(resto())
