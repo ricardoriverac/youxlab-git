37bac4cd9a818740c3fedb462a970a67859caafe
Thawane <thawane.correa@institutoyoux.org.br>
2025-12-03 14:10:14 -0300

    moeda.py do ex107.py

---
+++ python/mundo3/aula22/moeda.py
@@ -0,0 +1,14 @@
+def metade(n):
+    calculo_metade = n / 2
+    return calculo_metade
+
+def dobro(n):
+    return n*2
+
+def aumento(n):
+    novo_valor = n * 1.1
+    return novo_valor
+
+def diminuir(n):
+    novo_valor = n * 0.87
+    return round(novo_valor, 2)
