80c6812d7f0008ec1cde04866bb3e46c2714b246
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-12-04 13:42:00 -0500

    aula22 - Javascript

---
+++ Ciclo 2/JavaScript/Arquivos/aula22.js
@@ -0,0 +1,21 @@
+function soma(n1, n2){
+    console.log(n1+n2)
+}
+
+soma("Bruno")
+soma(2022)
+soma(5.2)
+
+console.log("================")
+
+
+soma(10, 5)
+
+function soma2(v1, v2){
+    let res 
+    res = v1+v2
+    return res
+}
+
+let resultado_soma = soma2(5, 5)
+console.log(resultado_soma)
