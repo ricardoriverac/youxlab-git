926c964a977c95cc39d131e91e6e305d4f587acf
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-12-04 14:42:38 -0500

    aula25 - Javascript

---
+++ Ciclo 2/JavaScript/Arquivos/aula25.js
@@ -0,0 +1,15 @@
+/* Arrow Function */ 
+
+
+// const soma = () => {}
+
+
+const soma = (v1, v2) => {return v1 + v2}
+
+console.log(soma(10, 5))
+
+const soma2 = (n1, n2) => {
+    let res = n1+n2
+    return n1 + n2}
+
+console.log(soma2(10, 5))
