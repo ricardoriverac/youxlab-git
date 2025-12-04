08ff98858962295bf68221af2eaf933d36b6405f
Thawane <thawane.correa@institutoyoux.org.br>
2025-12-04 15:17:30 -0300

    teoria.py

--- python/mundo3/aula23/teoria.py
+++ python/mundo3/aula23/teoria.py
@@ -1,1 +1,11 @@
 #Teoria aula23.
+try:
+    num1 = int(input('Digite um número:'))
+    num2 = int(input('Digite o segundo número:'))
+    multi = num1 * num2
+except:
+    print('ERRO, certifique se digitou o número corretamente!!')
+else:
+    print(f'O resultado da multiplicação é:{multi}')
+finally:
+    print('Tenha uma boa tarde, volte sempre!!')
