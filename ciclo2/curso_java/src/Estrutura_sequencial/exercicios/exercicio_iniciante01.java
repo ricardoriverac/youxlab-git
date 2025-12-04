bf73970802aef9238b5d13564337dfdef4158666
daniella <daniella.flor-youxlab@institutoyoux.org.br>
2025-12-04 16:16:05 -0300

    exercicios java

---
+++ ciclo2/curso_java/src/Estrutura_sequencial/exercicios/exercicio_iniciante01.java
@@ -0,0 +1,27 @@
+package Estrutura_sequencial.exercicios;
+
+/* Faça um programa para ler dois valores inteiros, e depois mostrar
+na tela a soma desses números com uma mensagem
+ */
+
+import java.util.Scanner;
+
+    public class exercicio_iniciante01 {
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            int A, B, soma;
+
+            A = sc.nextInt();
+            B = sc.nextInt();
+
+            soma = A + B;
+
+            System.out.println("Resultado da soma = " + soma);
+
+            sc.close();
+        }
+    }
+
--- ciclo2/curso_java/src/aulas_java/aula1.java
+++
@@ -1,8 +0,0 @@
-package aulas_java;
-
-public class aula1 {
-    public static void main(String[] args) {
-        System.out.println("Olá mundo!");
-
-    }
-}
