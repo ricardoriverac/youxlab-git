7ec83a26ce4be427beced38bf40cc8b5f0f3917a
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2025-12-04 16:21:35 -0300

    Aula 27 feita.

---
+++ Ciclo 2/JavaScript/Arquivos/aula27.js
@@ -0,0 +1,39 @@
+//Funções geradoras
+/*function* cores(){
+    yield 'Vermelho'
+    yield 'Verde'
+    yield 'Azul'
+}
+let itc=cores()
+console.log(itc.next().value)
+console.log(itc.next().value)
+console.log(itc.next().value)
+console.log(itc.next().value)*/
+
+/*function* perguntas(){
+    const nome=yield 'Qual seu nome?'
+    const esporte=yield 'Qual seu esporte favorito?'
+    return "Seu nome é " + nome + ', seu esporte favorito é ' + esporte
+
+}
+
+const itp=perguntas()
+console.log(itp.next().value)
+console.log(itp.next('Bruno').value)
+console.log(itp.next('Natação').value)*/
+
+function* contator(){
+    let i=0
+    while(true){
+        yield i++
+        if(i>5)
+            break
+    }
+}
+const itc=contator()
+for(let c of itc){
+    console.log(c)
+}
+    
+
+
