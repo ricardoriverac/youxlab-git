f4b6789b44a28c6efe70ad8575937da0c65e6174
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2025-12-04 13:49:00 -0300

    Aula 22,feita.

---
+++ Ciclo 2/JavaScript/Arquivos/aula22.js
@@ -0,0 +1,32 @@
+const VALOR_PADRAO=0
+
+let valor=0
+
+console.log(valor)
+
+function add(v){       //adiciona um valor
+     valor+=v
+}
+add(10)
+
+console.log(valor)
+
+add(5)
+
+console.log(valor)
+
+
+
+
+
+
+
+function soma(n1=VALOR_PADRAO, n2=VALOR_PADRAO){
+    let res
+    res=n1+n2
+    return res
+}
+
+let resultado_soma=soma(5)
+//console.log(soma(resultado_soma))
+
