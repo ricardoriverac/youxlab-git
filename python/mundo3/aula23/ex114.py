f561b48b282f98cca528e53bc417bdd31b410635
Thawane <thawane.correa@institutoyoux.org.br>
2025-12-04 16:16:03 -0300

    ex114.py

--- python/mundo3/aula23/ex114.py
+++ python/mundo3/aula23/ex114.py
@@ -1,2 +1,10 @@
 #Crie um código em Python que teste se o site pudim está acessível pelo computador usado.
 
+import urllib
+import urllib.request
+try:
+    site = urllib.request.urlopen('https://www.pudim.com.br')
+except urllib.error.URLError:
+    print('Não consegui acessar.')
+else:
+    print('Consegui acessar.')
