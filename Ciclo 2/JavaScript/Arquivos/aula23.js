17d831b88c5a15edd779bbdf7fa27d8cb91809fb
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-12-04 13:56:56 -0500

    aula23 - Javascript

---
+++ Ciclo 2/JavaScript/Arquivos/aula23.js
@@ -0,0 +1,12 @@
+/* Parâmetros REST */
+
+function soma(...valores){
+    let tam = valores.length
+    let res = 0
+    for(let i=0;i<tam;i++){
+        res += valores [i]
+    }
+    return res
+}
+
+console.log(soma(10, 5, 7, 8))
