d128a6b98b748aae8078527a34ac51b80e12e1ef
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-12-08 13:47:00 -0500

    aula 29 feita

---
+++ Ciclo 2/JavaScript/Arquivos/aula29.js
@@ -0,0 +1,25 @@
+/* THIS dentro de funções */
+
+function aluno(nome, nota){
+    this.nome=nome
+    this.nota=nota
+
+    this.dados_anonimo=function(){
+        setTimeout(function(){ 
+            console.log(this.nome)
+            console.log(this.nota)
+        },2000)
+    }
+
+    this.dados_arrow=function(){
+        setTimeout(()=>{ 
+            console.log(this.nome)
+            console.log(this.nota)
+        },2000)
+    }
+
+}
+
+const al1=new aluno('Ana', 100)
+al1.dados_anonimo()
+al1.dados_arrow()
