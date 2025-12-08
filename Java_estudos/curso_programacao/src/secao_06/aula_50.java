12b3ff95202c438caa98013e29f2094ce479e3b2
agathae <agatha.silva-youxlab@institutoyoux.org.br>
2025-12-08 13:56:18 -0300

    Aula 50 de java while

---
+++ Java_estudos/curso_programacao/src/secao_06/aula_50.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class aula_50 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        while (x != 0) {
+            x = sc.nextInt();
+        }
+        sc.close();
+    }
+}
