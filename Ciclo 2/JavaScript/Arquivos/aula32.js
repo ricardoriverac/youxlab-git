40918fba6c97614b4021c238062765beeea0f549
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-12-10 13:36:26 -0500

    aula 32 feita

---
+++ Ciclo 2/JavaScript/Arquivos/aula32.js
@@ -0,0 +1,11 @@
+const cursosTodos=[...document.getElementsByClassName("curso")]
+const cursosC1=[...document.getElementsByClassName("c1")]
+const cursosC2=[...document.getElementsByClassName("c2")]
+
+console.log(cursosTodos)
+console.log(cursosC1)
+console.log(cursosC2)
+
+cursosC1.map((el, i)=>{
+    el.classList.add("destaque")
+})
