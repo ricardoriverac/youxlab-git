dd70d89312cff121ebd25e996d534cd5e68dad88
oigorromao <igor.faustino-youxlab@institutoyoux.org.br>
2025-12-10 13:39:53 -0300

    Exercicio 32

---
+++ javascript/aula31/aula31.js
@@ -0,0 +1,16 @@
+const dc1 = document.getElementById("c1")
+const dc2 = document.getElementById("c2")
+const dc3 = document.getElementById("c3")
+const dc4 = document.getElementById("c4")
+const dc5 = document.getElementById("dc5")
+
+const arrayElementos = [dc1, dc2, dc3, dc4, dc5, dc6]
+
+const colecaoHTML = document.getElementsByTagName("div")
+
+console.log(colecaoHTML);
+
+colecaoHTML.map((e)=>{
+    console.log(e);
+})
+
