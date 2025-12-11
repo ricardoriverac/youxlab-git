e8c2a8bc3ebde6c037cbd60de54118601e1410b9
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2025-12-11 11:08:02 -0300

    Aula 101 - Boxing, Unboxing e Wrapper

---
+++ Estudos/java/untitled1/src/application/a_101.java
@@ -0,0 +1,15 @@
+package application;
+
+public class a_101 {
+    public static void main(String[] args) {
+
+        int x = 20;
+        Integer obj = x;
+
+        System.out.println(obj);
+
+        int y = obj * 2;
+
+        System.out.println(y);
+    }
+}
