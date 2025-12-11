618d4817f84866bf94b6caf2d953dced65f9bb10
Youx <juan.ferreira-youxlab@institutoyoux.org.br>
2025-12-11 13:43:38 -0300

    aula35

---
+++ curso_javascript/aula35/aula35.js
@@ -0,0 +1,23 @@
+const caixa1=document.querySelector('#caixa1')
+const caixa2=document.querySelector('#caixa2')
+const btn=document.querySelector('#btn_trasferir')
+const todosCursos=[...document.querySelectorAll(".curso")]
+
+todosCursos.map((el)=>{
+    el.addEventListener('click',(evt)=>{
+        const curso=evt.target
+        console.log(curso);
+        curso.classList.toggle('selecionado')
+    })
+})
+
+btn_trasferir.addEventListener("click",()=>{
+    const cursosSelecionados=[...document.querySelectorAll(".selecionado")]
+    const cursosNãoSelecionados=[...document.querySelectorAll(".curso:not(.selecionado)")]
+    cursosSelecionados.map((el)=>{
+            caixa2.appendChild(el)
+    })   
+    cursosNãoSelecionados.map((el)=>{
+            caixa1.appendChild(el)
+    })  
+})
