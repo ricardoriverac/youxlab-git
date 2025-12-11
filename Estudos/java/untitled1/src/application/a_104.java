03744197a5fb27485b641b810dcf2eba9c479253
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2025-12-11 14:12:44 -0300

    Aula 104 - Listas parte 2

---
+++ Estudos/java/untitled1/src/application/a_104.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class a_104 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
+
+        list.add("Maria");
+        list.add("Alex");
+        list.add("Bob");
+        list.add("Anna");
+
+        for(String x : list){
+            System.out.println(x);
+        }
+        list.add(2, "Joao");
+        System.out.println(list.size());
+        list.removeIf(x -> x.charAt(0) == 'M');
+        for(String x : list){
+            System.out.println(x);
+        }
+        System.out.println("-------------------");
+        System.out.println(list.indexOf("Bob"));
+        System.out.println(list.indexOf("Maria"));
+        List<String> result = list.stream().filter(x -> x.charAt(0) == 'A').collect(Collectors.toList());
+        for (String x : result){
+            System.out.println(x);
+        }
+        System.out.println("--------------------");
+        String nome = list.stream().filter(x -> x.charAt(0) == 'A').findFirst().orElse(null);
+        System.out.println(nome);
+    }
+}
