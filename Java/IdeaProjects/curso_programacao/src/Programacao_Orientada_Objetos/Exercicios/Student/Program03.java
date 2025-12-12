4cb548b5476192d54d76e1f3da137a1baf11f6d3
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2025-12-11 21:08:19 -0300

    Bug do commit

--- Java/IdeaProjects/curso_programacao/src/Programacao_Orientada_Objetos/Exercicios/Student/Program03.java
+++ Java/IdeaProjects/curso_programacao/src/Programacao_Orientada_Objetos/Exercicios/Student/Program03.java
@@ -1,4 +1,32 @@
 package Programacao_Orientada_Objetos.Exercicios.Student;
 
+import java.awt.*;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program03 {
+    static void main() {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Student stu = new Student();
+
+        System.out.print("Nome: ");
+        stu.name = sc.nextLine();
+        System.out.print("Primeiro Trimestre: ");
+        stu.nota1 = sc.nextDouble();
+        System.out.print("Segundo Trimestre: ");
+        stu.nota2 = sc.nextDouble();
+        System.out.print("Terceiro Trimestre: ");
+        stu.nota3 = sc.nextDouble();
+        double notaFinal = stu.nota1 + stu.nota2 + stu.nota3;
+
+        System.out.printf("FINAL GRADE = %.2f%n", notaFinal);
+        if (stu.boletim(notaFinal) > 60) {
+            System.out.println("PASS");
+        }
+        else {
+            System.out.println("FAILED");
+            System.out.printf("MISSING %.2f POINTS", stu.boletim(notaFinal));
+        }
+    }
 }
