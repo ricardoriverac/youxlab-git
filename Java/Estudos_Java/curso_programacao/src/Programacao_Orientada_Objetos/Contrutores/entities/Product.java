b1bd697f627d7f0ced284a3bf7c6a166eacb8f3e
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2025-12-15 08:51:53 -0300

    AULA DE CONSTRUTORES - Codigo do product com construtor

---
+++ Java/Estudos_Java/curso_programacao/src/Programacao_Orientada_Objetos/Contrutores/entities/Product.java
@@ -0,0 +1,35 @@
+package Programacao_Orientada_Objetos.Contrutores.entities;
+
+public class Product {
+    public String name;
+    public double price;
+    public int quantity;
+
+    public Product(String name, double price, int quantity) { // construtor
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public double totalValueInStock() {
+        return price * quantity;
+    }
+
+    public void addProducts(int quantity) {
+        this.quantity += quantity; // soma a quantidade da classe Produto com a quantidade que foi dada como argumento
+    }
+
+    public void removeProducts(int quantity) {
+        this.quantity -= quantity; // subtrai a quantidade da classe Produto com a quantidade que foi dada como argumento
+    }
+
+    public String toString() {
+        return name
+                + ", $ "
+                + String.format("%.2f", price)
+                + ", "
+                + quantity
+                + " units, Total: $ "
+                + String.format("%.2f", totalValueInStock());
+    }
+}
