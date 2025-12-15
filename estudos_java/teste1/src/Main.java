6017922cdd451d3fcbecf773f17cc55ecbcbfe28
pietromarinho22 <pietrovmarinho@gmail.com>
2025-12-15 10:51:03 -0300

    Curso Java Estrutura Sequencial

---
+++ estudos_java/teste1/src/Main.java
@@ -0,0 +1,64 @@
+/* import java.util.List;
+*  import java.util.Locale;
+*  import java.util.Scanner;*/
+
+public class Main {
+    public static void main(String[] args){
+
+        double x = 3.0;
+        double y = 4.0;
+        double z = -5.0;
+        double A, B, C;
+
+        A = Math.sqrt(x);
+        B = Math.sqrt(y);
+        C = Math.sqrt(25.0);
+        System.out.println("Raiz quadrada de " + x + " = " + A);
+        System.out.println("%Raiz quadrada de " + y + " = " + B);
+        System.out.println("Raiz quadrada de 25 = " + C);
+
+        A = Math.pow(x, y);
+        B = Math.pow(x, 2.0);
+        C = Math.pow(5.0, 2.0);
+        System.out.println(x + " elevado a " + y + " = " + A);
+        System.out.println(x + " elevado ao quadrado = " + B);
+        System.out.println("5 elevado ao quadrado = " + C);
+
+        A = Math.abs(y);
+        B = Math.abs(z);
+        System.out.println("Valor absoluto de " + y + " = " + A);
+        System.out.println("Valor absoluto de " + z + " = " + B);
+
+    }
+}
+
+/* %n quebra linha
+*  %f float
+*  %s string
+*  %d int
+*
+*  print escreve sem formatação e quebra de linha
+*  printf escreve com formatação
+*  println escreve e faz a quebra de linha
+*
+*  Boa Prática:
+*  Colocar .0 em variáveis double, ex: 9.0
+*  Colocar f em variáveis float, ex: 9f
+*
+*  Locale define se a variável vai ser com "." ou ","
+*
+*  Next você tem que diferenciar se é Int, Float, Double
+*  charAt exibe o caracter que
+*  nextLine para frase
+*
+* Código do Scanner:
+*  Scanner sc = new Scanner(System.in)
+* */
+
+
+
+
+
+
+
+
---
+++ estudos_java/teste1/src/exercicio_4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class exercicio_4 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int idemployee = sc.nextInt();
+        double wagePerHour = sc.nextDouble();
+        double hour = sc.nextDouble();
+        double salary = wagePerHour * hour;
+
+        System.out.printf("EMPLOYEE ID NUMBER = %s",idemployee);
+        System.out.printf("%nSALARY = U$ %.2f",salary);
+    }
+}
