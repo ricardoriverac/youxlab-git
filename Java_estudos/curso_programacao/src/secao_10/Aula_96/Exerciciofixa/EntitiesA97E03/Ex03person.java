beb5b61e69d23b1602272b6cf50e53f3cc6a9556
agathae <agatha.silva-youxlab@institutoyoux.org.br>
2025-12-16 11:47:38 -0300

    Exercicio 03 vetor

---
+++ Java_estudos/curso_programacao/src/secao_10/Aula_96/Exerciciofixa/EntitiesA97E03/Ex03person.java
@@ -0,0 +1,26 @@
+package secao_10.Aula_96.Exerciciofixa.EntitiesA97E03;
+
+public class Ex03person {
+    private String name;
+    private  int age;
+
+    public Ex03person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ Java_estudos/curso_programacao/src/secao_10/Aula_96/Exerciciofixa/exercicio03vet.java
@@ -0,0 +1,41 @@
+package secao_10.Aula_96.Exerciciofixa;
+
+import secao_10.Aula_96.Exerciciofixa.EntitiesA97E03.Ex03person;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio03vet {
+    static void main() {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantas pessoas você quer digitar? ");
+        int qntPessoas = sc.nextInt();
+        String[] nomes = new String[qntPessoas];
+        int[] idades = new int[qntPessoas];
+
+        for (int i = 0; i < qntPessoas; i++) {
+            System.out.println("Dados da " + (i + 1) + "a pessoa: ");
+            System.out.print("Name: ");
+            nomes[i] = sc.next();
+
+            System.out.print("Age: ");
+            idades[i] = sc.nextInt();
+
+        }
+
+        int maioridade = idades[0];
+        int posicaomaior = 0;
+
+        for (int i = 1; i < qntPessoas; i++) {
+            if (idades[i] > maioridade) {
+                maioridade = idades[i];
+                posicaomaior = i;
+            }
+        }
+
+        System.out.println("Pessoa mais velha é: " + nomes[posicaomaior]);
+
+    }
+}
