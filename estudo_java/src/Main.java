13750c1fe1143aa437ac8a6135a063ed2d12f157
desafio_python_git <ottofillipi185@gmail.com>
2025-12-16 14:49:08 -0300

    estudo_java

---
+++ estudo_java/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        /*Esse comando serve para fazer qualquer
+        tipo de valor apareça no terminal*/
+
+        System.out.println("Olá, Mundo!!!");
+
+    }
+}
