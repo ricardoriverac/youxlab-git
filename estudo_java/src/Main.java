ae48e3a3f40358b6362d249d3ddeb25cf40cf31d
desafio_python_git <ottofillipi185@gmail.com>
2025-12-17 14:54:50 -0300

    Aula_30-Saida_de_dados_java

--- estudo_java/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        /*Esse comando serve para fazer qualquer
-        tipo de valor apareça no terminal*/
-
-        System.out.println("Olá, Mundo!!!");
-
-    }
-}
