e5e1f45ce064ef7e02a216bcaed5b8b86ae020a6
agathae <agatha.silva-youxlab@institutoyoux.org.br>
2025-12-17 15:46:09 -0300

    Aulas da secao 10 e aula sobre listas

---
+++ Java_estudos/curso_programacao/src/secao_10/Aula103/Aula103.java
@@ -0,0 +1,13 @@
+package secao_10.Aula103;
+
+public class Aula103 {
+    static void main() {
+        String[] vect = new String[] {"Maria", "Bob", "Alex"};
+        for (int i=0; i< vect.length; i++) {
+            System.out.println(vect[i]);
+        }
+        for (String obj : vect) {
+            System.out.println(obj);
+        }
+    }
+}
---
+++ Java_estudos/curso_programacao/src/secao_10/aula102/aula102.java
@@ -0,0 +1,7 @@
+package secao_10.aula102;
+
+public class aula102 {
+        public String name;
+        public Double price;
+        public Integer quantity;
+}
