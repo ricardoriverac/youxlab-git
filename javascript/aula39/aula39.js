32617f76d8cfbc51d906c95947151840b397cb6d
oigorromao <igor.faustino-youxlab@institutoyoux.org.br>
2026-01-06 10:41:30 -0300

    exercicio 39

--- javascript/aula39/aula39.js
+++ javascript/aula39/aula39.js
@@ -1,7 +1,23 @@
 const caixa1 = document.querySelector("#caixa1")
 const btn_c = [...document.querySelectorAll(".curso")]
+const c1_2 = document.querySelector("#c1_2")
 
-console.log(caixa1.hasChildNodes());
+console.log(c1_2.parentNode);
+
+// console.log(caixa1.hasChildNodes());
+// console.log(btn_c[0].hasChildNodes());
+// console.log(btn_c[0].childNodes);
+
+// console.log(caixa1.children.length > 0 ? "POssui filhos" : "Nâo possui filhos")
+
+// console.log(caixa1.children[1].innerHTML = "TESTE");
+
+
+// if(btn_c[0].children.length > 0){
+//     console.log("Possui filhos");
+// }else{
+//      console.log("Não possui filhos")
+// }
 
 // console.log(caixa1.firstElementChild); //o ultimo
 // console.log(caixa1.lastElementChild); //o primeiro
