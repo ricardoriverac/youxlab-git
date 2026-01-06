cf8518e30f8756250003077fe1d1f70c06a8551d
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2026-01-06 08:48:49 -0300

    Aula 51 e desafio

--- Ciclo 2/JavaScript/Desafio logica /desafio2.js
+++ Ciclo 2/JavaScript/Desafio logica /desafio2.js
@@ -20,17 +20,83 @@ const produtos = [
 { nome: "Notebook Gamer", categoria: "Eletrônicos", preco: 7500, estoque: 2 }, 
 { nome: "Estabilizador", categoria: "Energia", preco: 350, estoque: 5 } 
 ];
-let valoresEstoque = 0
-const produtosRedes = []
+
+let redeEstoque = 0
+let energiaEstoque = 0
+let iluminacaoEstoque = 0
+let eletronicosEstoque = 0
+let perifericosEstoque = 0
+let acessoriosEstoque = 0
+let armazenamentoEstoque = 0
+let moveisEstoque = 0 
+let utilidadesEstoque = 0 
+
+
+const produtosRede = []
 const produtosEnergia = []
+const produtosIluminacao = []
+const produtosEletronicos = []
+const produtosPerifericos = []
+const produtosAcessorios = []
+const produtosArmazenamento = []
+const produtosMoveis = []
+const produtosUtilidades = []
 
 produtos.forEach(produto => {
-    if (produto.categoria === 'Redes') {
-        console.log(produto.estoque, 'Redes')
-        valoresEstoque += produto.estoque
-        produtosRedes.push(produto.nome)
+    if (produto.categoria === 'Rede') {
+        redeEstoque += produto.estoque
+        produtosRede.push(produto.nome)
     } else if (produto.categoria === 'Energia'){
-        valoresEstoque += produto.estoque
+        energiaEstoque += produto.estoque
         produtosEnergia.push(produto.nome)
-    } 
-})
+    } else if (produto.categoria === 'Iluminação'){
+        iluminacaoEstoque += produto.estoque
+        produtosIluminacao.push(produto.nome)
+    }else if (produto.categoria === 'Eletrônicos'){
+        eletronicosEstoque += produto.estoque
+        produtosEletronicos.push(produto.nome)
+    }else if (produto.categoria === 'Periféricos'){
+        perifericosEstoque += produto.estoque
+        produtosPerifericos.push(produto.nome)
+    }else if (produto.categoria === 'Acessórios'){
+        acessoriosEstoque += produto.estoque
+        produtosAcessorios.push(produto.nome)
+    }else if (produto.categoria === 'Armazenamento'){
+        armazenamentoEstoque += produto.estoque
+        produtosArmazenamento.push(produto.nome)
+    }else if (produto.categoria === 'Móveis'){
+        moveisEstoque += produto.estoque
+        produtosMoveis.push(produto.nome)
+    }else if (produto.categoria === 'Utilidades'){
+        utilidadesEstoque += produto.estoque
+        produtosUtilidades.push(produto.nome)}
+    })
+
+console.log('------------------------------------------------------------------------------');
+console.log('Categoria Rede: ' + produtosRede);
+console.log('Quantidade produtos rede: ' + redeEstoque );
+console.log( );
+console.log('Categoria Energia: ' + produtosEnergia);
+console.log('Quantidade produtos energia: ' + energiaEstoque);
+console.log( );
+console.log('Categoria Iluminação: ' + produtosIluminacao);
+console.log('Quantidade produtos Iluminação: ' + iluminacaoEstoque);
+console.log( );
+console.log('Categoria Eletrônicos: ' + produtosEletronicos);
+console.log('Quantidade produtos eletrônicos: ' + eletronicosEstoque);
+console.log( );
+console.log('Categoria Perifericos: ' + produtosPerifericos);
+console.log('Quantidade produtos perifericos: ' + perifericosEstoque);
+console.log( );
+console.log('Categoria Acessorios: ' + produtosAcessorios);
+console.log('Quantidade produtos acessorios: ' + acessoriosEstoque);
+console.log( );
+console.log('Categoria Armazenamento: ' + produtosArmazenamento);
+console.log('Quantidade produtos Armazenamento: ' + armazenamentoEstoque);
+console.log( );
+console.log('Categoria Moveis: ' + produtosMoveis);
+console.log('Quantidade produtos moveis: ' + moveisEstoque);
+console.log( )
+console.log('Categoria Utilidades: ' + produtosUtilidades);
+console.log('Quantidade produtos utilidades: ' + utilidadesEstoque);
+console.log('------------------------------------------------------------------------------');
