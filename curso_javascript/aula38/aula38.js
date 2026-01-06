83d6faa7cfc0392d4efa6bb1b4d25d8ee414a8b9
Youx <juan.ferreira-youxlab@institutoyoux.org.br>
2026-01-06 10:37:07 -0300

    aula40

--- curso_javascript/aula38/aula38.js
+++ curso_javascript/aula38/aula38.js
@@ -1,6 +1,9 @@
 const caixa1=document.querySelector("#caixa1")
 const btn_c=[...document.querySelector(".curso")]
 
-console.log(caixa1.firstElementChild);
-console.log(caixa1.lastElementChild);
-console.log(caixa1.children);
+console.log(btn_c[0].getRootNote());
+console.log(btn_c[0].ownerDocument);
+
+// console.log(caixa1.firstElementChild);
+// console.log(caixa1.lastElementChild);
+// console.log(caixa1.children);
---
+++ curso_javascript/aula39/aula39.js
@@ -0,0 +1,16 @@
+const caixa1=document.querySelector("#caixa1")
+const btn_c=[...document.querySelector(".curso")]
+const c1_2=document.querySelector("c1_2")
+
+console.log(c1_2.parentNode.parentNode.children[4]);
+
+// console.log(caixa1.hasChildNotes());
+// console.log(btn_c[0].hasChildNotes());
+// console.log(btn_c[0].childNotes);
+
+// console.log(caixa1.children.length > 0 ? "possui filhos" : "Não possui filhos")
+
+
+// console.log(caixa1.firstElementChild);
+// console.log(caixa1.lastElementChild);
+// console.log(caixa1.children);
