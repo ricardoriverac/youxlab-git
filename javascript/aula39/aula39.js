f658c2127df0c0352d62bc1a02d3766310354c2a
oigorromao <igor.faustino-youxlab@institutoyoux.org.br>
2026-01-06 09:06:15 -0300

    exercicio 39

---
+++ javascript/aula39/aula39.js
@@ -0,0 +1,8 @@
+const caixa1 = document.querySelector("#caixa1")
+const btn_c = [...document.querySelectorAll(".curso")]
+
+console.log(caixa1.hasChildNodes());
+
+// console.log(caixa1.firstElementChild); //o ultimo
+// console.log(caixa1.lastElementChild); //o primeiro
+// console.log(childrem); // todos
