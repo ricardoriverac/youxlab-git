4dcbb7ac6bbc15ab66f6b0a325d3237b2ba0054e
agathae <agatha.silva-youxlab@institutoyoux.org.br>
2026-01-07 11:37:48 -0300

    Exercicio fixacao aula 133 secao 12

---
+++ Java_estudos/curso_programacao/src/secao_12/exercicioFixa2/entitiesfixa/Comment.java
@@ -0,0 +1,25 @@
+package secao_12.exercicioFixa2.entitiesfixa;
+
+public class Comment {
+    private String text;
+
+
+    public Comment(){
+
+    }
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+
+    public String getText() {
+        return text;
+    }
+
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
