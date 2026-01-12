48cbae7384f8cbe5f97223a44c4b8eede7703799
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-01-12 08:55:55 -0300

    Desafio poo2 em andamento - Busca por id/status

---
+++ Estudos/java/untitled1/src/application/POO/desafioPOO2.java
@@ -0,0 +1,53 @@
+package application.POO;
+
+import application.entities.OrderStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import static application.POO.Status.EM_ANDAMENTO;
+
+public class desafioPOO2 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        int menu = 0;
+        List<Produto> produtos = new ArrayList<>();
+        List<Pedido> pedidos = new ArrayList<>();
+        produtos.add(new Produto(1, "TV", 1000.0));
+        produtos.add(new Produto(2, "Mouse", 40.0));
+        produtos.add(new Produto(3, "Computador", 3000.0));
+        produtos.add(new Produto(4, "Alexa", 800.0));
+        produtos.add(new Produto(5, "Celular", 3000.0));
+        produtos.add(new Produto(6, "Fone", 70.0));
+        pedidos.add(new Pedido(1, new Cliente(1, "Ruan", "Ruan@gmail.com"), produtos, new Endereco("Rua Joaquim Olímpio de Carvalho", "1200 A", "Lavras", "Minas Gerais"), Status.EM_ANDAMENTO));
+        pedidos.add(new Pedido(2, new Cliente(2, "Davi", "Davi@gmail.com"), produtos, new Endereco("Rua José Giarola", "200 A", "Lavras", "Minas Gerais"), Status.EM_ANDAMENTO));
+        pedidos.add(new Pedido(3, new Cliente(3, "Renan", "Renan@gmail.com"), produtos, new Endereco("Rua raimunda Marques Guimaraẽs", "40 B", "Lavras", "Minas Gerais"), Status.EM_ANDAMENTO));
+        pedidos.add(new Pedido(4, new Cliente(4, "Ravi", "Ravi@gmail.com"), produtos, new Endereco("Rua 2", "140", "Ijaci", "Minas Gerais"), Status.EM_ANDAMENTO));
+        pedidos.add(new Pedido(5, new Cliente(5, "João", "João@gmail.com"), produtos, new Endereco("Rua Primeiro de maio", "275", "Belo Horizonte", "Minas Gerais"), Status.EM_ANDAMENTO));
+
+        while (menu!= 3){
+            System.out.print("\nCaro usuário, escolha uma das opções abaixo: ");
+            System.out.println("1- Buscar pedido por ID\n"
+                    + "2- Buscar pedido por Status\n"
+                    + "3- Sair");
+        }
+        menu = sc.nextInt();
+
+        switch (menu){
+            case 1:
+                System.out.print("Caro usuário, por favor informe o id de seu pedido: ");
+                Integer idVerificacao = sc.nextInt();
+                System.out.print("\nCaro usuário, seu pedido de id" + idVerificacao + ", é composto por:" + pedidos.stream().filter(p -> p.getId().equals(idVerificacao)).findFirst().orElse(null));
+
+            case 2:
+                System.out.print("Caro usuário, por favor informe o status de seu pedido: ");
+                String statusVerificado = sc.next();
+                System.out.print("\nCaro usuário, seu pedido é: " + pedidos.stream().filter(p -> p.getStatus() == statusVerificado).toList());)
+
+            }
+        }
+    }
+}
---
+++ Estudos/java/untitled1/src/application/a_150.java
@@ -0,0 +1,4 @@
+package application;
+
+public class a_150 {
+}
