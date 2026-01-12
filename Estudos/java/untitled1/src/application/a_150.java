1016ce185c1452f38e392966dcab4b4aa16ea7cb
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-01-12 09:01:57 -0300

    Aula 150 - Bloco try-catch

--- Estudos/java/untitled1/src/application/a_150.java
+++ Estudos/java/untitled1/src/application/a_150.java
@@ -1,4 +1,24 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class a_150 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try{
+            String[] vect = sc.nextLine().split(" ");
+            int posicao = sc.nextInt();
+            System.out.println(vect[posicao]);
+        }
+
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.print("Posição inválida");
+        }
+        catch (InputMismatchException e){
+            System.out.print("Erro de input");
+        }
+        System.out.print("Fim do programa! ");
+    }
 }
