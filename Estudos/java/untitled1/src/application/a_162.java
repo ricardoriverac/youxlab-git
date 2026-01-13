724b2a6d33de17d0a1b7159bbc3624d3df6cc82b
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-01-13 10:59:42 -0300

    Aula 162 - FileReader e BufferedReader

---
+++ Estudos/java/untitled1/src/application/a_162.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class a_162 {
+    public static void main(String[] args) {
+        String caminho = "/home/youx/in.txt";
+        BufferedReader br = null;
+        FileReader fr = null;
+
+        try{
+            fr = new FileReader(caminho);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+
+            while(line != null){
+                System.out.println(line);
+                 line = br.readLine();
+            }
+        }
+        catch (IOException e){
+            System.out.print("Erro: " + e.getMessage());
+        }
+        finally {
+            try {
+                if(br != null)
+                    br.close();
+                if(fr != null)
+                    fr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
