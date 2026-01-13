b7e10f94cee1fe5f7fd065a6d7a7f15a988574cf
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-01-13 11:38:30 -0300

    Aula 163 - BufferedWriter e FileWriter

---
+++ Estudos/java/untitled1/src/application/a_163.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class a_163 {
+    public static void main(String[] args) {
+
+        String [] lines = new String[] {"Good Morning", "Good afternoon", "Good night", "Good evening"};
+        String caminho = "/home/youx/out.txt";
+
+        try (BufferedWriter br = new BufferedWriter(new FileWriter(caminho, true))){
+            for(String l: lines ){
+                br.write(l);
+                br.newLine();
+            }
+        } catch (IOException e) {
+            System.out.print("Erro: " + e.getMessage());
+        }
+    }
+}
