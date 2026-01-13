a456856ca765d656475c06e15989af083442342e
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-01-13 11:57:35 -0300

    Aula 164 - Manipulando pastas com file

---
+++ Estudos/java/untitled1/src/application/a_164.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class a_164 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insira o caminho do arquivo: ");
+        String strCaminho = sc.next();
+
+        File caminho = new File(strCaminho);
+
+        File[] folders = caminho.listFiles(File::isDirectory);
+        System.out.println("Arquivos: ");
+        for(File folder : folders){
+            System.out.println(folder);
+        }
+
+        File[] files = caminho.listFiles(File::isFile);
+        System.out.println("Files: ");
+        for (File file : files){
+            System.out.println(file);
+        }
+
+        boolean sucess = new File(strCaminho + "//pastateste").mkdir();
+        System.out.println("DIretório criado com sucesso" + sucess);
+        sc.close();
+    }
+}
