3bd844a4bb13a6f030f76b3cddafdc52db469273
oigorromao <igor.faustino-youxlab@institutoyoux.org.br>
2026-01-14 09:10:29 -0300

    exercicio 40

---
+++ atividade.crud/crud.js
@@ -0,0 +1,64 @@
+const botaoInserir = document.getElementById("botaoInserir");
+
+function limparcontainer() {
+    const inputs = document.getElementsByClassName("informacoes");
+    for (limpar of inputs) {
+        limpar.value = "";
+    }
+}
+
+
+function inserir() {
+    const valornome = document.getElementById("input-nome").value
+    const valorTelefone = document.getElementById("input-telefone").value
+    const valorCPF = document.getElementById("input-cpf").value
+    const valorEmail = document.getElementById("input-email").value
+    const corpotabela = document.getElementById("tabelaCorpo")
+
+    let novaLinha = document.createElement("tr")
+    novaLinha.innerHTML = `
+        <td>${valornome}</td>
+        <td>${valorTelefone}</td>
+        <td>${valorCPF}</td>
+        <td>${valorEmail}</td>
+        <td>
+        <button id="btn_remover" >Remover</button>
+        <button onclick=editar(this)>Editar</button>
+        </td>
+        `;
+    corpotabela.appendChild(novaLinha)
+
+    const remove = document.getElementById("btn_remover");
+    remove.addEventListener("click", (evt) => {
+        novaLinha.remove();
+    })
+}
+
+botaoInserir.addEventListener("click", function () {
+    inserir()
+    limparcontainer()
+})
+
+function editar (editarBotao) {
+    const editarNome = document.getElementById("input-nome")
+    const editarTelefone = document.getElementById("input-telefone")
+    const editarCPF = document.getElementById("input-cpf")
+    const editarEmail = document.getElementById("input-email")
+
+    const novaLinha2 = editarBotao.closest('tr')
+
+    if (novaLinha2) {
+        const dados = novaLinha2.querySelectorAll("td")
+
+        editarNome.value = dados[0].textContent
+        editarTelefone.value = dados[1].textContent
+        editarCPF.value = dados[2].textContent
+        editarEmail.value = dados[3].textContent
+
+        novaLinha2.remove()
+    }
+}
+
+
+
+
