03677f96b5b6729f96a7741451a30c9407c91099
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-01-14 11:11:45 -0300

    Exercicio de Fixação 1 - Seção 13

---
+++ Java/Estudos_Java/curso_programacao/src/herancaPolimorfismo/Exercicios/ExerciciosFixacao/exer01/entities/UsedProduct.java
@@ -0,0 +1,36 @@
+package herancaPolimorfismo.Exercicios.ExerciciosFixacao.exer01.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UsedProduct extends Product{
+
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    private Date manuFactureDate;
+
+    public UsedProduct(){
+    }
+
+    public UsedProduct(String name, Double price, Date manuFactureDate) {
+        super(name, price);
+        this.manuFactureDate = manuFactureDate;
+    }
+
+    public Date getManuFactureDate() {
+        return manuFactureDate;
+    }
+
+    public void setManuFactureDate(Date manuFactureDate) {
+        this.manuFactureDate = manuFactureDate;
+    }
+
+    @Override
+    public String priceTag() {
+        return getName()
+                + " (used) $ "
+                + String.format("%.2f", getPrice())
+                + " (Manufacture date: "
+                + sdf.format(manuFactureDate)
+                + ")";
+    }
+}
