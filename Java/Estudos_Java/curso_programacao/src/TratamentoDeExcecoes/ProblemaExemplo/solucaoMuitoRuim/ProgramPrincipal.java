691b5817a11288c900be84179f04275671f37ef1
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-01-15 11:16:22 -0300

    Organizando print - SOLUCAO MUITO RUIM - Problema Exemplo

--- Java/Estudos_Java/curso_programacao/src/TratamentoDeExcecoes/ProblemaExemplo/solucaoMuitoRuim/ProgramPrincipal.java
+++ Java/Estudos_Java/curso_programacao/src/TratamentoDeExcecoes/ProblemaExemplo/solucaoMuitoRuim/ProgramPrincipal.java
@@ -28,7 +28,7 @@ public class ProgramPrincipal {
             Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
             System.out.println("Reservation: " + reservation);
 
-            System.out.print("Enter data to update the reservation: ");
+            System.out.println("Enter data to update the reservation: ");
             System.out.print("Check-in date (DD/MM/YYYY): ");
             checkIn = sdf.parse(sc.next());
             System.out.print("Check-out date (DD/MM/YYYY): ");
