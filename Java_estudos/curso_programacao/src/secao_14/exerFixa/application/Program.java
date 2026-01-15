52b30760e30be519ca0221d1bbf3cd9a4ddd0797
agathae <agatha.silva-youxlab@institutoyoux.org.br>
2026-01-15 08:40:09 -0300

    Exercicio de fixacao Execoes

---
+++ Java_estudos/curso_programacao/src/secao_14/exerFixa/application/Program.java
@@ -0,0 +1,44 @@
+package secao_14.exerFixa.application;
+
+import secao_14.exerFixa.entities.Account;
+import secao_14.exerFixa.exeptions.BusinessException;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    static void main() {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe os dados da conta");
+        System.out.print("Numero: ");
+        int number = sc.nextInt();
+        System.out.print("Titular: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+        System.out.print("Saldo inicial: ");
+        double balance = sc.nextDouble();
+        System.out.print("Limite de saque: ");
+        double withdrawLimit = sc.nextDouble();
+
+        Account acc = new Account(number, holder, balance, withdrawLimit);
+
+        System.out.println();
+        System.out.print("Informe uma quantia para sacar: ");
+        double amount = sc.nextDouble();
+
+        try {
+            acc.withdraw(amount);
+            System.out.printf("Novo saldo: %.2f%n", acc.getBalance());
+        }
+        catch (BusinessException e) {
+            System.out.println(e.getMessage());
+        }
+
+        sc.close();
+    }
+
+
+}
