02124d8c2a28c7f1155b1a7f4fddd79d872301cf
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-01-15 11:15:22 -0300

    Problema exemplo - SOLUÇÃO RUIM

---
+++ Java/Estudos_Java/curso_programacao/src/TratamentoDeExcecoes/ProblemaExemplo/solucaoRuim/ProgramPrincipal.java
@@ -0,0 +1,45 @@
+package TratamentoDeExcecoes.ProblemaExemplo.solucaoRuim;
+
+import TratamentoDeExcecoes.ProblemaExemplo.model.entities.Reservation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class ProgramPrincipal {
+    static void main() throws ParseException {
+
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Room number: ");
+        int roomNumber = sc.nextInt();
+        System.out.print("Check-in date (DD/MM/YYYY): ");
+        Date checkIn = sdf.parse(sc.next());
+        System.out.print("Check-out date (DD/MM/YYYY): ");
+        Date checkOut = sdf.parse(sc.next());
+
+        if (!checkOut.after(checkIn)) {
+            System.out.println("Error in reservation: Check-out date must be after check-in date");
+        } else {
+            Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
+
+            System.out.println();
+            System.out.println("Enter data to update the reservation: ");
+            System.out.print("Check-in date (DD/MM/YYYY): ");
+            checkIn = sdf.parse(sc.next());
+            System.out.print("Check-out date (DD/MM/YYYY): ");
+            checkOut = sdf.parse(sc.next());
+
+            String error = reservation.updateDates(checkIn, checkOut);
+            if (error != null) {
+                System.out.println("Error in reservation: " + error);
+            }
+            else {
+                System.out.println("Reservation: " + reservation);
+            }
+        }
+    }
+}
