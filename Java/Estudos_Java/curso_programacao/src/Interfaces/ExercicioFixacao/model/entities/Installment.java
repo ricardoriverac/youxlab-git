4ab5913c8b8c744bde5f79803f861170dc69eee8
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-01-19 16:03:03 -0300

    Exercicio de fixação - Interface

---
+++ Java/Estudos_Java/curso_programacao/src/Interfaces/ExercicioFixacao/model/entities/Installment.java
@@ -0,0 +1,39 @@
+package Interfaces.ExercicioFixacao.model.entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class Installment {
+
+    DateTimeFormatter dmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private LocalDate dueDate;
+    private Double amount;
+
+    public Installment(LocalDate dueDate, Double amount) {
+        this.dueDate = dueDate;
+        this.amount = amount;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return dmt.format(dueDate) +
+                " - " + String.format("%.2f%n", amount);
+    }
+}
