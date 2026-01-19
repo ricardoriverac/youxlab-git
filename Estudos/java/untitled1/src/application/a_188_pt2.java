7c0e24006982943d1593732d8b7478ee32f8042b
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-01-19 15:27:21 -0300

    Aula 188 - Demonstrando erro de compilação

---
+++ Estudos/java/untitled1/src/application/a_188_pt2.java
@@ -0,0 +1,12 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class a_188_pt2 {
+//   public static void main(String[] args) {
+//        List<?> list = new ArrayList<Integer>();
+//        list.add(3);
+//    }
+//}
+//isso dara erro de compilação
