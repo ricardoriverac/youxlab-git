44174553c28d594cc5ed04641334181ef2af0857
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-01-21 13:14:06 -0300

    Como o Set tsta igualdade - Set

---
+++ Java/Estudos_Java/curso_programacao/src/GenericsSetMap/Set/igualdade/application/Program.java
@@ -0,0 +1,23 @@
+package GenericsSetMap.Set.igualdade.application;
+
+import GenericsSetMap.Set.igualdade.entities.Product;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Program {
+
+    static void main() {
+
+        Set<Product> set = new HashSet<>();
+
+        set.add(new Product("TV", 900.0));
+        set.add(new Product("Notebook", 1200.0));
+        set.add(new Product("Tablet", 400.0));
+
+        Product prod = new Product("Notebook", 1200.0);
+
+        System.out.println(set.contains(prod));
+
+    }
+}
