62fdcdc34b37b0c63fe40cdeb217b567ef477cb7
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-01-21 12:27:07 -0300

    HashCode e Equals - aula 190

---
+++ Java/Estudos_Java/curso_programacao/src/GenericsSetMap/hashCode_Equals/hashCode/Program.java
@@ -0,0 +1,13 @@
+package GenericsSetMap.hashCode_Equals.hashCode;
+
+public class Program {
+
+    static void main() {
+
+        String a = "Maria";
+        String b = "Alex";
+
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+    }
+}
