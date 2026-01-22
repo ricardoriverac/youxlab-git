bbe60efd401583fc0f63ac3e026b5ad94d792c3c
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-01-22 15:51:44 -0300

    Reference method com método não estático e  Expressão lambda declarada - Consumer

--- Java/Estudos_Java/curso_programacao/src/ProgramacaoFuncionalExpressaoLambda/entities/Product.java
+++ Java/Estudos_Java/curso_programacao/src/ProgramacaoFuncionalExpressaoLambda/entities/Product.java
@@ -44,4 +44,9 @@ public class Product {
         p.setPrice(p.getPrice() * 1.1);
     }
 
+    public void noStaticPriceUpdate(){
+        setPrice(getPrice() * 1.1);
+        // price = price * 1.1
+    }
+
 }
