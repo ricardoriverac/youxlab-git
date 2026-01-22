48d3d1d8b7bdacfbad2390381b26ef8fd68ee6cf
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-01-22 15:42:00 -0300

    Implementando a interface Consumer - Consumer

---
+++ Java/Estudos_Java/curso_programacao/src/ProgramacaoFuncionalExpressaoLambda/Consumer/util/PriceUpdate.java
@@ -0,0 +1,13 @@
+package ProgramacaoFuncionalExpressaoLambda.Consumer.util;
+
+import ProgramacaoFuncionalExpressaoLambda.entities.Product;
+
+import java.util.function.Consumer;
+
+public class PriceUpdate implements Consumer<Product> {
+
+    @Override
+    public void accept(Product p) {
+        p.setPrice(p.getPrice() * 1.1);
+    }
+}
