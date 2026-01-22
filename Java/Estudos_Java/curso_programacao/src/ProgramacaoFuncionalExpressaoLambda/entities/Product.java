059cf57eebebd69dbbce017cea50860c68bc4f0e
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-01-22 13:55:34 -0300

    Reference method com método não estático - Predicate

--- Java/Estudos_Java/curso_programacao/src/ProgramacaoFuncionalExpressaoLambda/entities/Product.java
+++ Java/Estudos_Java/curso_programacao/src/ProgramacaoFuncionalExpressaoLambda/entities/Product.java
@@ -30,6 +30,10 @@ public class Product {
         return p.getPrice() >= 100.0;
     }
 
+    public boolean noStaticProductPredicate(){
+        return price >= 100.0;
+    }
+
     @Override
     public String toString() {
         return name +
