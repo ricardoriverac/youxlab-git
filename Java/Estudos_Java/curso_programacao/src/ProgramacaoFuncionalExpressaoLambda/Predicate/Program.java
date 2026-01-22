88c345aeda6737458c55be97f80f0917c32cd4a1
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-01-22 13:50:04 -0300

    Instanciando a interface Predicate

---
+++ Java/Estudos_Java/curso_programacao/src/ProgramacaoFuncionalExpressaoLambda/Predicate/Program.java
@@ -0,0 +1,29 @@
+package ProgramacaoFuncionalExpressaoLambda.Predicate;
+
+import ProgramacaoFuncionalExpressaoLambda.Predicate.util.ProductPredicate;
+import ProgramacaoFuncionalExpressaoLambda.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Program {
+
+    static void main() {
+
+        Locale.setDefault(Locale.US);
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        list.removeIf(new ProductPredicate());
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+
+    }
+}
--- Java/Estudos_Java/curso_programacao/src/ProgramacaoFuncionalExpressaoLambda/entities/Product.java
+++ Java/Estudos_Java/curso_programacao/src/ProgramacaoFuncionalExpressaoLambda/entities/Product.java
@@ -26,12 +26,15 @@ public class Product {
         this.price = price;
     }
 
+
+    public static boolean staticProductPredicate(Product p){
+        return p.getPrice() >= 100.0;
+    }
+
     @Override
     public String toString() {
-        return "Product [" +
-                "name=" + name +
-                ", price=" + price +
-                ']';
+        return name +
+                ", "+ String.format("%.2f", price);
     }
 
 }
