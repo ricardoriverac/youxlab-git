efa96ba95193b45c1625607f37bb89c87e7b9aaa
Thawane <thawane.correa@institutoyoux.org.br>
2026-01-23 16:29:01 -0300

    Exercicio1 aula37

---
+++ Java/CursoJava/src/Secao4/aula37/exercicio1.java
@@ -0,0 +1,21 @@
+package Secao4.aula37;
+
+import java.util.Scanner;
+
+public class exercicio1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x, y, z;
+        x = sc.nextInt();
+        y = sc.nextInt();
+        z = x + y;
+        System.out.println("Soma: " + x + " + " + y + " = " + z);
+
+
+        sc.close();
+
+
+
+    }
+}
