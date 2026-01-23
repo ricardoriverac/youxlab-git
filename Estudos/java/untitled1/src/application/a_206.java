3f0c1855d7b60ab5d344ee8f9de5009307cbf194
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-01-23 10:33:13 -0300

    Aula 206 - Funções que recebem funções como parâmetro

--- Estudos/java/untitled1/src/application/a_206.java
+++ Estudos/java/untitled1/src/application/a_206.java
@@ -6,6 +6,7 @@ import application.entities.ServicoProduto;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class a_206 {
     public static void main(String[] args) {
@@ -20,5 +21,6 @@ public class a_206 {
         ServicoProduto sp = new ServicoProduto();
         double soma = sp.filteredSum(produtos, p -> p.getNome().charAt(0) == 'T');
         System.out.println("Soma: " + String.format("%.2f", soma));
+
     }
 }
