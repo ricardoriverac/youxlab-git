d9a4b0ba983915cbf4d4457426a370de351e1d2e
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-01-23 13:10:10 -0300

    Aula 207 - Pipelines

--- Estudos/java/untitled1/src/application/a_208.java
+++ Estudos/java/untitled1/src/application/a_208.java
@@ -1,12 +1,12 @@
 package application;
 
-
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class a_207 {
+public class a_208 {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
         Stream<Integer> st1 = list.stream().map(x -> x * 10);
@@ -18,8 +18,5 @@ public class a_207 {
                 .map(x -> x * 10)
                 .collect(Collectors.toList());
         System.out.println(Arrays.toString(newList.toArray()));
-
-
-
     }
 }
