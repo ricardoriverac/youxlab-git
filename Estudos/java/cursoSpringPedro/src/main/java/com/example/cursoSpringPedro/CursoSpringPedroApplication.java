13cdd2e3c00de8fb5472972525e8236bb02b1ff3
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-01-23 16:41:37 -0300

    Projeto criado

---
+++ Estudos/java/cursoSpringPedro/src/main/java/com/example/cursoSpringPedro/CursoSpringPedroApplication.java
@@ -0,0 +1,13 @@
+package com.example.cursoSpringPedro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringPedroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringPedroApplication.class, args);
+	}
+
+}
---
+++ Estudos/java/cursoSpringPedro/src/test/java/com/example/cursoSpringPedro/CursoSpringPedroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cursoSpringPedro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringPedroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
