e75e8f424fa6381349927213c4ab40ea7e14ad27
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-01-23 15:57:36 -0300

    Aula 269 - JPA/HIBERNATE - PARTE 3

--- Estudos/java/untitled1/src/application/a_267.java
+++ Estudos/java/untitled1/src/application/a_267.java
@@ -4,11 +4,24 @@
 //
 //public class a_267 {
 //    public static void main(String[] args) {
-//        Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-//        Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquim@gmail.com");
-//        Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
-//        System.out.println(p1);
-//        System.out.println(p2);
-//        System.out.println(p3);
+//        Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+//        Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+//        Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+
+//          EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+//          EntityManager em = emf.createEntityManager();
+//
+//            Pessoa p = em.find(Pessoal.class, 2);
+//              em.getTransaction().begin();
+//              em.remove(p);
+//              em.getTransaction().commit();
+//          em.persist(p1);
+//          em.persist(p2);
+//          em.persist(p3);
+//          em.getTransaction().commit();
+//          System.out.println("Pronto");
+            em.close();
+            emf.close();
+//
 //    }
 //}
