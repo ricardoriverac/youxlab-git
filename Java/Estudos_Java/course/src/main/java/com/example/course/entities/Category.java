edb1dbc90251e88bca311880307afe3551904bb3
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-01-26 16:50:33 -0300

    Many-to-many association with JoinTable

--- Java/Estudos_Java/course/src/main/java/com/example/course/entities/Category.java
+++ Java/Estudos_Java/course/src/main/java/com/example/course/entities/Category.java
@@ -1,5 +1,7 @@
 package com.example.course.entities;
 
+import com.fasterxml.jackson.annotation.JacksonInject;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -17,7 +19,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category(){
