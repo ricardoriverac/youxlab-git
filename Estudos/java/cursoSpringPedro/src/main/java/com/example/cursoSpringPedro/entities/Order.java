178036666c13a632d995936bab299ca5adcfba8f
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-01-27 13:24:53 -0300

    Aula 287 - Entidade Payment, associação um para um

--- Estudos/java/cursoSpringPedro/src/main/java/com/example/cursoSpringPedro/entities/Order.java
+++ Estudos/java/cursoSpringPedro/src/main/java/com/example/cursoSpringPedro/entities/Order.java
@@ -33,6 +33,8 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order() {
 
@@ -71,6 +73,14 @@ public class Order implements Serializable {
 
     public void setClient(Users client) {
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Set<OrderItem> getItems() {
