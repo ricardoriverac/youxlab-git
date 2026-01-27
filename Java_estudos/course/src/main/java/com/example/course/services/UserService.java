70f69580832a3232d15df9b48bbb6d0a76a5bf90
agathae <agatha.silva-youxlab@institutoyoux.org.br>
2026-01-27 13:50:05 -0300

    User insert

--- Java_estudos/course/src/main/java/com/example/course/services/UserService.java
+++ Java_estudos/course/src/main/java/com/example/course/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
        Optional<User> obj = repository.findById(id);
        return obj.get();
     }
+
+    public User inserrt(User obj){
+        return repository.save(obj);
+    }
 }
