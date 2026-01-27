961b4810c87967adad549a92c02c5b2554a3560e
Thawane <thawane.correa@institutoyoux.org.br>
2026-01-27 13:42:48 -0300

    Exercicio1 aula44

--- Java/CursoJava/src/Secao5/Aula44/aula44_exercicio1.java
+++ Java/CursoJava/src/Secao5/Aula44/aula44_exercicio1.java
@@ -4,7 +4,17 @@ import java.util.Scanner;
 
 public class aula44_exercicio1 {
     public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
 
+     int n;
+     System.out.println("Digite um número: ");
+     n = sc.nextInt();
+     if (n < 0) {
+         System.out.println("NEGATIVO");
+     }
+     else {
+         System.out.println("NÃO NEGATIVO");
+     }
 
     }
 }
