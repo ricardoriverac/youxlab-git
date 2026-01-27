b4fc24666f72a585310ded038706c5032c7aa669
agathae <agatha.silva-youxlab@institutoyoux.org.br>
2026-01-27 13:28:28 -0300

    Payment, one-to-one association

--- Java_estudos/course/src/main/java/com/example/course/entities/Order.java
+++ Java_estudos/course/src/main/java/com/example/course/entities/Order.java
@@ -34,6 +34,11 @@ public class Order  implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+
+
+    @OneToOne(mappedBy = "order" , cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
     }
 
@@ -52,9 +57,19 @@ public class Order  implements Serializable {
         this.id = id;
     }
 
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
     public Instant getMoment() {
         return moment;
     }
+
+
 
     public void setMoment(Instant moment) {
         this.moment = moment;
