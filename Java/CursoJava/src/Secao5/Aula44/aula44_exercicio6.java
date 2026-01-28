ea40f1c6f3d91671c02ec9bdddc73d327a163762
Thawane <thawane.correa@institutoyoux.org.br>
2026-01-28 12:39:12 -0300

    Exercicio6 aula44

---
+++ Java/CursoJava/src/Secao5/Aula44/aula44_exercicio6.java
@@ -0,0 +1,26 @@
+package Secao5.Aula44;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class aula44_exercicio6 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double valor;
+        System.out.print("Digite um valor: ");
+        valor = sc.nextDouble();
+        if (valor >= 0 && valor <= 25) {
+            System.out.println("Intervalo 0,25");
+        } else if (valor > 25 && valor <= 50) {
+            System.out.println("Intervalo 25,50");
+        } else if (valor > 50 && valor <= 75) {
+            System.out.println("Intervalo 50,75");
+        } else if (valor > 75 && valor <= 100) {
+            System.out.println("Intervalo 75,100");
+        } else {
+            System.out.println("Fora de intervalo");
+        }
+    }
+}
