23845ddea61404ab9c74f7808dc9b983ae38911e
Thawane <thawane.correa@institutoyoux.org.br>
2026-01-28 16:00:48 -0300

    Aula47 ExpressaoCondicional

---
+++ Java/CursoJava/src/Secao5/Aula47/Aula47_ExpressaoCondicional.java
@@ -0,0 +1,14 @@
+package Secao5.Aula47;
+
+import java.util.Scanner;
+
+public class Aula47_ExpressaoCondicional {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double preco = 34.5;
+        double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+        System.out.println(desconto);
+        sc.close();
+    }
+}
