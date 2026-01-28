5bee87d7f87e0cd201cc65466837bda08bc51f1c
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-01-28 15:31:28 -0300

    Inserindo produtos no banco de dados - aula 14

---
+++ Java/Estudos_Java/produtosapi/src/main/java/com/example/produtosapi/repository/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package com.example.produtosapi.repository;
+
+import com.example.produtosapi.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+}
