e0aa09c1b85c9682330cfb4e732632f21654fab0
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-01-28 16:23:43 -0300

    Aula 18 - Realizando busca com parametros

--- Estudos/java/cursoSpringPedro(Ciclo 3)/src/main/java/com/example/cursoPedro/controller/ProdutoController.java
+++ Estudos/java/cursoSpringPedro(Ciclo 3)/src/main/java/com/example/cursoPedro/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import com.example.cursoPedro.model.Produto;
 import com.example.cursoPedro.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -37,8 +38,12 @@ public class ProdutoController {
     @PutMapping("{id}")
     public void atualizar(@PathVariable("id") String id, @RequestBody Produto produto){
         produto.setId(id);
-        produtoRepository.save(produto);c
-
+        produtoRepository.save(produto);
+    }
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome){
+        return produtoRepository.findByNome(nome);
     }
 
 }
+
--- Estudos/java/cursoSpringPedro(Ciclo 3)/src/main/java/com/example/cursoPedro/repository/ProdutoRepository.java
+++ Estudos/java/cursoSpringPedro(Ciclo 3)/src/main/java/com/example/cursoPedro/repository/ProdutoRepository.java
@@ -3,6 +3,8 @@ package com.example.cursoPedro.repository;
 import com.example.cursoPedro.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProdutoRepository extends JpaRepository <Produto, String>{
+import java.util.List;
 
+public interface ProdutoRepository extends JpaRepository <Produto, String>{
+    List<Produto> findByNome(String nome);
 }
