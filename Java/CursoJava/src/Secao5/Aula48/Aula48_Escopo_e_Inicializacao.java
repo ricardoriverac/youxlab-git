1ab8425891f03c83109cb0475d4f2b5a9d8fc6d5
Thawane <thawane.correa@institutoyoux.org.br>
2026-01-28 16:28:33 -0300

    Aula48 Escopo e Inicializacao

---
+++ Java/CursoJava/src/Secao5/Aula48/Aula48_Escopo_e_Inicializacao.java
@@ -0,0 +1,17 @@
+package Secao5.Aula48;
+
+public class Aula48_Escopo_e_Inicializacao {
+    public static void main(String[] args) {
+
+        double price = 400.00;
+        double discount;
+
+        if (price < 200.00) {
+            discount = price * 0.1;
+        }
+        else {
+            discount = 0;
+        }
+        System.out.println(discount);
+    }
+}
