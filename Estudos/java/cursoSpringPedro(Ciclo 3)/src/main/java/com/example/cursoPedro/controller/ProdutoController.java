d1e7caccded1d655a6d5460f0f2ecb4ac48e515c
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-01-28 14:13:16 -0300

    Aula 16 - Deletando produtos

--- Estudos/java/cursoSpringPedro(Ciclo 3)/src/main/java/com/example/cursoPedro/controller/ProdutoController.java
+++ Estudos/java/cursoSpringPedro(Ciclo 3)/src/main/java/com/example/cursoPedro/controller/ProdutoController.java
@@ -30,4 +30,9 @@ public class ProdutoController {
          return produtoRepository.findById(id).orElse(null);
     }
 
+    @DeleteMapping("{id}")
+    public void deletar(@PathVariable("id") String id){
+        produtoRepository.deleteById(id);
+    }
+
 }
