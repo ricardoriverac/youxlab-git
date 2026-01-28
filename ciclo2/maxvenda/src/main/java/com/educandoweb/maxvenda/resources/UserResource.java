70ec204ddec53a30b552769b27cb55da997f9290
daniella <daniella.flor-youxlab@institutoyoux.org.br>
2026-01-28 12:37:45 -0300

    User update

--- ciclo2/maxvenda/src/main/java/com/educandoweb/maxvenda/resources/UserResource.java
+++ ciclo2/maxvenda/src/main/java/com/educandoweb/maxvenda/resources/UserResource.java
@@ -43,6 +43,12 @@ import java.util.List;
                 return ResponseEntity.noContent().build();
             }
 
+            @PutMapping(value = "/{id}")
+            public ResponseEntity<User> update (@PathVariable Long id, @RequestBody User obj) {
+                obj = service.update(id, obj);
+                return ResponseEntity.ok().body(obj);
+            }
+
 
     }
 
