3517dffff1a6ea0249f6a2e31b56bd304d307c2f
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-01-29 15:25:08 -0300

    Aula 36 - Inicialização Lazy dos Beans

--- Estudos/java/arquiteturaspring/src/main/java/com/example/arquiteturaspring/ArquiteturaspringApplication.java
+++ Estudos/java/arquiteturaspring/src/main/java/com/example/arquiteturaspring/ArquiteturaspringApplication.java
@@ -1,13 +1,24 @@
 package com.example.arquiteturaspring;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ArquiteturaspringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ArquiteturaspringApplication.class, args);
+	public static void main(String[] args){
+			//SpringApplication.run(ArquiteturaspringApplication.class, args);
+
+		SpringApplicationBuilder builder =
+				new SpringApplicationBuilder(ArquiteturaspringApplication.class);
+
+		builder.bannerMode(Banner.Mode.OFF);
+		builder.profiles("producao", "homologacao");
+		//builder.lazyInitialization(true);
+
+		builder.run(args);
 	}
 
 }
--- Estudos/java/arquiteturaspring/src/main/java/com/example/arquiteturaspring/BeanGerenciado.java
+++ Estudos/java/arquiteturaspring/src/main/java/com/example/arquiteturaspring/BeanGerenciado.java
@@ -4,9 +4,11 @@ import com.example.arquiteturaspring.todos.TodoEntity;
 import com.example.arquiteturaspring.todos.TodoValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+//@Lazy
 @Component
 @Scope(BeanDefinition.SCOPE_SINGLETON)
 public class BeanGerenciado {
