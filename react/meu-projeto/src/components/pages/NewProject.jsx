89a5fd15b0506fdc23dd94054cc52e82a7a806ff
Lucas Paim <lucas.morais@institutoyoux.org.br>
2026-01-29 15:59:59 -0300

    Arrumando

--- react/meu-projeto/src/components/pages/NewProject.jsx
+++ react/meu-projeto/src/components/pages/NewProject.jsx
@@ -4,36 +4,36 @@ import ProjectForm from "../project/ProjectForm";
 import styles from "./NewProject.module.css";
 
 function NewProject() {
-  //   const history = useHistory();
-  const navigatorTo = useNavigate();
+    //   const history = useHistory();
+    const navigatorTo = useNavigate();
 
-  function createPost(project) {
-    //initialize cost and services
-    project.cost = 0;
-    project.services = [];
+    function createPost(project) {
+        //initialize cost and services
+        project.cost = 0;
+        project.services = [];
 
-    fetch("http:/localhost:5000/projects", {
-      method: "POST",
-      headers: {
-        "Content-type": "aplication/json",
-      },
-      body: JSON.stringify(project),
-    })
-      .then(
-        ((resp) => resp.json()).then((data) => {
-          console.log(data);
-        }),
-      )
-      .catch((err) => console.log(err));
-  }
+        fetch("http:/localhost:5000/projects", {
+            method: "POST",
+            headers: {
+                "Content-type": "aplication/json",
+            },
+            body: JSON.stringify(project),
+        }) //Cabeça de melão
+            .then(
+                ((resp) => resp.json()).then((data) => {
+                    console.log(data);
+                }),
+            )
+            .catch((err) => console.log(err));
+    }
 
-  return (
-    <div className={styles.newproject_container}>
-      <h1>Criar Projeto</h1>
-      <p>Crie seu projeto para depois adicionar os serviços</p>
-      <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
-    </div>
-  );
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
+        </div>
+    );
 }
 
 export default NewProject;
