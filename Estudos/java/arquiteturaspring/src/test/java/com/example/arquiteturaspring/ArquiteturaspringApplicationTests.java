a83249e7661f43068b55f097503e176f9437541e
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-01-28 22:23:16 -0300

    Aula 30 - Iniciando pela entidade

---
+++ Estudos/java/arquiteturaspring/src/test/java/com/example/arquiteturaspring/ArquiteturaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arquiteturaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
