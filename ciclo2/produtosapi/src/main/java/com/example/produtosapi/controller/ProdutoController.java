22bf19aea4a3c2092ef58d4ae67cfa757959b735
daniella <daniella.flor-youxlab@institutoyoux.org.br>
2026-01-29 13:07:09 -0300

    resolvendo problemas de git

--- ciclo2/produtosapi/src/main/java/com/example/produtosapi/controller/ProdutoController.java
+++ ciclo2/produtosapi/src/main/java/com/example/produtosapi/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import com.example.produtosapi.model.Produto;
 import com.example.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -32,4 +33,19 @@ public class ProdutoController {
     public Produto obterPorId(@PathVariable("id") String id) {
         return produtoRepository.findById(id).orElse(null);
     }
+
+    @DeleteMapping("{id}")
+    public void deletar (@PathVariable("id") String id) {
+        produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable("id") String id,
+                          @RequestBody Produto produto) {
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome)){}
 }
