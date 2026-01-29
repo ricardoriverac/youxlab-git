16d9c5e53760683edae4275ee728440871c0c39c
Thawane <thawane.correa@institutoyoux.org.br>
2026-01-29 09:33:52 -0300

    Aula52

---
+++ Java/CursoJava/src/Secao6/Aula52/aula52.java
@@ -0,0 +1,19 @@
+package Secao6.Aula52;
+
+import java.util.Scanner;
+
+public class aula52 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x  = sc.nextInt();
+        int soma = 0;
+        while (x != 0) {
+            soma += x;
+            x = sc.nextInt();
+        }
+        System.out.println(soma);
+
+        sc.close();
+    }
+}
