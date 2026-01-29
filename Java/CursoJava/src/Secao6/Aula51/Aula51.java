5e9ef5b820285e203a2cc962286bd4034b9ee5e4
Thawane <thawane.correa@institutoyoux.org.br>
2026-01-29 08:39:06 -0300

    Aula51

---
+++ Java/CursoJava/src/Secao6/Aula51/Aula51.java
@@ -0,0 +1,23 @@
+package Secao6.Aula51;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aula51 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double largura = sc.nextDouble();
+        double comprimento = sc.nextDouble();
+        double metroQuadrado = sc.nextDouble();
+
+        double area = largura * comprimento;
+        double preco = area * metroQuadrado;
+
+        System.out.printf("Area: %.2f%n", area);
+        System.out.printf("Preco: %.2f%n", preco);
+
+        sc.close();
+    }
+}
