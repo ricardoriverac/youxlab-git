6054319ef87d523c611b53ed503df49bcf1bb56b
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-01-29 16:04:42 -0300

    Aula 38 - Lendo propriedades com @Value

--- Estudos/java/arquiteturaspring/src/main/java/com/example/arquiteturaspring/ArquiteturaspringApplication.java
+++ Estudos/java/arquiteturaspring/src/main/java/com/example/arquiteturaspring/ArquiteturaspringApplication.java
@@ -1,9 +1,12 @@
 package com.example.arquiteturaspring;
 
+import com.example.arquiteturaspring.todos.ExemploValue;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class ArquiteturaspringApplication {
@@ -19,6 +22,15 @@ public class ArquiteturaspringApplication {
 		//builder.lazyInitialization(true);
 
 		builder.run(args);
+
+		ConfigurableApplicationContext applicationContext = builder.context();
+
+		ConfigurableEnvironment environment = applicationContext.getEnvironment();
+		String applicationName = environment.getProperty("spring.application.name");
+		System.out.println("Nome da aplicacao: " + applicationName);
+
+		ExemploValue value = applicationContext.getBean(ExemploValue.class);
+		value.imprimirVariavel();
 	}
 
 }
