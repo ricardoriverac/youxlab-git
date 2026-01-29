3d5192bfc21b0af12de4b1f903dafc7f8a8b6bb4
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-01-29 13:35:38 -0300

    Aula 33 - Trabalhando com components

--- Estudos/java/arquiteturaspring/src/main/java/com/example/arquiteturaspring/todos/TodoController.java
+++ Estudos/java/arquiteturaspring/src/main/java/com/example/arquiteturaspring/todos/TodoController.java
@@ -1,6 +1,9 @@
 package com.example.arquiteturaspring.todos;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("todos")
@@ -13,8 +16,15 @@ public class TodoController {
 
     @PostMapping
     public TodoEntity salvar(@RequestBody TodoEntity todo){
-        this.service.salvar(todo);
-        return todo;
+        try {
+
+
+            this.service.salvar(todo);
+            return todo;
+        }catch (IllegalArgumentException e){
+            var mensagemErro = e.getMessage();
+            throw new ResponseStatusException(HttpStatus.CONFLICT, mensagemErro);
+        }
     }
 
     @PutMapping("{id}")
