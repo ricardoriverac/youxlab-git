8ee487f3ce62e4b4ad3026ee66609a28d163d882
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-01-29 23:33:59 -0300

    Aula 54 - Mapeamento da entidade Livro e utilização do Lombok

---
+++ Estudos/java/libraryapi/src/main/java/com/example/libraryapi/model/Livro.java
@@ -0,0 +1,42 @@
+package com.example.libraryapi.model;
+
+import com.example.libraryapi.model.enums.GeneroLivro;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "livro")
+@Data
+public class Livro {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    UUID id;
+
+    @Column(name = "isbn", length = 20, nullable = false)
+    String isbn;
+
+    @Column(name = "titulo", length = 150, nullable = false)
+    String titulo;
+
+    @Column(name = "data_publicacao", nullable = false)
+    LocalDate data_publicacao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "genero", length = 30, nullable = false)
+    GeneroLivro genero;
+
+    @Column(name = "preco", precision = 18, scale = 2)
+    private BigDecimal preco;
+
+    @ManyToOne
+    @JoinColumn(name = "id_autor")
+    private Autor autor;
+
+
+}
---
+++ Estudos/java/libraryapi/src/main/java/com/example/libraryapi/model/enums/GeneroLivro.java
@@ -0,0 +1,10 @@
+package com.example.libraryapi.model.enums;
+
+public enum GeneroLivro {
+    FICCAO,
+    FANTASIA,
+    MISTERIO,
+    ROMANCE,
+    BIOGRAFIA,
+    CIENCIA;
+}
