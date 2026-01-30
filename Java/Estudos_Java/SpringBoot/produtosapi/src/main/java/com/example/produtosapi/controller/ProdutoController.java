41c5320b432225f5212bd5b740f73b93fa3b0530
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-01-30 16:53:42 -0300

    Reorganizando aulas de Spring Boot

---
+++ Java/Estudos_Java/SpringBoot/produtosapi/src/main/java/com/example/produtosapi/controller/ProdutoController.java
@@ -0,0 +1,51 @@
+package com.example.produtosapi.controller;
+
+import com.example.produtosapi.model.Produto;
+import com.example.produtosapi.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto){
+        System.out.println("Produto recebido: " + produto);
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+        produtoRepository.save(produto);
+        return produto;
+    }
+
+    @GetMapping("{id}")
+    public Produto obterPorId(@PathVariable("id") String id){
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletar(@PathVariable("id") String id){
+        produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable("id") String id, @RequestBody Produto p){
+
+        p.setId(id);
+        produtoRepository.save(p);
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome){
+        return produtoRepository.findByNome(nome);
+    }
+}
