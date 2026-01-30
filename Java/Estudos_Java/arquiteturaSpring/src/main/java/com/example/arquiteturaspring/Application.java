7e6ec1df8f782ac3912c90fd9c47c06c9b2bd938
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-01-30 15:44:47 -0300

    Projeto MVC

--- Java/Estudos_Java/arquiteturaSpring/src/main/java/com/example/arquiteturaspring/Application.java
+++ Java/Estudos_Java/arquiteturaSpring/src/main/java/com/example/arquiteturaspring/Application.java
@@ -1,13 +1,39 @@
 package com.example.arquiteturaspring;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		//SpringApplication.run(Application.class, args);
+
+		SpringApplicationBuilder builder =
+				new SpringApplicationBuilder(Application.class);
+
+		builder.bannerMode(Banner.Mode.OFF);
+		builder.properties("producao", "homologacao");
+//		builder.lazyInitialization(true);
+
+		builder.run(args);
+
+		ConfigurableApplicationContext applicationContext = builder.context();
+		ConfigurableEnvironment environment = applicationContext.getEnvironment();
+
+		ExemploValue value = applicationContext.getBean(ExemploValue.class);
+		value.imprimirVariavel();
+
+		AppProperties properties = applicationContext.getBean(AppProperties.class);
+		System.out.println(properties.getValor1());
 	}
 
 }
--- Java/Estudos_Java/arquiteturaSpring/src/main/java/com/example/arquiteturaspring/BeanGerenciado.java
+++ Java/Estudos_Java/arquiteturaSpring/src/main/java/com/example/arquiteturaspring/BeanGerenciado.java
@@ -4,11 +4,13 @@ import com.example.arquiteturaspring.todos.TodoEntity;
 import com.example.arquiteturaspring.todos.TodoValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
 
 // singleton
+//@Lazy(false)
 @Component
 @Scope(BeanDefinition.SCOPE_SINGLETON)
 //@Scope(WebApplicationContext.SCOPE_APPLICATION);
