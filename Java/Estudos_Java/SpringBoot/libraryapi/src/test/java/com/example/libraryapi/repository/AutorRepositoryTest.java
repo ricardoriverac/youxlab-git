7bbed096298db2a5937373d1afa11f9da838a335
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-02-02 17:53:29 -0300

    Operações básicas para entidades simples - aula 59

--- Java/Estudos_Java/SpringBoot/libraryapi/src/test/java/com/example/libraryapi/repository/AutorRepositoryTest.java
+++ Java/Estudos_Java/SpringBoot/libraryapi/src/test/java/com/example/libraryapi/repository/AutorRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -29,7 +30,7 @@ public class AutorRepositoryTest {
 
     @Test
     public void atualizarTest(){
-        var id = UUID.fromString("55c90c94-15cb-4e4a-a644-acd0f0294c0d");
+        var id = UUID.fromString("0b5803fe-f44e-4bd7-b37a-fa0d06a7d43c");
 
         Optional<Autor> possivelAutor = repository.findById(id);
         if (possivelAutor.isPresent()) {
@@ -43,6 +44,30 @@ public class AutorRepositoryTest {
             repository.save(autorEncontrado);
 
         }
+    }
+
+    @Test
+    public void listarTest(){
+        List<Autor> lista = repository.findAll();
+        lista.forEach(System.out::println);
+    }
+
+    @Test
+    public void countTest(){
+        System.out.println("Contagem de autores: " + repository.count());
+    }
+
+    @Test
+    public void deletePorIdTest(){
+        var id = UUID.fromString("55c90c94-15cb-4e4a-a644-acd0f0294c0d");
+        repository.deleteById(id);
+    }
+
+    @Test
+    public void deleteTest(){
+        var id = UUID.fromString("8c60d5ec-e57b-4d0d-82fa-945a599ad337");
+        var maria = repository.findById(id).get();
+        repository.delete(maria);
     }
 
 }
