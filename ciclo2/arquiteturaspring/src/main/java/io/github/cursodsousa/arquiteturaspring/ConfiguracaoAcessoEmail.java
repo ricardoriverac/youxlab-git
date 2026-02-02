23676e93a17dd1a3052565a3079ed975efe87702
daniella <daniella.flor-youxlab@institutoyoux.org.br>
2026-02-02 17:49:38 -0300

    reorganização de pastas

--- ciclo2/arquiteturaspring/src/main/java/io/github/cursodsousa/arquiteturaspring/ConfiguracaoAcessoEmail.java
+++
@@ -1,4 +0,0 @@
-package io.github.cursodsousa.arquiteturaspring;
-
-public class ConfiguracaoAcessoEmail {
-}
---
+++ ciclo2/springboot/arquiteturaspring/src/main/java/io/github/cursodsousa/arquiteturaspring/ConfiguracaoAcessoEmail.java
@@ -0,0 +1,17 @@
+package io.github.cursodsousa.arquiteturaspring;
+
+import io.github.cursodsousa.arquiteturaspring.Todos.MailSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfiguracaoAcessoEmail {
+
+    @Autowired
+    private AppProperties properties;
+
+    //@Bean
+    public MailSender mailSender(){
+        return null;
+    }
+}
