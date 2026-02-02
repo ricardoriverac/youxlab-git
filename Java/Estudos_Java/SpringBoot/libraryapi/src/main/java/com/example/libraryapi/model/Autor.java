cd5bd8d95ca22ab20e3c11846f2bb25716d3c81d
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-02-02 17:03:04 -0300

    Executando uma operação de persistencia com repositorio - aula 57

--- Java/Estudos_Java/SpringBoot/libraryapi/src/main/java/com/example/libraryapi/model/Autor.java
+++ Java/Estudos_Java/SpringBoot/libraryapi/src/main/java/com/example/libraryapi/model/Autor.java
@@ -3,6 +3,7 @@ package com.example.libraryapi.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Table(name = "autor", schema = "public")
 @Getter
 @Setter
+@ToString
 public class Autor {
 
     @Id
---
+++ Java/Estudos_Java/SpringBoot/libraryapi/src/main/java/com/example/libraryapi/repository/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.example.libraryapi.repository;
+
+import com.example.libraryapi.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface LivroRepository extends JpaRepository<Livro, UUID> {
+}
