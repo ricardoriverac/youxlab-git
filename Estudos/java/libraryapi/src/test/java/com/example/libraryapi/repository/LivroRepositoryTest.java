91cccc78185248fa97a2a976e9660fe1a57c96f2
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-02-02 15:05:25 -0300

    Aula 61 - Operação cascade

--- Estudos/java/libraryapi/src/test/java/com/example/libraryapi/repository/LivroRepositoryTest.java
+++ Estudos/java/libraryapi/src/test/java/com/example/libraryapi/repository/LivroRepositoryTest.java
@@ -27,10 +27,51 @@ class LivroRepositoryTest {
         livro.setIsbn("90887-84874");
         livro.setPreco(BigDecimal.valueOf(100));
         livro.setGenero(GeneroLivro.FICCAO);
-        livro.setTitulo("UFO");
+        livro.setTitulo("Outro Livro");
         livro.setData_publicacao(LocalDate.of(1980, 1, 2));
 
-        Autor autor = autorRepository.findById(UUID.fromString("a339b8e0-c5b2-4102-9d73-b60dcc7482e2")).orElse(null);
+         Autor autor = autorRepository.findById(UUID.fromString("a339b8e0-c5b2-4102-9d73-b60dcc7482e2")).orElse(null);
+
+        livro.setAutor(new Autor());
+
+        repository.save(livro);
+    }
+
+    @Test
+    void salvarAutorELivroTest(){
+        Livro livro = new Livro();
+        livro.setIsbn("90887-84874");
+        livro.setPreco(BigDecimal.valueOf(100));
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setTitulo("Terceiro Livro");
+        livro.setData_publicacao(LocalDate.of(1980, 1, 2));
+
+        Autor autor = new Autor();
+        autor.setNome("José");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of(1951, 1, 31));
+
+        autorRepository.save(autor);
+
+        livro.setAutor(autor);
+
+        repository.save(livro);
+
+    }
+
+    @Test
+    void salvarCascadeTest(){
+        Livro livro = new Livro();
+        livro.setIsbn("90887-84874");
+        livro.setPreco(BigDecimal.valueOf(100));
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setTitulo("Outro Livro");
+        livro.setData_publicacao(LocalDate.of(1980, 1, 2));
+
+        Autor autor = new Autor();
+        autor.setNome("João");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of(1951, 1, 31));
 
         livro.setAutor(autor);
 
