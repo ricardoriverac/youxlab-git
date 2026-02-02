bc597f35b0f4a2e810c06fe4a5c48795023a0ea4
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-02-02 16:14:05 -0300

    Mapeamento da entidade Livro e utilização do Lombok

--- Java/Estudos_Java/SpringBoot/libraryapi/src/main/java/com/example/libraryapi/model/Autor.java
+++ Java/Estudos_Java/SpringBoot/libraryapi/src/main/java/com/example/libraryapi/model/Autor.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
@@ -28,4 +29,6 @@ public class Autor {
     @Column(name = "nacionalidade", length = 50, nullable = false)
     private String nacionalidade;
 
+    @OneToMany(mappedBy = "autor")
+    private List<Livro> livros;
 }
