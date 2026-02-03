86bdfe62249624e81664b5cd46ae3cd475b25629
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-02-03 14:12:59 -0300

    AUla 66 - Implementação de pesquisas com query methods

--- Estudos/java/libraryapi/src/main/java/com/example/libraryapi/repository/LivroRepository.java
+++ Estudos/java/libraryapi/src/main/java/com/example/libraryapi/repository/LivroRepository.java
@@ -4,9 +4,19 @@ import com.example.libraryapi.model.Autor;
 import com.example.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
 public interface LivroRepository extends JpaRepository<Livro, UUID> {
     List<Livro> findByAutor(Autor autor);
+
+    List<Livro> findByTitulo(String titulo);
+
+
+    List<Livro> findByIsbn(String titulo);
+
+    List<Livro> findByTituloAndPreco(String titulo, BigDecimal preco);
+
+    List<Livro> findByTituloOrIsbn(String titulo, String isbn);
 }
--- Estudos/java/libraryapi/src/test/java/com/example/libraryapi/repository/LivroRepositoryTest.java
+++ Estudos/java/libraryapi/src/test/java/com/example/libraryapi/repository/LivroRepositoryTest.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @SpringBootTest
@@ -115,5 +116,26 @@ class LivroRepositoryTest {
 
         System.out.println("Autor: ");
         System.out.println(livro.getAutor().getNome());
+    }
+
+    @Test
+    void pesquisarPorTituloTest(){
+        List<Livro> lista = repository.findByTitulo("O roubo da casa assombrada");
+        lista.forEach(System.out::println);
+    }
+
+    @Test
+    void pesquisarPorISBNTest(){
+        List<Livro> lista = repository.findByIsbn("99999-84784");
+        lista.forEach(System.out::println);
+    }
+
+    @Test
+    void pesquisarPorTituloEPrecoTest(){
+       var preco = BigDecimal.valueOf(204.00);
+        String tituloPesquisa = "O roubo da casa assombrada";
+        List<Livro> lista = repository.findByTituloAndPreco(tituloPesquisa, preco);
+        lista.forEach(System.out::println);
+
     }
 }
