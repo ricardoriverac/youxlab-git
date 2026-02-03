51c2a4605d1e4084607218a41d0d2069e06a4d41
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-02-03 13:43:49 -0300

    Operações utilizando cascade - aula 61

--- Java/Estudos_Java/SpringBoot/libraryapi/src/test/java/com/example/libraryapi/repository/LivroRepositoryTest.java
+++ Java/Estudos_Java/SpringBoot/libraryapi/src/test/java/com/example/libraryapi/repository/LivroRepositoryTest.java
@@ -26,12 +26,50 @@ class LivroRepositoryTest {
         livro.setIsbn("90887-84874");
         livro.setPreco(BigDecimal.valueOf(100));
         livro.setGenero(GeneroLivro.FICCAO);
-        livro.setTitulo("UFO");
+        livro.setTitulo("Outro Livro");
         livro.setDataPublicacao(LocalDate.of(1980, 1, 2));
 
         Autor autor = autorRepository
                 .findById(UUID.fromString("0b5803fe-f44e-4bd7-b37a-fa0d06a7d43c"))
                 .orElse(null);
+
+        livro.setAutor(autor);
+        repository.save(livro);
+    }
+
+    @Test
+    void salvarAutorELivroTest(){
+        Livro livro = new Livro();
+        livro.setIsbn("90887-84874");
+        livro.setPreco(BigDecimal.valueOf(100));
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setTitulo("Terceiro Livro");
+        livro.setDataPublicacao(LocalDate.of(1980, 1, 2));
+
+        Autor autor = new Autor();
+        autor.setNome("José");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of(1951, 1, 31));
+
+        autorRepository.save(autor);
+
+        livro.setAutor(autor);
+        repository.save(livro);
+    }
+
+    @Test
+    void salvarCascadeTest(){
+        Livro livro = new Livro();
+        livro.setIsbn("90887-84874");
+        livro.setPreco(BigDecimal.valueOf(100));
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setTitulo("Outro Livro");
+        livro.setDataPublicacao(LocalDate.of(1980, 1, 2));
+
+        Autor autor = new Autor();
+        autor.setNome("João");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of(1951, 1, 31));
 
         livro.setAutor(autor);
         repository.save(livro);
