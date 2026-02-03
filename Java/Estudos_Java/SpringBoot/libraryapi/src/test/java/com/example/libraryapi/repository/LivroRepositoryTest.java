e51364aa82d0aa1402870195d0ee31b4c0512675
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-02-03 13:21:26 -0300

    Trabalhnado com relacionamentos - Entidade Livro

---
+++ Java/Estudos_Java/SpringBoot/libraryapi/src/test/java/com/example/libraryapi/repository/LivroRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.example.libraryapi.repository;
+
+import com.example.libraryapi.model.Autor;
+import com.example.libraryapi.model.GeneroLivro;
+import com.example.libraryapi.model.Livro;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@SpringBootTest
+class LivroRepositoryTest {
+
+    @Autowired
+    LivroRepository repository;
+
+    @Autowired
+    AutorRepository autorRepository;
+
+    @Test
+    void salvarTest(){
+        Livro livro = new Livro();
+        livro.setIsbn("90887-84874");
+        livro.setPreco(BigDecimal.valueOf(100));
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setTitulo("UFO");
+        livro.setDataPublicacao(LocalDate.of(1980, 1, 2));
+
+        Autor autor = autorRepository
+                .findById(UUID.fromString("0b5803fe-f44e-4bd7-b37a-fa0d06a7d43c"))
+                .orElse(null);
+
+        livro.setAutor(autor);
+        repository.save(livro);
+    }
+}
