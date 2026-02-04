9afc3fd9a45f220e31a8405e2ae057e5f69fab89
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-02-04 15:09:53 -0300

    Aula 72 - Entendendo como funcionam as transações do Spring

--- Estudos/java/libraryapi/src/main/java/com/example/libraryapi/service/TransacaoService.java
+++ Estudos/java/libraryapi/src/main/java/com/example/libraryapi/service/TransacaoService.java
@@ -1,4 +1,48 @@
 package com.example.libraryapi.service;
 
+import com.example.libraryapi.model.Autor;
+import com.example.libraryapi.model.Livro;
+import com.example.libraryapi.model.enums.GeneroLivro;
+import com.example.libraryapi.repository.AutorRepository;
+import com.example.libraryapi.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Service
 public class TransacaoService {
+    @Autowired
+    private AutorRepository autorRepository;
+
+    @Autowired
+    private LivroRepository livroRepository;
+
+    @Transactional
+    public void executar(){
+
+        Autor autor = new Autor();
+        autor.setNome("José");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of(1951, 1, 31));
+
+        autorRepository.save(autor);
+
+
+
+        autorRepository.save(autor);
+
+
+        Livro livro = new Livro();
+        livro.setIsbn("90887-8474");
+        livro.setPreco(BigDecimal.valueOf(100));
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setTitulo(GeneroLivro.FICCAO);
+        livro.setDataPublicacao(LocalDate.of(1980, 1, 2));
+
+        livro.setAutor(autor);
+    }
+
 }
