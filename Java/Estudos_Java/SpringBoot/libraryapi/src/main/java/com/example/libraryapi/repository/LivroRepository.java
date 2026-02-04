f22541fae8879734099ec24f4834f5af30cad779
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-02-04 15:26:46 -0300

    Variações de métodos de busca declarativos - aula 67

--- Java/Estudos_Java/SpringBoot/libraryapi/src/main/java/com/example/libraryapi/repository/LivroRepository.java
+++ Java/Estudos_Java/SpringBoot/libraryapi/src/main/java/com/example/libraryapi/repository/LivroRepository.java
@@ -5,6 +5,7 @@ import com.example.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -24,5 +25,8 @@ public interface LivroRepository extends JpaRepository<Livro, UUID> {
     List<Livro> findByTituloAndPreco(String titulo, BigDecimal preco);
 
     // select * from livro where titulo = ? or isbn = ?
-    List<Livro> findByTituloOrIsbn(String titulo, String isbn);
+    List<Livro> findByTituloOrIsbnOrderByTitulo(String titulo, String isbn);
+
+    // select * from livro where data_publicacao between ? and ?
+    List<Livro> findByDataPublicacaoBetween(LocalDate inicio, LocalDate fim);
 }
