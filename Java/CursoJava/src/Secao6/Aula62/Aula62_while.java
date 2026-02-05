3485094c21ce239e5fc5cd71d0757e4d4f9460b7
Thawane <thawane.correa@institutoyoux.org.br>
2026-02-05 19:07:43 -0300

    Aula62

---
+++ Java/CursoJava/src/Secao6/Aula62/Aula62_while.java
@@ -0,0 +1,24 @@
+package Secao6.Aula62;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aula62_while {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        char resp = 's';
+
+        while (resp != 'n') {
+            System.out.print("Digite a temperatura em Celsius: ");
+            double c = sc.nextDouble();
+            double f = 9.0 * c / 5.0 + 32.0;
+            System.out.printf("Equivalente em Fahrenheit: %.1f\n", f);
+            System.out.print("Você deseja repitir (s/n)? ");
+            resp = sc.next().charAt(0);
+
+        }
+        sc.close();
+    }
+}
