3a117f588c3063bf95bb59b33e653aac8e1a1e7a
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-02-06 16:27:37 -0300

    Aula 80 - Mapeamento de Requisição e padrão DTO

--- Estudos/java/libraryapi/src/main/java/com/example/libraryapi/controller/AutorController.java
+++ Estudos/java/libraryapi/src/main/java/com/example/libraryapi/controller/AutorController.java
@@ -1,9 +1,22 @@
 package com.example.libraryapi.controller;
 
+import com.example.libraryapi.model.Autor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/autores")
 public class AutorController {
+
+
+    @PostMapping
+    public ResponseEntity salvar(@RequestBody AutorDTO autor){
+
+        return new ResponseEntity("Autor salvo com sucesso! " + autor, HttpStatus.CREATED);
+    }
 }
---
+++ Estudos/java/libraryapi/src/main/java/com/example/libraryapi/controller/AutorDTO.java
@@ -0,0 +1,7 @@
+package com.example.libraryapi.controller;
+
+
+import java.time.LocalDate;
+
+public record AutorDTO(String nome, LocalDate dataNascimento, String Nacionalidade) {
+}
