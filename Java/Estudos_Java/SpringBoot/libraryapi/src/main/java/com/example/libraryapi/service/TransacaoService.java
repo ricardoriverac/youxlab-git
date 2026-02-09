4f609581d04ab3eab821f2a8cfb040ad0323e857
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-02-09 14:46:21 -0300

    Entendendo os conceitos de transações e estados na prática - aula 71

---
+++ Java/Estudos_Java/SpringBoot/libraryapi/src/main/java/com/example/libraryapi/service/TransacaoService.java
@@ -0,0 +1,50 @@
+package com.example.libraryapi.service;
+
+import com.example.libraryapi.model.Autor;
+import com.example.libraryapi.model.GeneroLivro;
+import com.example.libraryapi.model.Livro;
+import com.example.libraryapi.repository.AutorRepository;
+import com.example.libraryapi.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Service
+public class TransacaoService {
+
+    @Autowired
+    private AutorRepository autorRepository;
+
+    @Autowired
+    private LivroRepository livroRepository;
+
+    @Transactional
+    public void executar(){
+        // salva o autor
+        Autor autor = new Autor();
+        autor.setNome("Teste Francisco");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of(1951, 1, 31));
+
+        autorRepository.save(autor);
+
+
+        // salva o livro
+        Livro livro = new Livro();
+        livro.setIsbn("90887-84874");
+        livro.setPreco(BigDecimal.valueOf(100));
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setTitulo("Teste Livro do Francisco");
+        livro.setDataPublicacao(LocalDate.of(1980, 1, 2));
+
+        livro.setAutor(autor);
+        livroRepository.save(livro);
+
+        if (autor.getNome().equals("Teste Francisco")){
+            throw new RuntimeException("Rollback!");
+        }
+    }
+}
