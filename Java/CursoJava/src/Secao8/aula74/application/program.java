52f82a95f5930e2d1dbe5ecb7cfa0a7f958677c1
Thawane <thawane.correa@institutoyoux.org.br>
2026-02-09 16:49:34 -0300

    Aula74 product

---
+++ Java/CursoJava/src/Secao8/aula74/application/program.java
@@ -0,0 +1,16 @@
+package Secao8.aula74.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+
+
+        sc.close();
+    }
+}
---
+++ Java/CursoJava/src/Secao8/aula74/entities/product.java
@@ -0,0 +1,19 @@
+package Secao8.aula74.entities;
+
+public class product {
+
+    public String name;
+    public double price;
+    public int quantity;
+
+    public double totalValueInStock() {
+        return price * quantity;
+    }
+    public void addProducts(int quantity) {
+        this.quantity += quantity;
+    }
+    public void removeProducts(int quantity) {
+        this.quantity -= quantity;
+    }
+}
+
