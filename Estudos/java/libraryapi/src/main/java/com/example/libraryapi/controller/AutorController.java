f386e1b5dbc549419eea51b07b1c75c0d78acf8b
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-02-09 13:56:31 -0300

    Aula 81 - Finalizando o contrato de Salvar um Autor

--- Estudos/java/libraryapi/src/main/java/com/example/libraryapi/controller/AutorController.java
+++ Estudos/java/libraryapi/src/main/java/com/example/libraryapi/controller/AutorController.java
@@ -1,6 +1,7 @@
 package com.example.libraryapi.controller;
 
 import com.example.libraryapi.model.Autor;
+import com.example.libraryapi.service.AutorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -8,15 +9,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/autores")
 public class AutorController {
 
+    public final AutorService service;
+
+    public AutorController(AutorService service){
+        this.service = service;
+    }
 
     @PostMapping
-    public ResponseEntity salvar(@RequestBody AutorDTO autor){
+    public ResponseEntity<Void> salvar(@RequestBody AutorDTO autor){
+        Autor autorEntidade = autor.mapearParaAutor();
+        service.salvar(autorEntidade);
 
-        return new ResponseEntity("Autor salvo com sucesso! " + autor, HttpStatus.CREATED);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(autorEntidade.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 }
--- Estudos/java/libraryapi/src/main/java/com/example/libraryapi/controller/AutorDTO.java
+++ Estudos/java/libraryapi/src/main/java/com/example/libraryapi/controller/AutorDTO.java
@@ -1,7 +1,17 @@
 package com.example.libraryapi.controller;
 
 
+import com.example.libraryapi.model.Autor;
+
 import java.time.LocalDate;
 
 public record AutorDTO(String nome, LocalDate dataNascimento, String Nacionalidade) {
+
+    public Autor mapearParaAutor(){
+        Autor autor = new Autor();
+        autor.setNome(this.nome);
+        autor.setDataNascimento(this.dataNascimento);
+        autor.setNacionalidade(this.Nacionalidade);
+        return autor;
+    }
 }
