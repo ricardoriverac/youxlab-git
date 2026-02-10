2dacc8cf8bf192094f83322df1c0e17a9723ce0a
Thawane <thawane.correa@institutoyoux.org.br>
2026-02-10 13:16:58 -0300

    Aula74 pt1 e pt2

--- Java/CursoJava/src/Secao8/Aula74_pt1/application/program.java
+++ Java/CursoJava/src/Secao8/Aula74_pt1/application/program.java
@@ -1,5 +1,7 @@
 package Secao8.Aula74_pt1.application;
 
+import Secao8.Aula74_pt2.entities.product;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,12 +10,16 @@ public class program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        product Product = new product();
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        Product.name = sc.nextLine();
+        System.out.print("Price: ");
+        Product.price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        Product.quantity = sc.nextInt();
 
-
-
-
-
-
+        System.out.println(Product.name + ", " + Product.price + ", " + Product.quantity);
     sc.close();
     }
 }
