284dc90b7bc086b8b26721c90a33a4d182094b65
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-02-11 15:19:00 -0300

    Aula 95 - Melhorando a pesquisa de autores com Query By Example

--- Estudos/java/libraryapi/src/main/java/com/example/libraryapi/controller/AutorController.java
+++ Estudos/java/libraryapi/src/main/java/com/example/libraryapi/controller/AutorController.java
@@ -80,7 +80,7 @@ public class AutorController {
 
     @GetMapping
     public ResponseEntity<List<AutorDTO>> pesquisar(@RequestParam(value = "nome", required = false)String nome, @RequestParam(value = "nacionalidade", required = false) String nacionalidade){
-    List<Autor> resultado = service.pesquisa(nome, nacionalidade);
+    List<Autor> resultado = service.pesquisaByExample(nome, nacionalidade);
     List<AutorDTO> lista = resultado.stream().map(autor -> new AutorDTO(autor.getId(), autor.getNome(), autor.getDataNascimento(), autor.getNacionalidade())).collect(Collectors.toList());
     return ResponseEntity.ok(lista);
     }
