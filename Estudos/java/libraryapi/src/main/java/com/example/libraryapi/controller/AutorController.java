b1178f2543f6499b21fdebb29ee709cfc1c99a38
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-02-11 15:05:20 -0300

    Aula 94 - Explorando tipos de validação no Spring

--- Estudos/java/libraryapi/src/main/java/com/example/libraryapi/controller/AutorController.java
+++ Estudos/java/libraryapi/src/main/java/com/example/libraryapi/controller/AutorController.java
@@ -86,7 +86,7 @@ public class AutorController {
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<Object> atualizar(@PathVariable("id") String id, @RequestBody AutorDTO dto){
+    public ResponseEntity<Object> atualizar(@PathVariable("id") String id, @RequestBody @Valid AutorDTO dto){
 
         try{
         var idAutor = UUID.fromString(id);
--- Estudos/java/libraryapi/src/main/java/com/example/libraryapi/controller/dto/AutorDTO.java
+++ Estudos/java/libraryapi/src/main/java/com/example/libraryapi/controller/dto/AutorDTO.java
@@ -3,11 +3,13 @@ import jakarta.validation.constraints.NotBlank;
 
 import com.example.libraryapi.model.Autor;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record AutorDTO(UUID id, @NotBlank(message = "campo obrigatório") String nome, @NotNull(message = "campo obrigatório") LocalDate dataNascimento, @NotBlank(message = "campo obrigatório")  String Nacionalidade) {
+public record AutorDTO(UUID id, @NotBlank(message = "campo obrigatório") @Size(min = 2, max = 100, message ="campo fora do tamanho padrao" ) String nome, @NotNull(message = "campo obrigatório")@Past(message = "não pode ser uma data futura") LocalDate dataNascimento, @NotBlank(message = "campo obrigatório") @Size(max = 50, min = 2, message = "campo fora do tamanho padrao") String Nacionalidade) {
 
     public Autor mapearParaAutor(){
         Autor autor = new Autor();
