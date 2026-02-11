474b641461ebc95e60432aeba514030719fc4fdc
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-02-11 20:56:14 -0300

    Aula 98 - Endpoint para cadastro de livro com Bean Validation

--- Estudos/java/libraryapi/src/main/java/com/example/libraryapi/controller/LivroController.java
+++ Estudos/java/libraryapi/src/main/java/com/example/libraryapi/controller/LivroController.java
@@ -1,7 +1,14 @@
 package com.example.libraryapi.controller;
 
+import com.example.libraryapi.controller.dto.CadastroLivroDTO;
+import com.example.libraryapi.controller.dto.ErroResposta;
+import com.example.libraryapi.exceptions.RegistroDuplicadoException;
 import com.example.libraryapi.service.LivroService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class LivroController {
 
     private final LivroService service;
+
+    @PostMapping
+    public ResponseEntity<Object> salvar(@RequestBody @Valid CadastroLivroDTO dto) {
+        try {
+            return ResponseEntity.ok(dto);
+        } catch (RegistroDuplicadoException e) {
+            var erroDTO = ErroResposta.conflito(e.getMessage());
+            return ResponseEntity.status(erroDTO.status()).body(erroDTO);
+        }
+    }
 }
--- Estudos/java/libraryapi/src/main/java/com/example/libraryapi/controller/dto/CadastroLivroDTO.java
+++ Estudos/java/libraryapi/src/main/java/com/example/libraryapi/controller/dto/CadastroLivroDTO.java
@@ -1,10 +1,14 @@
 package com.example.libraryapi.controller.dto;
 
 import com.example.libraryapi.model.enums.GeneroLivro;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.hibernate.validator.constraints.ISBN;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record CadastroLivroDTO(String isbn, String titulo, LocalDate dataPublicacao, GeneroLivro genero, BigDecimal preco, UUID idAutor) {
+public record CadastroLivroDTO(@ISBN @NotBlank(message = "campo obrigatório") String isbn, @NotBlank(message = "campo obrigatório") String titulo, @NotNull(message = "campo obrigatório") @Past(message = "não pode ser uma data futura") LocalDate dataPublicacao, GeneroLivro genero, BigDecimal preco, @NotNull(message = "campo obrigatório") UUID idAutor) {
 }
