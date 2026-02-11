6322a9fc8a4b5227cafe3721aa28ba933c0cdab7
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-02-11 13:41:48 -0300

    Finalizando o contrato de Salvar um Autor, Adicionando Auditoria JPA nas entidades - aula 81 e 82

--- Java/Estudos_Java/SpringBoot/libraryapi/src/main/java/com/example/libraryapi/controller/AutorController.java
+++ Java/Estudos_Java/SpringBoot/libraryapi/src/main/java/com/example/libraryapi/controller/AutorController.java
@@ -1,9 +1,39 @@
 package com.example.libraryapi.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.libraryapi.controller.dto.AutorDTO;
+import com.example.libraryapi.service.AutorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("autores")
 public class AutorController {
+
+    private final AutorService service;
+
+    public AutorController(AutorService service){
+        this.service = service;
+    }
+
+    @PostMapping
+    //@RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> salvar(@RequestBody AutorDTO autor){
+        var autorEntidade = autor.mapearParaAutor();
+        service.salvar(autorEntidade);
+
+
+        // http://localhost:8080/autores/7d9a00e9-3ac7-49be-a3e4-3a15419ce8a5
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/" +
+                        "{id}")
+                .buildAndExpand(autorEntidade.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
 }
