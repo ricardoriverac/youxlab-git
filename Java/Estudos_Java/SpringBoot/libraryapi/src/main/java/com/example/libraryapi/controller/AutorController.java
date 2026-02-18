c7206f4a4a1d74a6c98a2f82ed02709860a4ef6a
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-02-18 13:50:19 -0300

    Pesquisa de Autores com filtro - aula 85, seção 5

--- Java/Estudos_Java/SpringBoot/libraryapi/src/main/java/com/example/libraryapi/controller/AutorController.java
+++ Java/Estudos_Java/SpringBoot/libraryapi/src/main/java/com/example/libraryapi/controller/AutorController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("autores")
@@ -61,7 +63,7 @@ public class AutorController {
         var idAutor = UUID.fromString(id);
         Optional<Autor> autorOptional =  service.obterPorId(idAutor);
 
-        if (autorOptional.isPresent()){
+        if (autorOptional.isEmpty()){
             return ResponseEntity.notFound().build();
         }
 
@@ -69,5 +71,22 @@ public class AutorController {
 
         return ResponseEntity.noContent().build();
 
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AutorDTO>> pesquisar(
+            @RequestParam(value = "nome", required = false) String nome,
+            @RequestParam(value = "nacionalidade", required = false) String nacionalidade){
+        List<Autor> resultado = service.pesquisa(nome, nacionalidade);
+        List<AutorDTO> lista = resultado
+                .stream()
+                .map(autor -> new AutorDTO(
+                        autor.getId(),
+                        autor.getNome(),
+                        autor.getDataNascimento(),
+                        autor.getNacionalidade())
+                ).collect(Collectors.toList());
+
+        return ResponseEntity.ok(lista);
     }
 }
