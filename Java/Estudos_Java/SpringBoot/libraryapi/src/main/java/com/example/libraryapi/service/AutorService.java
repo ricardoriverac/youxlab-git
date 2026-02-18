c260e1d42e210f1b3ca17df33d82bdaaee16074f
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-02-18 17:42:50 -0300

    Usando RequiredArgsContructor do Lombok - aula 91

--- Java/Estudos_Java/SpringBoot/libraryapi/src/main/java/com/example/libraryapi/service/AutorService.java
+++ Java/Estudos_Java/SpringBoot/libraryapi/src/main/java/com/example/libraryapi/service/AutorService.java
@@ -5,6 +5,7 @@ import com.example.libraryapi.model.Autor;
 import com.example.libraryapi.repository.AutorRepository;
 import com.example.libraryapi.repository.LivroRepository;
 import com.example.libraryapi.validator.AutorValidator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,19 +13,12 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class AutorService {
 
     private final AutorRepository repository;
     private final AutorValidator validator;
     private final LivroRepository livroRepository;
-
-    private AutorService(AutorRepository repository,
-                         AutorValidator validator,
-                         LivroRepository livroRepository){
-        this.repository = repository;
-        this.validator = validator;
-        this.livroRepository = livroRepository;
-    }
 
     public Autor salvar(Autor autor){
         validator.validar(autor);
