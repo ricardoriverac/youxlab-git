95ea5fc5fa789c1ae18b7ccd41d3d718947fc49f
desafio_python_git <ottofillipi185@gmail.com>
2026-02-18 16:37:12 -0300

    estudo_java

--- estudo_Java/src/Aula_109/Matrizes/Exercicio_Fixacao/Main_01.java
+++ estudo_Java/src/Aula_109/Matrizes/Exercicio_Fixacao/Main_01.java
@@ -2,7 +2,7 @@ package Aula_109.Matrizes.Exercicio_Fixacao;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main_01 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
