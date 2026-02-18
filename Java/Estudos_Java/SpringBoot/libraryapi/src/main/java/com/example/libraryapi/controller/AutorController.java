c2ed7051eb74c0f746f87a6bc7c3c23588588d41
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-02-18 14:13:10 -0300

    Atualização de Autores - aula 86

--- Java/Estudos_Java/SpringBoot/libraryapi/src/main/java/com/example/libraryapi/controller/AutorController.java
+++ Java/Estudos_Java/SpringBoot/libraryapi/src/main/java/com/example/libraryapi/controller/AutorController.java
@@ -89,4 +89,25 @@ public class AutorController {
 
         return ResponseEntity.ok(lista);
     }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> atualizar(
+            @PathVariable String id, @RequestBody AutorDTO dto){
+
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional =  service.obterPorId(idAutor);
+
+        if (autorOptional.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        var autor = autorOptional.get();
+        autor.setNome(dto.nome());
+        autor.setDataNascimento(dto.dataNascimento());
+        autor.setNacionalidade(dto.nacionalidade());
+
+        service.atualizar(autor);
+
+        return ResponseEntity.noContent().build();
+    }
 }
