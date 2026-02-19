2f752a3d70c0c6fb993f61a5ca736ed105f22729
Lucas RibYouX <lucas.pereira-youxlab@institutoyoux.org.br>
2026-02-19 14:42:47 -0300

    adicionando associacao muitos para muitos entre Product e OrderItems

--- Java/projeto/testeCurso1/testeCurso/src/main/java/com/TesteLucas/testeCurso/entities/OrderItem.java
+++ Java/projeto/testeCurso1/testeCurso/src/main/java/com/TesteLucas/testeCurso/entities/OrderItem.java
@@ -40,7 +40,7 @@ public class OrderItem implements Serializable {
         id.setOrder(order);
     }
 
-    public Product product(){
+    public Product getProduct(){
         return id.getProduct();
     }
     public void setProduct(Product product){
