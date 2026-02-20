b0536932b5950f100152229d08064a063cec727e
Lucas RibYouX <lucas.pereira-youxlab@institutoyoux.org.br>
2026-02-20 08:32:10 -0300

    Adicionando update

--- Java/projeto/testeCurso1/testeCurso/src/main/java/com/TesteLucas/testeCurso/service/UserService.java
+++ Java/projeto/testeCurso1/testeCurso/src/main/java/com/TesteLucas/testeCurso/service/UserService.java
@@ -2,6 +2,7 @@ package com.TesteLucas.testeCurso.service;
 
 import com.TesteLucas.testeCurso.entities.User;
 import com.TesteLucas.testeCurso.repositories.UserRepositories;
+import jakarta.persistence.Entity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -28,5 +29,17 @@ public class UserService {
 
     public void delete(Long id){
         repositories.deleteById(id);
+    }
+
+    public User update(Long id, User obj) {
+        User entity = repositories.getReferenceById(id);
+        updateData(entity, obj);
+        return repositories.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPassword(obj.getPassword());
     }
 }
