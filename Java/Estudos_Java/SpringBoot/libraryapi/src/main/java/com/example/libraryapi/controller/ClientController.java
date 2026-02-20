d880feaaae48984e34efc92325aaa3d436ebb029
Luis Paiva <luis.sousa-youxlab@institutoyoux.org.br>
2026-02-20 09:50:17 -0300

    Criando cadastramento de clients do Authorization Server - aula 143

---
+++ Java/Estudos_Java/SpringBoot/libraryapi/src/main/java/com/example/libraryapi/controller/ClientController.java
@@ -0,0 +1,16 @@
+package com.example.libraryapi.controller;
+
+//@RestController no usages new
+//@RequestMapping("clients")
+//@RequiredArgsConstructor
+public class ClientController {
+
+//        private final ClientService service;
+
+//        @PostMapping
+//        @ResponseStatus(HttpStatus.CREATED)
+//        @PreAuthorize("hasRole('GERENTE')");
+//        public void salvar (@RequestBody Client client){
+//            service.salvar (client);
+//        }
+}
---
+++ Java/Estudos_Java/SpringBoot/libraryapi/src/main/java/com/example/libraryapi/service/ClientService.java
@@ -0,0 +1,24 @@
+package com.example.libraryapi.service;
+
+import com.example.libraryapi.model.Client;
+import com.example.libraryapi.repository.ClientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//@Service
+//@RequiredArgsConstructor
+public class ClientService {
+//
+//    private final ClientRepository repository;
+//    private final PassWordEncoder encoder;
+//
+//    public Client salvar (Client client) {
+//        var senhaCriptografada = encoder.encode(client.getClientSecret);
+//        client.setClientSecret(senhaCriptografada);
+//        return repository.save(client);
+//    }
+//    public Client obterPorClientID (String clientId) {
+//        return repository.findByClientId(clientId);
+//    }
+}
