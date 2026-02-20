a3ce9eabff2339c326c06eb649a5d472498da6ac
pietromarinho22 <pietrovmarinho@gmail.com>
2026-02-20 08:42:02 -0300

    Aula 283 Seção 22

---
+++ spring/course/src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,29 @@
+package com.educandoweb.course.resources;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
