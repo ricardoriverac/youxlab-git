57117fa2c1b72c961ef9f78738154bcf579d1ce9
Thawane <thawane.correa@institutoyoux.org.br>
2026-02-22 10:11:00 -0300

    Aula97

---
+++ Java/CursoJava/src/Secao10/Aula97/aula97.java
@@ -0,0 +1,27 @@
+package Secao10.Aula97;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class aula97 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        product[] vect = new product[n];
+        for (int i=0; i<vect.length; i++) {
+            sc.nextLine();
+            String name = sc.nextLine();
+            double price = sc.nextDouble();
+            vect[i] = new product(name, price);
+        }
+        double sum = 0.0;
+        for (int i=0; i<vect.length; i++) {
+            sum += vect[i].getPrice();
+        }
+        double avg = sum / vect.length;
+        System.out.printf("AVERAGE PRICE = %.2f\n", avg);
+        sc.close();
+    }
+}
---
+++ Java/CursoJava/src/Secao10/Aula97/product.java
@@ -0,0 +1,26 @@
+package Secao10.Aula97;
+
+public class product {
+    private String name;
+    private double price;
+
+    public product(String name, double price) {
+        super();
+        this.name = name;
+        this.price = price;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name =  name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
