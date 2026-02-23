4078a29c1cca5993cc9592fd42b267b177f35947
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-02-23 16:13:38 -0300

    Curso Spring Boot Michelli Brito concluido

---
+++ Estudos/java/parking-control/src/test/java/com/api/packing_control/PackingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.packing_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
