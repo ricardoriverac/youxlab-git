8cab2ebd4e7f7ddfd1a008ca7de7e57b0daee35e
Thawane <thawane.correa@institutoyoux.org.br>
2026-02-24 11:35:39 -0300

    Aula132

---
+++ Java/CursoJava/src/Secao12/aula132/Comment.java
@@ -0,0 +1,17 @@
+package Secao12.aula132;
+
+public class Comment {
+    private String text;
+
+    public Comment(){
+    }
+    public Comment(String text){
+        this.text = text;
+    }
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+}
