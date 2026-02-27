b39967680bb5a724cd6743607b7e4ab0260adf8f
daniella <daniella.flor-youxlab@institutoyoux.org.br>
2026-02-27 17:03:34 -0300

    iniciando projeto security

---
+++ ciclo2/springboot/projetoSecurity/src/main/java/com/example/projetoSecurity/ProjetoSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetoSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSecurityApplication.class, args);
+	}
+
+}
