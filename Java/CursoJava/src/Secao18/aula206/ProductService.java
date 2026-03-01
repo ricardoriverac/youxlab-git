ca75d075fd9e6bb679873460cd9beb84e44deee9
Thawane <thawane.correa@institutoyoux.org.br>
2026-03-01 08:48:10 -0300

    Aula206

---
+++ Java/CursoJava/src/Secao18/aula206/ProductService.java
@@ -0,0 +1,16 @@
+package Secao18.aula206;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ProductService {
+    public double filteredSum(List<Product> list, Predicate<Product> criteria) {
+        double sum = 0.0;
+        for (Product p : list) {
+            if (criteria.test(p)) {
+                sum += p.getPrice();
+            }
+        }
+        return sum;
+    }
+}
