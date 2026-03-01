77195781492f5238b180eb3181d1134e66498b7d
Thawane <thawane.correa@institutoyoux.org.br>
2026-03-01 08:00:58 -0300

    Aula200

---
+++ Java/CursoJava/src/Secao18/aula200/Product.java
@@ -0,0 +1,23 @@
+package Secao18.aula200;
+
+public class Product {
+    private String name;
+    private Double price;
+
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+    @Override
+    public String toString() {
+        return "Product [name=" + name + ", price=" + price + "]";
+    }
+}
