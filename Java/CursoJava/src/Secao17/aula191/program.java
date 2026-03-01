b240316c3f447d9443c92cedeee225bba62193fd
Thawane <thawane.correa@institutoyoux.org.br>
2026-02-28 23:20:16 -0300

    Aula192

--- Java/CursoJava/src/Secao17/aula191/program.java
+++ Java/CursoJava/src/Secao17/aula191/program.java
@@ -18,5 +18,27 @@ public class program {
             e.removeAll(b);
             System.out.println(e);
         }
+
+    public static class Product {
+        private String name;
+        private Double price;
+
+        public Product(String name, Double price) {
+            this.name = name;
+            this.price = price;
+        }
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public Double getPrice() {
+            return price;
+        }
+        public void setPrice(Double price) {
+            this.price = price;
+        }
     }
+}
 
