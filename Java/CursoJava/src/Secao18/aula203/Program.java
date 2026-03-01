0b3a60377bf6357766576ffb568a3f9762a1accb
Thawane <thawane.correa@institutoyoux.org.br>
2026-03-01 08:29:27 -0300

    Aula203

---
+++ Java/CursoJava/src/Secao18/aula203/Program.java
@@ -0,0 +1,26 @@
+package Secao18.aula203;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Program {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        double min = 100.0;
+
+        Predicate<Product> pred = p -> p.getPrice() >= min;
+
+        list.removeIf(pred);
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+    }
+}
