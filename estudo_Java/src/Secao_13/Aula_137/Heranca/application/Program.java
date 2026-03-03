3fb8a0a6f43b87e17385db3b28becaec711c86ed
desafio_python_git <ottofillipi185@gmail.com>
2026-03-03 13:59:17 -0300

    Estudando_Java

---
+++ estudo_Java/src/Secao_13/Aula_137/Heranca/application/Program.java
@@ -0,0 +1,12 @@
+package Secao_13.Aula_137.Heranca.application;
+
+import Secao_13.Aula_137.Heranca.entities.BusinessAcount;
+
+public class Program {
+    public static void main(String[] args) {
+
+        BusinessAcount account = new BusinessAcount();
+
+
+    }
+}
