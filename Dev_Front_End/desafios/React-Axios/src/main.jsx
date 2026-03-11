f64150163f535c0feacb600a55414f31aa92d465
Youx <heronherin10@gmail.com>
2026-03-11 16:10:43 -0300

    programa feito para aprender axios terminado

---
+++ Dev_Front_End/desafios/React-Axios/src/main.jsx
@@ -0,0 +1,32 @@
+// import { StrictMode } from 'react'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Páginas
+import Home from "./routes/Home.jsx";
+import NewPost from "./routes/NewPost.jsx";
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/new",
+        element: <NewPost />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  // <StrictMode>
+  <RouterProvider router={router} />,
+  // </StrictMode>
+);
