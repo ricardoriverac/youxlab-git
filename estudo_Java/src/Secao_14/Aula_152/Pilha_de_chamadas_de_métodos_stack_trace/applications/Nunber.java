fb631766f1cd2eca4b35fa22cbc08b7b6588b881
otto321767 <ottofillipi185@gmail.com>
2026-03-16 15:51:34 -0300

    Métodos stack trace

--- estudo_Java/src/Secao_14/Aula_152/Pilha_de_chamadas_de_métodos_stack_trace/applications/Nunber.java
+++ estudo_Java/src/Secao_14/Aula_152/Pilha_de_chamadas_de_métodos_stack_trace/applications/Nunber.java
@@ -1,4 +1,20 @@
 package Secao_14.Aula_152.Pilha_de_chamadas_de_métodos_stack_trace.applications;
 
+import java.util.Scanner;
+
 public class Nunber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        try {
+            System.out.print("Digite um número inteiro: ");
+            int numeroInteiro = sc.nextInt();
+        }
+        catch (Exception e) {
+            System.out.println("ERRO!!");
+            e.printStackTrace();
+        }
+    }
 }
