abc0773ebde5c97eba7aa5bbb7120c8a05e41430
otto321767 <ottofillipi185@gmail.com>
2026-03-17 17:19:31 -0300

    Segunda solução - Bom

---
+++ estudo_Java/src/Secao_14/Aula_154/Execoes_Personalizadas/Solucao_Boa/applications/Main.java
@@ -0,0 +1,42 @@
+package Secao_14.Aula_154.Execoes_Personalizadas.Solucao_Boa.applications;
+
+import Secao_14.Aula_154.Execoes_Personalizadas.Solucao_Boa.model_entities.Reservation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        try {
+            System.out.print("Room number: ");
+            int number = sc.nextInt();
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            Date checkIn = sdf.parse(sc.next());
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            Date checkOut = sdf.parse(sc.next());
+
+
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
+
+            System.out.println("\nEnter date to update the reservation:");
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            checkIn = sdf.parse(sc.next());
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            checkOut = sdf.parse(sc.next());
+
+            reservation.updateDates(checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
+        }
+        catch (ParseException e) {
+            System.out.println("Invalid date format");
+        }
+    }
+}
+
