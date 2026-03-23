5e6809890b421ba43745ad70fe7594bd4f4d6554
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-03-23 16:37:38 -0300

    Aula 14 - SwitchCase em JavaScript

---
+++ Estudos/javaScript/aula14.js
@@ -0,0 +1,19 @@
+let colocacao = 10
+
+switch(colocacao){
+    case 1:
+        console.log("Primeiro lugar")
+        break
+    case 2:
+        console.log("Segundo lugar")
+        break
+    case 3:
+        console.log("Terceiro lugar")
+        break
+    case 4: case 5: case 6:
+        console.log("Premio de participacao")
+        break
+    default:
+        console.log("Não subiu ao podio")
+        break
+}
