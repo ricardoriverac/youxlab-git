9f3ef50b2fc73ed4269784fbdaf032982823d3be
otto321767 <ottofillipi185@gmail.com>
2026-03-23 14:32:00 -0300

    Exercício de Fixação

--- estudo_Java/src/Secao_15/Aula_162/Lendo_arquivo_texto_com_classes_File_e_Scanner/application/Main.java
+++ estudo_Java/src/Secao_15/Aula_162/Lendo_arquivo_texto_com_classes_File_e_Scanner/application/Main.java
@@ -1,4 +1,28 @@
 package Secao_15.Aula_162.Lendo_arquivo_texto_com_classes_File_e_Scanner.application;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        File file = new File("/home/youx/youxlab-git/estudo_Java/src/Secao_15/Aula_162/Lendo_arquivo_texto_com_classes_File_e_Scanner/arquivo/in.txt");
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+        finally {
+            if (sc != null) {
+                sc.close();
+            }
+        }
+    }
 }
