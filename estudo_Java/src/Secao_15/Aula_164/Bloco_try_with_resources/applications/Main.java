faefc6623d1b71ba020366af1219f3c2c84f01ed
otto321767 <ottofillipi185@gmail.com>
2026-03-23 16:43:57 -0300

    Bloco try-with-resoucer

--- estudo_Java/src/Secao_15/Aula_164/Bloco_try_with_resources/applications/Main.java
+++ estudo_Java/src/Secao_15/Aula_164/Bloco_try_with_resources/applications/Main.java
@@ -1,4 +1,24 @@
 package Secao_15.Aula_164.Bloco_try_with_resources.applications;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) {
+
+        String path = "src/Secao_15/Aula_163/FileReader_e_BufferedReader/file/in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Erro" + e.getMessage());
+        }
+    }
 }
