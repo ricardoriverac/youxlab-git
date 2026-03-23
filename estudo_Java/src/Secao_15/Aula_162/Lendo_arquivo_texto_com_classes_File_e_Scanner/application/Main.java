09f3c3400cacc1ceedd1a0f589c35915b476595d
otto321767 <ottofillipi185@gmail.com>
2026-03-23 14:27:46 -0300

    Exercício de Fixação

---
+++ estudo_Java/src/Secao_15/Aula_162/Lendo_arquivo_texto_com_classes_File_e_Scanner/application/Main.java
@@ -0,0 +1,4 @@
+package Secao_15.Aula_162.Lendo_arquivo_texto_com_classes_File_e_Scanner.application;
+
+public class Main {
+}
---
+++ estudo_Java/src/Secao_15/Aula_163/FileReader_e_BufferedReader/applications/Main.java
@@ -0,0 +1,4 @@
+package Secao_15.Aula_163.FileReader_e_BufferedReader.applications;
+
+public class Main {
+}
