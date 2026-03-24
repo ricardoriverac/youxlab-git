a5ef43ba42f1d197a0319cc56ac776b42f442bb3
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-03-23 22:51:07 -0300

    Aula 28 - Aprendendo sobre o método MAP em Javascript

---
+++ Estudos/javaScript/aula28.js
@@ -0,0 +1,21 @@
+//const cursos = ['HTML', 'CSS', 'Javascript', 'PHP', 'React']
+//cursos.map((e1, i)=>{
+//    console.log("Curso: " + e1 + " - Posição do curso: " + i)
+//let el=document.getElementsByTagName("div")
+//el=[...el]
+//el.map((e,i)=>{
+//    console.log(e.innerHTML)
+//})
+
+//const el = document.getElementsByTagName("div")
+//const val=Array.prototype.map.call(el,({innerHTML})=>innerHTML)
+//console.log(val)
+
+const converterInt=(e)=>parseInt(e)
+const dobrar = (e)=>e*2
+let num=['1','2','3','4','5'].map(dobrar)
+let num=['1','2','3','4','5']
+
+
+num.map(converterInt)
+console.log(num)
