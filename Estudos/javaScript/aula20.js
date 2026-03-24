f97c4e67a7108fba0e4d14cf7ec4e077f2ea80fa
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-03-23 21:33:21 -0300

    Aula 20 - Funções em JavaScript

---
+++ Estudos/javaScript/aula20.js
@@ -0,0 +1,8 @@
+function mudarTexto(){
+    let d1=document.getElementById("d1")
+    let d2=document.getElementById("d2")
+    let d3=document.getElementById("d3")
+    d1.innerHTML="CFB CURSOS"
+    d2.innerHTML="CFB CURSOS"
+    d3.innerHTML="CFB CURSOS"
+}
