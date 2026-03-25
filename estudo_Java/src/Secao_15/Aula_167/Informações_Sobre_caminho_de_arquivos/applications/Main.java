74f1893161583b58c62e16540b133dc7d9c0f19f
otto321767 <ottofillipi185@gmail.com>
2026-03-25 13:58:06 -0300

    Funções da classe File

---
+++ estudo_Java/src/Secao_15/Aula_167/Informações_Sobre_caminho_de_arquivos/applications/Main.java
@@ -0,0 +1,22 @@
+package Secao_15.Aula_167.Informações_Sobre_caminho_de_arquivos.applications;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a file path: ");
+        String strPath = sc.nextLine();
+
+        File path = new File(strPath);
+
+        System.out.println("getName: " + path.getName());
+        System.out.println("getParent: " + path.getParent());
+        System.out.println("getPath" + path.getPath());
+
+        sc.close();
+    }
+}
