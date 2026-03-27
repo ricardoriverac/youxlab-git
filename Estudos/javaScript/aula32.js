c232a6ff0d85d7660106c956e577bf3e9cc5d1dd
pedro.couto <pedro.couto-youxlab@institutoyoux.org.br>
2026-03-27 16:52:52 -0300

    Aula 32 java script concluida - getElementsByClassName

---
+++ Estudos/javaScript/aula32.js
@@ -0,0 +1,12 @@
+const cursosTodos = [...document.getElementsByClassName("curso")]
+const cursosC1=[...document.getElementsByClassName("c1")]
+const cursosC2=[...document.getElementsByClassName("c2")]
+const cursoEspecial= document.getElementsByClassName("curso")[0]
+console.log(cursosTodos)
+console.log(cursosC1)
+console.log(cursosC2)
+console.log(cursoEspecial)
+
+cursosC2.map((el, i)=>{
+    el.classList.add("destaque")
+})
