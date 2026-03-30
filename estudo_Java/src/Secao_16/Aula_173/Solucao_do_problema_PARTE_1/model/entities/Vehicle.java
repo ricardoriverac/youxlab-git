84fb38262d7e40f9f5cfe1e5123eedb0285e5a9d
otto321767 <ottofillipi185@gmail.com>
2026-03-30 14:22:40 -0300

    Solução do Problema - Parte 1

---
+++ estudo_Java/src/Secao_16/Aula_173/Solucao_do_problema_PARTE_1/model/entities/Vehicle.java
@@ -0,0 +1,20 @@
+package Secao_16.Aula_173.Solucao_do_problema_PARTE_1.model.entities;
+
+public class Vehicle {
+
+    private String model;
+
+    public Vehicle() {}
+
+    public Vehicle(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
