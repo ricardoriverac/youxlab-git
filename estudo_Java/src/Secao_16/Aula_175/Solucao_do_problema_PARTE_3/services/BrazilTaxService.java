644b121677dcb5022d85e30b211e515144928027
otto321767 <ottofillipi185@gmail.com>
2026-03-31 17:29:55 -0300

    Solução do Problema (Com Interface) - Parte 3

--- estudo_Java/src/Secao_16/Aula_175/Solucao_do_problema_PARTE_3/services/BrazilTaxService.java
+++ estudo_Java/src/Secao_16/Aula_175/Solucao_do_problema_PARTE_3/services/BrazilTaxService.java
@@ -1,7 +1,7 @@
 package Secao_16.Aula_175.Solucao_do_problema_PARTE_3.services;
 
 public class BrazilTaxService implements TaxService {
-
+    @Override
     public double tax(double amount) {
         if (amount <= 100.0) {
             return amount * 0.2;
