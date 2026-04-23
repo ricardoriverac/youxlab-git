8b2f3696416571f6df32df321d4264b4c3070547
otto321767 <ottofillipi185@gmail.com>
2026-04-23 17:16:42 -0300

    Interface Comparable

---
+++ estudo_Java/src/Secao_16/Aula_182/Interface_Comparable/entities/Employee.java
@@ -0,0 +1,35 @@
+package Secao_16.Aula_182.Interface_Comparable.entities;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Employee implements Comparable<Employee> {
+
+    private String name;
+    private Double salary;
+
+    public Employee(String name, Double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public int compareTo(@NotNull Employee other) {
+        return this.name.compareTo(other.getName());
+    }
+}
