f8cdf598bc7c9190d5d9c8675ce95fd5780884c3
otto321767 <ottofillipi185@gmail.com>
2026-06-02 13:07:33 -0300

    LinKedHashSet

--- estudo_Java/src/Secao_17/Aula_192/Set/LinKedHashSet/Main.java
+++ estudo_Java/src/Secao_17/Aula_192/Set/LinKedHashSet/Main.java
@@ -1,4 +1,26 @@
 package Secao_17.Aula_192.Set.LinKedHashSet;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
+    public static void main(String[] args) {
+
+        //LinKedHashSet - velocidade intermediária e elementos na ordem em que são adicionadas
+
+        // Ele mantem a ordem
+
+        Set<String> set = new LinkedHashSet<>();
+
+        set.add("TV");
+        set.add("Tablet");
+        set.add("Notebook");
+
+        System.out.println(set.contains("Notebook"));
+
+        for (String p : set) {
+            System.out.println(p);
+        }
+    }
 }
