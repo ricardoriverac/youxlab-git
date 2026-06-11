e76e80ba2dfa2fe5544df86bd8f79d3979093cb2
pedro-couto1-git <pedrocouto.profissional@gmail.com>
2026-06-11 11:54:57 -0300

    Avançando em props - Curso React #08

--- Estudos/curso-react/src/components/List.js
+++ Estudos/curso-react/src/components/List.js
@@ -5,8 +5,8 @@ function List() {
         <>
             <h1>Minha lista</h1>
             <ul>
-                <Item marca="Ferrari" />
-                <Item marca="Fiat" />
+                <Item marca="Ferrari" ano_lancamento={1985}/>
+                <Item marca="Fiat" ano_lancamento={1964}/>
                 <Item marca="Renault" />
             </ul>
         </>
