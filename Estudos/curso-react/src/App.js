9dda3d8e0557ff13029e09eea35925cbf1cbe5ab
pedro-couto1-git <pedrocouto.profissional@gmail.com>
2026-06-10 22:04:57 -0300

    Utilizando react fragments - #07

--- Estudos/curso-react/src/App.js
+++ Estudos/curso-react/src/App.js
@@ -3,6 +3,7 @@ import HelloWorld from './components/HelloWorld'
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase'
+import List  from './components/List';
 function App() {
 
   const nome = "Maria"
@@ -14,6 +15,7 @@ function App() {
       <SayMyName nome='Couto' />
       <SayMyName nome={nome} />
       <Pessoa nome="Rodrigo" idade="28" profissao="Programador" />
+      <List />
     </div>
   )
 }
---
+++ Estudos/curso-react/src/components/Item.js
@@ -0,0 +1,10 @@
+function Item(props) {
+    return (
+        <>
+        <li>{props.marca}</li>
+        <p>teste</p>
+        </>
+    )
+}
+
+export default Item;
