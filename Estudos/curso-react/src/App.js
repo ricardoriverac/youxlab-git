b7d6b712ae4ab349d5da3874b09bac9bea2a21b1
pedro-couto1-git <pedrocouto.profissional@gmail.com>
2026-06-11 20:19:14 -0300

    Renderização de listas - Curso react #13

--- Estudos/curso-react/src/App.js
+++ Estudos/curso-react/src/App.js
@@ -2,11 +2,15 @@ import './App.css'
 import Condicional from './components/Condicional';
 import Evento from './components/Evento'
 import Form from './components/Form';
+import OutraLIsta from './components/OutraLIsta';
 function App() {
+
+  const meusItens = ['React', 'Vue', 'Angular']
+
   return (
   <div className='App'>
-    <h1>Renderização condicional</h1>
-    <Condicional />
+    <h1>Renderização de listas</h1>
+    <OutraLIsta  itens={meusItens}/>
   </div>
   )
 }
