2c3aa33f808cb7b972f090ae58652289b4f94716
pedro-couto1-git <pedrocouto.profissional@gmail.com>
2026-06-11 21:11:16 -0300

    React Icons - #16

--- Estudos/curso-react/src/components/layout/Footer.js
+++ Estudos/curso-react/src/components/layout/Footer.js
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
 function Footer() {
   return (
-    <Footer>rodapé</Footer>
+    <Footer>
+        <ul>
+            <li><FaFacebook /></li>
+            <li><FaInstagram /></li>
+            <li><FaLinkedin /></li>
+        </ul>
+        <p>Nosso rodapé</p>
+    </Footer>
   )
 }
 
