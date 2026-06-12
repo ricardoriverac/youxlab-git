f447e9c476618a8c50b0a39fab1730a8f3fe1bdd
pedro-couto1-git <pedrocouto.profissional@gmail.com>
2026-06-11 21:04:05 -0300

    Implementando o React ROuter - #15

--- Estudos/curso-react/src/App.js
+++ Estudos/curso-react/src/App.js
@@ -1,16 +1,22 @@
-import { useState } from 'react';
-import './App.css'
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/saudacao';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Contato from './pages/Contato';
+import Empresa from './pages/Empresa';
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer';
+
 function App() {
-  const [nome, setNome] = useState()
   return (
-  <div className='App'>
-    <h1>State lift</h1>
-    <SeuNome setNome={setNome}/>
-    <Saudacao nome={nome}/>
-  </div>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/empresa" element={<Empresa />} />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
---
+++ Estudos/curso-react/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <Footer>rodapé</Footer>
+  )
+}
+
+export default Footer
